ca3f42004ebdd1755470a796b04766fe67977926
pedro tsividis <tsividis@mit.edu>
2016-06-22 17:13:34 +0300

    started theory template

--- vgdl/theory_template.py
+++ vgdl/theory_template.py
@@ -5,8 +5,19 @@ class Theory(object):
 	def __init__(self, color):
 		self.color = color
 		self.oType = False #extend this to a set of types and a distribution over them?
-		self.terms = [] #a list of 
+		self.terms = [\
+						#1	self wall undoAll
+						#2	self box bounceForward
+						#	[self mushroom killSprite, self mushroom killIfHasLess resource=medicine limit=-1
+						] #a list of 
 
-
+		self.discarded = [\
+						# 
+							]
 	#def printToTxt(self):
 		#creates relevant lines of interaction set, etc.
+
+	#To prevent storing duplicate theories (e.g., box wall interactions in the box theories
+	#and in the wall theories), have a checklist at the top of possible non-agent pairs and 
+	#manage which new theories you create there? or just make everything but don't print twice?
+
