383a32290575078fab1329424d8508c9f6d75df6
Jacqueline Xu <jackielxu@gmail.com>
2016-08-11 14:15:51 -0400

    Edited how to run induction

--- vgdl/induction.py
+++ vgdl/induction.py
@@ -30,14 +30,14 @@ def runInduction_DFS(vgdlString, gameOutput):
 
 if __name__ == "__main__":
     """
-    Run: "python induction.py ../vgdl_text/simpleGame1.txt ../output/simpleGame1.txt" 
+    Run: "python induction.py simpleGame1.txt simpleGame1.txt" 
     """
-    vgdlFile = sys.argv[1]
-    with open(vgdlFile, 'r') as vf:
+    game = sys.argv[1]
+    output = sys.argv[2]
+    with open("../vgdl_text/{}".format(game), 'r') as vf:
         vgdlString = ast.literal_eval(vf.read())
 
-    gameOutput = sys.argv[2]
-    with open(gameOutput, 'r') as f:
+    with open("../output/{}".format(output), 'r') as f:
         output = f.readline()
         output_tuple = ast.literal_eval(output)
         #print output_tuple
