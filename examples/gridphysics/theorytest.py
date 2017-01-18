6dad54b016fbd8445fc7cbe315e1ab8a63114139
Pedro <tsividis@mit.edu>
2017-01-17 23:57:39 -0500

    Renamed. Main code is in agent.py now.

--- examples/gridphysics/theorytest.py
+++ examples/gridphysics/theorytest.py
@@ -1,8 +1,8 @@
 level="""
 000000000
-0       0
-0    A  0
-0 3  0200
+0  G    0
+0    3  0
+0A2  0O00
 0    0 00
 000000000
 """
@@ -10,27 +10,28 @@ game = """
 BasicGame
 	SpriteSet
 		avatar > MovingAvatar color=DARKBLUE
-		poison > ResourcePack color=BROWN
-		box1 > Resource color=ORANGE
-		box2 > ResourcePack color=RED
-		goal > Passive color=GOLD
-		wall > Immovable color=BLACK
+		poison > Passive color=BROWN
+		box1 > Missile color=ORANGE
+		goal > Missile color=ORANGE
+		box2 > RandomNPC color=RED
+		oldGoal > Passive color=GOLD
+		wall > Passive color=BLACK
 	InteractionSet
+		box1 avatar > bounceForward
 		poison avatar > bounceForward
 		wall avatar > bounceForward
 		goal avatar > bounceForward
-		avatar wall > stepBack
-		box1 avatar > killSprite
-		box2 avatar > killSprite
+		box2 avatar > bounceForward
 	TerminationSet
 		SpriteCounter stype=avatar limit=0 win=False
 		SpriteCounter stype=goal limit=0 win=True
 	LevelMapping
+		G > goal
 		0 > wall
+		2 > poison
+		O > oldGoal
 		1 > box1
-		2 > goal
-		3 > poison
-		4 > box2
+		3 > box2
 """
 if __name__ == "__main__":
 	from vgdl.core import VGDLParser
--- vgdl/agent.py
+++ vgdl/agent.py
@@ -3,7 +3,7 @@ from util import *
 from core import colorDict
 from ontology import Immovable, Passive, Resource, ResourcePack, RandomNPC, Chaser, AStarChaser, OrientedSprite, Missile
 from ontology import initializeDistribution, updateDistribution, updateOptions, sampleFromDistribution, spriteInduction, selectSubgoal
-from theory_template import TimeStep, Precondition, InteractionRule, TerminationRule, TimeoutRule, SpriteCounterRule, MultiSpriteCounterRule, ruleCluster, Theory, Game
+from theory_template import TimeStep, Precondition, InteractionRule, TerminationRule, TimeoutRule, SpriteCounterRule, MultiSpriteCounterRule, ruleCluster, Theory, Game, writeTheoryToTxt
 
 '''
 ## helpful functions or access methods:
@@ -32,48 +32,39 @@ if __name__ == "__main__":
 
 	spriteInduction(rle, step=0)					## Initialize sprite induction
 
+	## Initialize mental theory
+	sample = sampleFromDistribution(rle._game.spriteDistribution, all_objects)
+	g = Game(spriteInductionResult=sample)
+	t = g.buildGenericTheory(sample)
+	hypotheses = [t]	
 
 	## When you restart episodes, reset the rle.agentStatePrev. Maybe some other things, too.
 	
 	print ""
 	print ""
 	print np.reshape(rle._getSensors(), rle.outdim)
-	sample = sampleFromDistribution(rle._game.spriteDistribution, all_objects)
-	g = Game(spriteInductionResult=sample)
+
 	## Temporary hack -- change as soon as we can write theory files.
-	theories = [createRLVirtualGame, createRLVirtualGame2]
+	# theories = [createRLVirtualGame, createRLVirtualGame2]
 	goals = ['box1', 'box2']
 	for i in range(2):
+		
 		# subgoal = selectSubgoal(rle, method='preselected')
+		print i
+
 		subgoal = random.choice(rle._game.sprite_groups[goals[i]])
+		pos = rle._rect2pos(subgoal.rect)
+		print pos
+
+		game, level = writeTheoryToTxt(rle,hypotheses[0], "./examples/gridphysics/theorytest.py", rle._rect2pos(subgoal.rect))
 		
-		## Initialize some theory
-		## Includes specifying the goal you selected as the termination condition. E.g., use pos.
-		## This will merely overwrite the pre-existing file.
-
-		rleVirtualFunc = theories[i] 		
-		Vrle = rleVirtualFunc(OBSERVATION_GLOBAL)	##World in agent's head.
-
+		Vrle = createMindEnv(game, level, OBSERVATION_GLOBAL)	##World in agent's head.
+		
 		## Plan to achieve that goal
-		rle, hypotheses = getToSubgoal(rle, Vrle, subgoal, all_objects, finalEventList, sample)
-		print "in agent loop"
-		from rlenvironmentnonstatic import *
-		from theory_template import writeTheoryToTxt
-		# rle2= createRLtextTheory(obsType=OBSERVATION_GLOBAL)
-		
+		rle, hypotheses, finalEventList = getToSubgoal(rle, Vrle, subgoal, all_objects, finalEventList, sample)
 
-		# sample = sampleFromDistribution(rle._game.spriteDistribution, all_objects)
-		# g = Game(spriteInductionResult=sample)
-		# t = g.buildGenericTheory(sample)
-		# embed()
-
-		newsubgoal = random.choice(rle._game.sprite_groups['box2'])
-		pos = rle._rect2pos(newsubgoal.rect)
-		writeTheoryToTxt(rle,hypotheses[0], "./examples/gridphysics/textTheorytest.py", pos)
-		embed()
-		## Select hypothesis according to whichever method, make new VRLE
-		## theory_to_world(hypotheses[0]) ## should write new .py file
 		print ""
 
-	# embed()
+	print "ended loop"
+	embed()
 
