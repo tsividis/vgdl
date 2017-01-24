45cb66fa8fd6a6710f5db8316dc2e14436bf0d10
Aritro Biswas <aritrobiswas@Aritros-MacBook-Pro.local>
2017-01-24 14:14:27 -0500

    with spriteInduction being a single function both for human play and rle play

--- vgdl/ontology.py
+++ vgdl/ontology.py
@@ -10,6 +10,7 @@ import pygame
 from tools import triPoints, unitVector, vectNorm, oncePerStep
 from ai import AStarWorld
 from IPython import embed
+import core
 
 # ---------------------------------------------------------------------
 #     Constants
@@ -193,7 +194,9 @@ class GravityPhysics(ContinuousPhysics):
 # ---------------------------------------------------------------------
 #     Sprite types
 # ---------------------------------------------------------------------
-from core import VGDLSprite, Resource
+# from core import VGDLSprite, Resource
+VGDLSprite = core.VGDLSprite
+Resource = core.Resource
 
 class Immovable(VGDLSprite):
     """ A gray square that does not budge. """
@@ -535,7 +538,8 @@ class AStarChaser(RandomNPC): ##
 # ---------------------------------------------------------------------
 #     Avatars: player-controlled sprite types
 # ---------------------------------------------------------------------
-from core import Avatar
+# from core import Avatar
+Avatar = core.Avatar
 
 class MovingAvatar(VGDLSprite, Avatar):
     """ Default avatar, moves in the 4 cardinal directions. """
@@ -918,7 +922,8 @@ class FrostbiteIgloo(SpawnPoint, Switch):
 # ---------------------------------------------------------------------
 #     Conditional criteria
 # ---------------------------------------------------------------------
-from core import Conditional
+# from core import Conditional
+Conditional = core.Conditional
 
 class SpriteCount(Conditional):
     ops = {'equ': lambda x, y: x == y,
@@ -947,7 +952,8 @@ class OnStart(Conditional):
 # ---------------------------------------------------------------------
 #     Termination criteria
 # ---------------------------------------------------------------------
-from core import Termination
+# from core import Termination
+Termination = core.Termination
 
 class Timeout(Termination):
     def __init__(self, limit=0, win=False):
@@ -1557,59 +1563,63 @@ def sampleFromDistribution(curr_distribution, all_objects):
 
     return sample
 
-def spriteInduction(rle, step):
+def spriteInduction(game, step):
+    """
+    game = a BasicGame object
+    """
     sprite_types = [Immovable, Passive, Resource, ResourcePack, RandomNPC, Chaser, AStarChaser, OrientedSprite, Missile]
 
     if step==0:
     ## Prep for sprite induction
 
-        for sprite in rle._game.getObjects():
-            rle._game.spriteDistribution[sprite] = initializeDistribution(sprite_types) # Indexed by object ID
-            rle._game.movement_options[sprite] = {"OTHER":{}}
+        for sprite in game.getObjects():
+            game.spriteDistribution[sprite] = initializeDistribution(sprite_types) # Indexed by object ID
+            game.movement_options[sprite] = {"OTHER":{}}
             for sprite_type in sprite_types:
-                rle._game.movement_options[sprite][sprite_type] = {}
+                game.movement_options[sprite][sprite_type] = {}
 
     elif step==1:
 
         ## Sprite Induction Part 1:
         ## every time you act, make sure there aren't new object
         ## if there are, update spriteDistribution etc.
-        objects = rle._game.getObjects()
+        objects = game.getObjects()
         for sprite in objects:
-            if sprite not in rle._game.spriteDistribution:
-                rle._game.all_objects[sprite] = objects[sprite]
-                rle._game.spriteDistribution[sprite] = initializeDistribution(sprite_types) # Indexed by object ID
-                rle._game.movement_options[sprite] = {"OTHER":{}}
+            if sprite not in game.spriteDistribution:
+                game.all_objects[sprite] = objects[sprite]
+                game.spriteDistribution[sprite] = initializeDistribution(sprite_types) # Indexed by object ID
+                game.movement_options[sprite] = {"OTHER":{}}
                 for sprite_type in sprite_types:
-                    rle._game.movement_options[sprite][sprite_type] = {}
+                    game.movement_options[sprite][sprite_type] = {}
 
+    elif step == 2:
         ## See the update options for each sprite type the sprite could be
-        objects = rle._game.getObjects()
-        game = rle._game                                               # Save game state
-        for sprite in rle._game.spriteDistribution.keys():                  # Keys are the IDs of the game objects
-            for sprite_type in rle._game.spriteDistribution[sprite].keys(): # Check each potential sprite type                    
-                if rle._game.spriteDistribution[sprite][sprite_type] > 0 and sprite in objects.keys():    # Make sure sprite_type is an option for sprite, and sprite is not killed
+        objects = game.getObjects()
+        game = game                                               # Save game state
+        for sprite in game.spriteDistribution.keys():                  # Keys are the IDs of the game objects
+            for sprite_type in game.spriteDistribution[sprite].keys(): # Check each potential sprite type                    
+                if game.spriteDistribution[sprite][sprite_type] > 0 and sprite in objects.keys():    # Make sure sprite_type is an option for sprite, and sprite is not killed
                     sprite_obj = objects[sprite]["sprite"]
 
                     # Get potential next positions for sprite if it were that sprite type
                     # TODO: Implement Avatar updateOptions function (if desired)
                     if sprite_obj.name != 'avatar':
-                        rle._game.movement_options[sprite][sprite_type] = updateOptions(game, sprite_type, sprite_obj) 
+                        game.movement_options[sprite][sprite_type] = updateOptions(game, sprite_type, sprite_obj) 
                         # print sprite_obj.name, sprite_type # For debugging
                         # print movement_options[sprite][sprite_type]
 
-        rle._game.collision_objects = set()
-    elif step==2:
+        game.collision_objects = set()
+    elif step==3: # TODO: remember to change all instances of 2s in function calls to 3s!
         ## Sprite Induction Part 2: Update sprite distribution based on observations
-        objects = rle._game.getObjects()
-        for sprite in rle._game.spriteDistribution.keys():        # Keys are the IDs of the game objects
+        objects = game.getObjects()
+        for sprite in game.spriteDistribution.keys():        # Keys are the IDs of the game objects
             if sprite in objects.keys():                # Sprite may have been killed
                 sprite_obj = objects[sprite]["sprite"] 
 
-                if sprite not in rle._game.collision_objects and sprite_obj.name != 'avatar':
+                if sprite not in game.collision_objects and sprite_obj.name != 'avatar':
 
                     outcome = objects[sprite]["position"]
-                    rle._game.spriteDistribution = updateDistribution(sprite, rle._game.spriteDistribution, rle._game.movement_options, outcome)
+                    game.spriteDistribution = updateDistribution(sprite, game.spriteDistribution, game.movement_options, outcome)
 
 def selectSubgoal(rle, method):
     if method=='random':
