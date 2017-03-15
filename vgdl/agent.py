9e6165841aca6fceb8f309ebce39c488d25883d5
Pedro <tsividis@mit.edu>
2017-03-15 15:23:46 -0400

    representing killerObjects. Storing list of all events across all episodes. Completing hypotheses when not playing first round -- now we can see new objects in new episodes.

--- vgdl/agent.py
+++ vgdl/agent.py
@@ -18,13 +18,14 @@ class Agent:
 		self.hypotheses = []
 		self.symbolDict = None
 		self.knownColors = []
-		self.goalColor = None
+		self.goalColor = []
+		self.killerObjects = []
 		self.finalEventList = []
 		self.plannerType = plannerType
 		self.initializeEnvironment(gameFilename)
 	
 	def initializeEnvironment(self, gameFilename):
-		self.gameString, self.levelString = defInputGame(gameFilename, randomize=True)
+		self.gameString, self.levelString = defInputGame(gameFilename, randomize=False)
 		# self.rleCreateFunc = lambda: createRLInputGame(gameFilename)
 		# rle = self.rleCreateFunc()
 		self.rleCreateFunc = lambda: createRLInputGameFromStrings(self.gameString, self.levelString)
@@ -41,7 +42,7 @@ class Agent:
 
 	def initializeHypotheses(self, rle, allObjects, learnSprites=True):
 		if learnSprites:
-			observe(rle, 5)
+			observe(rle, 10)
 			spriteTypeHypothesis = sampleFromDistribution(rle._game.spriteDistribution, allObjects)
 			gameObject = Game(spriteInductionResult=spriteTypeHypothesis)
 			initialTheory = gameObject.buildGenericTheory(spriteTypeHypothesis)
@@ -56,15 +57,17 @@ class Agent:
 		## one chunk.
 
 		self.symbolDict = generateSymbolDict(rle)
-		
-		# ##TODO: You may not need to initialize the VRLE here.
-		# game, level, immovables = writeTheoryToTxt(rle, self.hypotheses[0], self.symbolDict,\
-		#  	"./examples/gridphysics/theorytest.py")
-
-		# Vrle = createMindEnv(game, level, output=False)
-		# Vrle.immovables = immovables
 
 		return gameObject
+
+	def completeHypotheses(self, rle, allObjects):
+		observe(rle, 10)
+		spriteTypeHypothesis = sampleFromDistribution(rle._game.spriteDistribution, allObjects)
+		gameObject = Game(spriteInductionResult=spriteTypeHypothesis)
+		newHypotheses = []
+		for hypothesis in self.hypotheses:
+			newHypotheses.append(gameObject.addNewObjectsToTheory(hypothesis, spriteTypeHypothesis))
+		self.hypotheses = newHypotheses
 
 	def getSpriteColor(self, sprite):
 		return colorDict[str(sprite.color)]
@@ -77,16 +80,18 @@ class Agent:
 		
 		epsilon = .1
 		## With probability 1-epsilon, select known goal if it's known, otherwise unkown object.
-		if self.goalColor and random.random()>epsilon:
-			key = [k for k in rle._game.sprite_groups.keys() if self.getSpriteNameColor(k, rle) == self.goalColor][0]
+		if len(self.goalColor)>0 and \
+		len([k for k in rle._game.sprite_groups.keys() if self.getSpriteNameColor(k, rle) in self.goalColor])>0 and \
+		random.random()>epsilon:
+			key = random.choice([k for k in rle._game.sprite_groups.keys() if self.getSpriteNameColor(k, rle) in self.goalColor])
 			objectGoal = rle._game.sprite_groups[key][0]
 			# actualGoal = objectGoal
 			# objectGoalLocation = rle._rect2posFlipCoords(objectGoal.rect)
-			print "goal is known:", self.goalColor
+			print "Selecting from known goals", list(set(self.goalColor))
 			print ""
 		else:
 			try:
-				objectGoal = selectObjectGoal(rle, unknownColors, allColors, method="random_then_nearest")
+				objectGoal = selectObjectGoal(rle, unknownColors, allColors, self.killerObjects, method="random_then_nearest")
 				print ""
 			except:
 				print "no unknown objects and no goal? Embedding so you can debug."
@@ -97,17 +102,18 @@ class Agent:
 
 	def initializeVrle(self, hypothesis, objectGoalLocation, rle):
 		## World in agent's head given 'hypothesis', including object goal
-		gameString, levelString, symbolDict, immovables = writeTheoryToTxt(rle, hypothesis, self.symbolDict,\
+		gameString, levelString, symbolDict, immovables, killerObjects = writeTheoryToTxt(rle, hypothesis, self.symbolDict,\
 				 "./examples/gridphysics/theorytest.py", objectGoalLocation)
 
 		print "Initializing mental theory *with* object goal"
 		Vrle = createMindEnv(gameString, levelString, output=True)
-		Vrle.immovables = immovables
+		Vrle.immovables, Vrle.killerObjects = immovables, killerObjects
 		return Vrle
 
 	def VrleInitPhase(self, objectGoalLocation, rle):
 		## Initialize multiple VRLEs, each corresponding to one hypothesis in self.hypotheses
 		VRLEs = []
+		print "in VrleInitPhase.", len(self.hypotheses), "hypotheses"
 		for hypothesis in self.hypotheses:
 			VRLEs.append(self.initializeVrle(hypothesis, objectGoalLocation, rle))
 		return VRLEs
@@ -119,12 +125,15 @@ class Agent:
 
 		gameObject = None
 		for i in range(numEpisodes):
+			print "Starting episode", i
 			self.pickNewLevel(index=i)
-			gameObject, won, statesEncountered = self.playEpisode(gameObject, finalEventList)
+			gameObject, won, eventList, statesEncountered = self.playEpisode(gameObject, finalEventList)
+			finalEventList.extend(eventList)
 			VGDLParser.playGame(self.gameString, self.levelString, statesEncountered, persist_movie=True, make_images=True, make_movie=False, movie_dir="videos/"+self.gameName, padding=10)
 			totalStatesEncountered.append(statesEncountered)
 			tally.append(won)
 			print "Episode ended. Won:", won
+			print "Have won", sum(tally), "out of", len(tally), "episodes"
 		
 		print "Won", sum(tally), "out of ", len(tally), "episodes."
 		makeVideo(movie_dir="videos/"+self.gameName)
@@ -139,7 +148,9 @@ class Agent:
 		## Initialize external environment
 		rle = self.rleCreateFunc()
 		allObjects= rle._game.getObjects()
-		allColors = [colorDict[str(rle._game.sprite_groups[k][0].color)] for k in rle._game.sprite_groups.keys()]# if k!='DARKBLUE']
+		# allColors = [colorDict[str(rle._game.sprite_groups[k][0].color)] for k in rle._game.sprite_groups.keys()]
+		##select only non-moving objects as goals. Avoids chasing, which takes forever at the moment.
+		allColors = [colorDict[str(rle._game.sprite_groups[k][0].color)] for k in rle._game.sprite_groups.keys() if rle._game.sprite_groups[k][0].is_static]
 		allColors = [c for c in allColors if c!='DARKBLUE']
 		unknownColors = [c for c in allColors if c not in self.knownColors]
 
@@ -147,15 +158,17 @@ class Agent:
 		print "Known colors:", self.knownColors
 
 		ended, won = rle._isDone()
+		## Start storing encountered states.
 		totalStatesEncountered = [rle._game.getFullState()]
 
-		## Start storing encountered states.
+
+		## initialize theory if necessary.
+		if len(self.hypotheses) == 0:
+			gameObject = self.initializeHypotheses(rle, allObjects, learnSprites=True)
+		else:
+			gameObject = self.completeHypotheses(rle, allObjects)
 
 		while not ended:
-
-			## initialize theory if necessary.
-			if len(self.hypotheses) == 0:
-				gameObject = self.initializeHypotheses(rle, allObjects, learnSprites=True)
 
 			## select explore / exploit goal
 			objectGoal, objectGoalLocation = self.objectSelectionPhase(unknownColors, allColors, rle)
@@ -176,8 +189,13 @@ class Agent:
 			ended, won = rle._isDone()
 
 			if won:
-				self.goalColor = finalEventList[-1]['effectList'][0][1]
-
+				for event in finalEventList[-1]['effectList']:
+					if event[2]=='DARKBLUE':
+						self.goalColor.append(event[1])
+			else:
+				for event in finalEventList[-1]['effectList']:
+					if event[0]=='killSprite' and event[1]=='DARKBLUE':
+						self.killerObjects.append(event[2])
 			## update knowns/unknowns
 			for col in candidateNewColors:
 				if col not in self.knownColors:
@@ -186,7 +204,7 @@ class Agent:
 			print "updated known colors", self.knownColors
 			unknownColors = [c for c in unknownColors if c not in self.knownColors]
 			print "updated unknownColors", unknownColors
-		return gameObject, won, totalStatesEncountered
+		return gameObject, won, finalEventList, totalStatesEncountered
 
 if __name__ == "__main__":
 	# filename = "examples.gridphysics.simpleGame_resourceTest"
@@ -194,16 +212,28 @@ if __name__ == "__main__":
 	# filename = "examples.gridphysics.simpleGame_preconditions" ## won't work until eventHandling() is corrected.
 	# filename = "examples.gridphysics.simpleGame_inductionTest"
 	# filename = "examples.gridphysics.simpleGame_missile2"	
-	filename = "examples.gridphysics.movers5"	
-	# filename = "examples.gridphysics.simpleGame_many_poisons"
+	# filename = "examples.gridphysics.movers2d"	
+	# filename = "examples.gridphysics.movers3c"	
+	# filename = "examples.gridphysics.movers4"	
+
+	# filename = "examples.gridphysics.simpleGame_many_poisons_big"
+	# filename = "examples.gridphysics.simpleGame_push_boulders2"
 	# filename = "examples.gridphysics.pushtest"
+	# filename = "examples.gridphysics.simpleGame_small"
 	# filename = "examples.gridphysics.simpleGame_teleport"	
+	# filename = "examples.gridphysics.new_object_test"	
+	filename = "examples.gridphysics.push_boulders_multigoal_incremental"	
+
+
+	# filename = "examples.gridphysics.movers5"	
+	# filename = "examples.gridphysics.simpleGame_push_boulders_multigoal"	
+
 	plannerType = "QLearning"
 	# plannerType = "AStar"
 	print ""
 	print "Playing {} with {}".format(filename, plannerType)
 	agent = Agent(filename, plannerType)
 	t1 = time.time()
-	numEpisodes = 5
+	numEpisodes = 10
 	agent.playMultipleEpisodes(numEpisodes)
 	print "Ended {} episodes of {} with planner {} in {} seconds".format(numEpisodes, filename, plannerType, time.time()-t1)
