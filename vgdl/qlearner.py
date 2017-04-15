9fce54236ac5bbc0e69ac8a8b6d412b2f1e4a472
Pedro <tsividis@mit.edu>
2017-04-15 18:46:59 -0400

    Now uses planner class.

--- vgdl/qlearner.py
+++ vgdl/qlearner.py
@@ -1,244 +1,24 @@
-import numpy as np
-from numpy import zeros
-import pygame    
-from ontology import BASEDIRS
-from core import VGDLSprite, colorDict, sys
-from stateobsnonstatic import StateObsHandlerNonStatic 
-from rlenvironmentnonstatic import *
-import argparse
-import random
-from IPython import embed
-import math
-from threading import Thread
-from collections import defaultdict, deque
-import time
-import copy
-from threading import Lock
-from Queue import Queue
-import multiprocessing
-from ontology import Immovable, Passive, Resource, ResourcePack, RandomNPC, Chaser, AStarChaser, OrientedSprite, Missile
-from ontology import initializeDistribution, updateDistribution, updateOptions, sampleFromDistribution, spriteInduction, selectObjectGoal
-from theory_template import TimeStep, Precondition, InteractionRule, TerminationRule, TimeoutRule, SpriteCounterRule, MultiSpriteCounterRule, \
-generateSymbolDict, ruleCluster, Theory, Game, writeTheoryToTxt, generateTheoryFromGame
-from rlenvironmentnonstatic import createRLInputGame
-import curses
+from planner import *
 
-#A hack to display things to the terminal conveniently.
-np.core.arrayprint._line_width=250
 
-ACTIONS = {(0,0):'stay',(0,-1):'up', (0,1):'down', (1,0):'right', (-1,0):'left', None:'none'}
-
-class QLearner:
-	def __init__(self, rle, gameString, levelString, episodes=100, memory=None, alpha=1, epsilon=.1, gamma=.9):
-		self.rle = rle
-		self.gameString = gameString
-		self.levelString = levelString
+class QLearner(Planner):
+	def __init__(self, rle, gameString, levelString, gameFilename, display, episodes=100, memory=None, \
+		alpha=1, epsilon=.1, gamma=.9, partitionWeights = [20,1, 1], stepLimit=100, anneal=False):
+		Planner.__init__(self, rle, gameString, levelString, gameFilename, display)
 		self.alpha = alpha
 		self.epsilon = epsilon
 		self.gamma = gamma
 		self.episodes = episodes
 		self.actions = [(0,0), (1,0), (-1,0), (0,1), (0,-1)]
-		# self.actions = [(1,0), (-1,0), (0,1), (0,-1)]
 		self.QVals = defaultdict(lambda:0)
 		self.memory = memory ## provide dicts of Q-values from previous runs. Use some function to smoothe
-		self.maxPseudoReward = 10
+		self.maxPseudoReward = 1
 		self.pseudoRewardDecay = .99
-		self.partitionWeights = [20,1]
+		self.partitionWeights = partitionWeights
+		self.stepLimit=stepLimit
 		self.heuristicDecay = .99
-		self.immovables = []
-		goalLoc = self.findObjectInRLE(rle, 'goal')
-		self.rewardDict = {goalLoc:self.maxPseudoReward}
-		self.scanDomainForMovementOptions()
-		# self.getSubgoals(3)
-		self.propagateRewards(goalLoc)
-
-	def scanDomainForMovementOptions(self):
-		##TODO: Take a state, so that you can re-perform this scan as needed and take changes into account.
-		##TODO: query VGDL description for penetrable/nonpenetrable objects, add to list.
-		immovable_codes = []
-		# immovables = ['wall']
-		try:
-			immovables = self.rle.immovables
-			self.immovables = immovables
-			print "immovables", immovables
-		except:
-			immovables = ['wall']#, 'poison']
-			self.immovables = immovables
-			print "Using defaults as immovables", immovables
-
-		for i in immovables:
-			if i in self.rle._obstypes.keys():
-				immovable_codes.append(2**(1+sorted(self.rle._obstypes.keys())[::-1].index(i)))
-
-		actionDict = defaultdict(list)
-		neighborDict = defaultdict(list)
-		# action_superset = [(0,0),(-1,0), (1,0), (0,-1), (0,1)]
-		action_superset = [(-1,0), (1,0), (0,-1), (0,1)]
-		
-		board = np.reshape(self.rle._getSensors(), self.rle.outdim)
-		y,x=np.shape(board)
-		for i in range(y):
-			for j in range(x):
-				if board[i,j] not in immovable_codes:
-					for action in action_superset:
-						nextPos = (i+action[1], j+action[0])
-						## Don't look at positions off the board.
-						if 0<=nextPos[0]<y and 0<=nextPos[1]<x:
-							if board[nextPos] not in immovable_codes:
-								actionDict[(i,j)].append(action)
-								neighborDict[(i,j)].append(nextPos)
-		self.actionDict = actionDict
-		self.neighborDict = neighborDict
-		return
-	
-	def propagateRewards(self, goalLoc):
-		rewardQueue = deque()
-		processed = [goalLoc]
-		rewardQueue.append(goalLoc)
-		for n in self.neighborDict[goalLoc]:
-			if n not in rewardQueue:
-				rewardQueue.append(n)
-
-		while len(rewardQueue)>0:
-			loc = rewardQueue.popleft()
-			if loc not in processed:
-				valid_neighbors = [n for n in self.neighborDict[loc] if n in self.rewardDict.keys()]
-				self.rewardDict[loc] = max([self.rewardDict[n] for n in valid_neighbors]) * self.pseudoRewardDecay
-				processed.append(loc)
-				for n in self.neighborDict[loc]:
-					if n not in processed:
-						rewardQueue.append(n)
-		return
-
-	def findObjectInRLE(self, rle, objName):
-		if objName not in rle._obstypes.keys():
-			print objName, "not in rle."
-			return None
-		objCode = 2**(1+sorted(self.rle._obstypes.keys())[::-1].index(objName))
-		objLoc = np.where(np.reshape(self.rle._getSensors(), self.rle.outdim)==objCode)
-		objLoc = objLoc[0][0], objLoc[1][0] #(y,x)
-		return objLoc
-	
-	def findAvatarInRLE(self, rle):
-		avatar_code = 1
-		state = np.reshape(rle._getSensors(), self.rle.outdim)
-		if avatar_code in state:
-			avatar_loc = np.where(state==avatar_code)
-			avatar_loc = avatar_loc[0][0], avatar_loc[1][0]
-		else:
-			avatar_loc = None
-		return avatar_loc
-
-	def findAvatarInState(self, s):
-		## takes a string representation of a state, returns avatar location
-		state = np.reshape(np.fromstring(s,dtype=float), self.rle.outdim)
-		avatar_code = 1
-		if avatar_code in state:
-			avatar_loc = np.where(state==avatar_code)
-			avatar_loc = avatar_loc[0][0], avatar_loc[1][0]
-		else:
-			avatar_loc = None
-		return avatar_loc
-
-	def findObjectInState(self, s, objName):
-		##TODO: Finish last part of this function -- sometimes it can't access objloc[0][0], objloc[1][0]
-		state = np.reshape(np.fromstring(s,dtype=float), self.rle.outdim)
-		if objName not in rle._obstypes.keys():
-			print objName, "not in rle."
-			return None
-		objCode = 2**(1+sorted(self.rle._obstypes.keys())[::-1].index(objName))
-		objLoc = np.where(state==objCode)
-		try:
-			objLoc = objLoc[0][0], objLoc[1][0] #(y,x)
-		except:
-			print "can't find objloc"
-			embed()
-		return objLoc
-
-	def getPathToGoal(self, avatar_loc, goal_loc):
-
-		q = deque()
-		# q stores tuples in which the first element is a node and the next
-		# is the shortest path to that node
-		q.append((avatar_loc, []))
-		node = avatar_loc
-		path = []
-		seen = {avatar_loc}
-		while q:
-			node, path = q.popleft()
-			seen.add(node)
-			if node == goal_loc:
-				break
-
-			for neighbor in self.neighborDict[node]:
-				if not neighbor in seen:
-					q.append((neighbor, path + [neighbor]))
-
-		if node != goal_loc:
-			print "didn't find path to goal in getSubgoals (in getPathToGoal)"
-			return False
-			# embed()
-			# raise Exception("Didn't find a path to the goal location.")
-		return path
-
-	def getSubgoals(self, subgoal_path_threshold):
-		avatar_loc = self.findAvatarInRLE(self.rle)
-		## find location of goal, add to rewardDict.
-		## also add neighbors of goal rewardQueue.
-		##TODO: update this if goal moves!!
-		if "goal" not in self.rle._obstypes.keys():
-			print "no goal to get subgoals to"
-			return []
-		goal_code = 2**(1+sorted(self.rle._obstypes.keys())[::-1].index("goal"))
-		killerObjectCodes = []
-		for o in self.rle.killerObjects:
-			if o in self.rle._obstypes.keys():
-				killerObjectCodes.append(2**(1+sorted(self.rle._obstypes.keys())[::-1].index(o)))
-		board = np.reshape(self.rle._getSensors(), self.rle.outdim)
-		goal_loc = np.where(board==goal_code)
-		goal_loc = goal_loc[0][0], goal_loc[1][0]
-		self.subgoals = []
-		# print "showing RLE we're getting path for."
-		# print self.rle.show()
-		path = self.getPathToGoal(avatar_loc, goal_loc)
-
-		if not path:
-			return [] ## so that you can try pursuing a different goal
-
-		if subgoal_path_threshold > len(path):
-			# don't use any subgoals in this case.
-			return [goal_loc]
-		else:
-			subgoal_index = -1
-			num_subgoals = int(math.ceil(float(len(path))/subgoal_path_threshold))
-			for i in range(num_subgoals):
-				if i < len(path) % num_subgoals:
-					subgoal_index += (len(path)/num_subgoals + 1)
-				else:
-					subgoal_index += len(path)/num_subgoals
-
-
-				## Doesn't add subgoals that correspond to objects we know to be dangerous.
-				if board[path[subgoal_index][0]][path[subgoal_index][1]] not in killerObjectCodes:
-					self.subgoals.append(path[subgoal_index])
-				else:
-					for i in range(1, subgoal_path_threshold):
-						# print "path fell on a killer object. changing path slightly."
-						try:
-							if path[subgoal_index-i] not in killerObjectCodes:
-								self.subgoals.append(path[subgoal_index-i])
-								# print "found altered path", path[subgoal_index-i]
-								break
-							elif path[subgoal_index+i] not in killerObjectCodes:
-								self.subgoals.append(path[subgoal_index+i])
-								# print "found altered path", path[subgoal_index+i]
-								break
-						except:
-							print "indices didn't work out in looking for different path"
-				# self.subgoals.append(path[subgoal_index])
-
-		
-		return self.subgoals
+		self.anneal=anneal
+		self.printSummary()
 
 	def selectAction(self, s, policy, partitionWeights = None, domainKnowledge=True, printout=False):
 		if policy == 'epsilonGreedy':
@@ -248,7 +28,7 @@ class QLearner:
 				bestQVal, bestA, QValsAreAllEqual = self.bestSA(s, partitionWeights, domainKnowledge = True)
 				return bestA
 		elif policy == 'greedy':
-			bestQVal, bestA, QValsAreAllEqual = self.bestSA(s, partitionWeights = [1,0], domainKnowledge = True)
+			bestQVal, bestA, QValsAreAllEqual = self.bestSA(s, partitionWeights = [1,0,0], domainKnowledge = True)
 			if printout:
 				print bestQVal
 			if QValsAreAllEqual:
@@ -256,19 +36,16 @@ class QLearner:
 			else:
 				return bestA
 
-	def getPseudoReward(self, s, a):
-		## returns pseudoreward of taking action a from location currentLoc.
-		## gives pseudoReward[currentLoc] if a doesn't move states.
-
-		currentLoc = self.findAvatarInState(s)
-		if currentLoc:
-			nextLoc = currentLoc[0]+a[1], currentLoc[1]+a[0] #again, locations are (y,x) and actions are (x,y)
-		else:
-			return 0.
-		if nextLoc in self.rewardDict.keys():
-			return self.rewardDict[nextLoc]
-		elif currentLoc in self.rewardDict.keys():
-			return self.rewardDict[currentLoc]
+	def getAvoidanceScores(self, s, a):
+		decay=.9
+		avatarLoc = self.findAvatarInState(s)
+		if avatarLoc:
+			enemyLocs = []
+			for enemy in self.killerObjects:
+				enemyLocs.extend(self.findObjectsInState(s, enemy))
+			nextLoc = avatarLoc[0]+a[1], avatarLoc[1]+a[0]
+			penalties = [decay**manhattanDist(nextLoc, e) for e in enemyLocs]
+			return -np.mean(penalties)
 		else:
 			return 0.
 
@@ -288,11 +65,15 @@ class QLearner:
 		maxFuncVal = -float('inf')
 		sumQVal = 0.
 		sumPseudoReward = 0.
+		sumAvoidanceScores = 0.
 		rewardCoefficient = partitionWeights[0]
 		heuristicCoefficient = partitionWeights[1]
+		avoidanceCoefficient = partitionWeights[2]
 		# print heuristicCoefficient
 		bestAction = None
 		QValsAreAllEqual = False
+
+		# print self.rle.show()
 		if debug:
 			print "debugging bestSA"
 			embed()
@@ -301,6 +82,7 @@ class QLearner:
 				self.QVals[(s,a)] = 0.
 			sumQVal += abs(self.QVals[(s,a)])
 			sumPseudoReward += self.getPseudoReward(s, a)
+			sumAvoidanceScores += abs(self.getAvoidanceScores(s,a))
 
 		for a in actions:
 
@@ -310,13 +92,24 @@ class QLearner:
 				QValFunction = self.QVals[(s,a)]/sumQVal
 
 			if sumPseudoReward == 0:
-				pseudoRewardFunction =0.
+				pseudoRewardFunction = 0.
 			else:
 				pseudoRewardFunction = self.getPseudoReward(s,a)/sumPseudoReward
 
-			funcVal = rewardCoefficient*QValFunction + \
-						heuristicCoefficient*pseudoRewardFunction
+			if sumAvoidanceScores == 0:
+				avoidanceFunction = 0.
+			else:
+				avoidanceFunction = self.getAvoidanceScores(s,a)/sumAvoidanceScores
 
+			# print a
+			# print QValFunction, pseudoRewardFunction, avoidanceFunction
+			# print rewardCoefficient*QValFunction, heuristicCoefficient*pseudoRewardFunction, avoidanceCoefficient*avoidanceFunction
+			
+			funcVal = rewardCoefficient*QValFunction + \
+						heuristicCoefficient*pseudoRewardFunction + \
+						avoidanceCoefficient*avoidanceFunction
+
+			# print funcVal
 			if funcVal > maxFuncVal:
 				maxFuncVal = funcVal
 				bestAction = a
@@ -332,78 +125,79 @@ class QLearner:
 				print np.reshape(np.fromstring(s,dtype=float),self.rle.outdim)
 				embed()
 
-		# QVals = [self.QVals[(s,a)] for a in actions]
-		# if len(QVals)>0:
-		# 	bestQVal = max(QVals)
-		# 	QValsAreAllEqual = bestQVal == min(QVals)
-		# 	bestA = actions[QVals.index(bestQVal)]
-		# else:
-		# 	bestQVal, QValsAreAllEqual, bestA = 0., True, None
+		# embed()
 		return bestQVal, bestAction, QValsAreAllEqual
 
 	def update(self, s, a, sPrime, r):
 		bestQVal, bestA, QValsAreAllEqual = self.bestSA(sPrime, self.partitionWeights)
 		bestQVal
-		# if (s,a) in self.QVals.keys():
 		try:
 			self.QVals[(s,a)] = self.QVals[(s,a)] + self.alpha * (r + self.gamma*bestQVal - self.QVals[(s,a)])
-		# else:
-		# 	self.QVals[(s,a)] = 0 + self.alpha * (r + self.gamma*bestQVal - self.QVals[(s,a)])
 		except:
 			print "didn't find qvals[s,a]"
 			embed()
 
-	def runEpisode(self, stepLimit=float('inf')):
+	def runEpisode(self):
 		rle = copy.deepcopy(self.rle)
 		terminal = rle._isDone()[0]
 		s = rle._getSensors().tostring()
 		i=0
 		total_reward = 0.
 
-		while not terminal and i<stepLimit:
+		while not terminal and i<self.stepLimit:
+			
+
 			a = self.selectAction(s, policy='epsilonGreedy', partitionWeights = self.partitionWeights)
 			res = rle.step(a)
 			sPrime, r = res['observation'].tostring(), res['reward']
+			if rle._isDone()[1]==True:
+				r = 100
 
 			## UNCOMMENT HERE IF YOU WANT TO WATCH Q-learner learning.
-			# print r
-			# print rle.show()
-			if r==1:
+			if self.display:
+				print rle.show()
+
+			## lower epsilon once you've found winning states.
+			if self.anneal and rle._isDone()[1]:
 				self.partitionWeights[1] = self.partitionWeights[1]*self.heuristicDecay
+				self.partitionWeights[2] = self.partitionWeights[2]*self.heuristicDecay
 				self.epsilon = self.epsilon*self.heuristicDecay
+
 				# print self.partitionWeights
 				# print 'reward'
+
 			self.update(s,a,sPrime,r)
 			s = sPrime
 			terminal = rle._isDone()[0]
+
 			i += 1
 			total_reward += r
-		# print total_reward
-		# print "reset"
-		# print ""
+
 		self.QVals[s] = 0.
 
-	def learn(self, episodes, satisfice=0):
+	def learn(self, satisfice=0):
 		t1 = time.time()
 		satisfice_episodes = 0
-		for i in range(episodes):
+		for i in range(self.episodes):
 			# sys.stdout.write("Episodes: {}\r".format(i) )
 			# sys.stdout.flush()
-			self.runEpisode(stepLimit=100)
+			self.runEpisode()
 			satisfice_episodes +=1
 			if i%10==0:
 				s = self.rle._getSensors().tostring()
-				a = self.selectAction(s, policy='epsilonGreedy', partitionWeights = self.partitionWeights)
+				a = self.selectAction(s, policy='greedy', partitionWeights = self.partitionWeights)
 				print i, self.QVals[(s,a)]
 				if satisfice: ## see if values have propagated to start state; if so, return.
 					actions = self.getBestActionsForPlayout()
 					if len(actions)>0:
 						if satisfice_episodes>satisfice:
 							return i
-						
-						# print "satisfice found actions in", time.time()-t1, "seconds."
-						# return i
-
+			if i%100==0:
+				self.getBestActionsForPlayout(False, True)
+		time_elapsed = time.time()-t1
+		self.printSummary()
+		print "Time to solution: {}".format(time_elapsed)
+		print ""
 		return i
 
 	def getBestActionsForPlayout(self, aggressive=False, showActions = False):
@@ -413,15 +207,17 @@ class QLearner:
 		actions = []
 		# print rle.show()
 		while not terminal:
-			a = self.selectAction(s, policy='greedy', partitionWeights = None, domainKnowledge = None, printout = False)
+			a = self.selectAction(s, policy='greedy', partitionWeights = [20,0,0], domainKnowledge = None, printout = False)
 			# print self.QVals[(s,a)]
 			if aggressive:
 				if a is None:
-					return actions
+					break
+					# return actions
 			else:
 				if a is None or self.QVals[(s,a)]<=0:
 					# print "Negative q-values or no action. Breaking."
-					return actions
+					break
+					# return actions
 			actions.append(a)
 			res = rle.step(a)
 			if showActions:
@@ -439,30 +235,52 @@ class QLearner:
 				print np.reshape(np.fromstring(l[0][0],dtype=float),self.rle.outdim)
 				print l[1]
 
+	def printSummary(self):
+		print ""
+		print "Game: {}".format(self.gameFilename)
+		print "Immovables: {}".format(self.immovables)
+		print "Enemies: {}".format(self.killerObjects)
+		print "Parameters: Epsilon: {}. Gamma: {}. PartitionWeights: {}".format(self.epsilon, self.gamma, self.partitionWeights)
+		return
 if __name__ == "__main__":
 	
 	# gameFilename = "examples.gridphysics.simpleGame_push_boulders_multigoal"
 	# gameFilename = "examples.gridphysics.waypointtheory" 
-	# gameFilename = "examples.gridphysics.simpleGame_teleport"
+	# gameFilename = "examples.gridphysics.demo_teleport"
 	# gameFilename = "examples.gridphysics.movers3c"
-	gameFilename = "examples.gridphysics.scoretest" 
+	# gameFilename = "examples.gridphysics.scoretest" 
+	# gameFilename = "examples.gridphysics.portals" 
+	# gameFilename = "examples.gridphysics.pick_apples" 
+	# gameFilename = "examples.gridphysics.demo_transform_small" 
+
+	# gameFilename = "examples.gridphysics.demo_dodge" 
+	# gameFilename = "examples.gridphysics.rivercross" 
+	gameFilename = "examples.gridphysics.demo_chaser" 
 
 	gameString, levelString = defInputGame(gameFilename, randomize=True)
 	rleCreateFunc = lambda: createRLInputGame(gameFilename)
 	rle = rleCreateFunc()
-	print rle.show()
-	# rle.immovables = ['wall', 'poison1', 'poison2']
-	print ""
-	print "Initializing learner. Playing", gameFilename
-	ql = QLearner(rle, gameString, levelString, alpha=1, epsilon=.5, gamma=.9, episodes=1000)
-	# for x in range(10):
-	# 	print '{0}\r'.format(x),
-	# print
 	# embed()
-	t1 = time.time()
-	ql.learn(500, satisfice=200)
-	t2 = time.time() - t1
-	print "done in {} seconds".format(t2)
-	# ql.learn(100, satisfice=False)
+	# p = Planner(rle, gameString, levelString)
+	# embed()
+	# print rle.show()
+	# print ""
+	# print "Initializing learner. Playing", gameFilename
+	ql = QLearner(rle, gameString, levelString, gameFilename, display=True, alpha=1, epsilon=.5, gamma=.9, \
+		episodes=1000, partitionWeights=[20,1, .01], stepLimit=100, anneal=True)
+	ql.killerObjects = ['chaser']
+	# # ql.immovables = ['wall', 'poison']
 
+	# t1 = time.time()
+	ql.learn(satisfice=0)
 	embed()
+	# print max([ql.QVals[k] for k in ql.QVals.keys()])
+
+	# time_elapsed = time.time()-t1
+	# print "Game: {}".format(gameFilename)
+	# print "Ran {} rounds in {} seconds".format(ql.episodes, time_elapsed)
+	# print "Immovables: {}".format(ql.immovables)
+	# print "Parameters: Epsilon: {}. Gamma: {}. PartitionWeights: {}".format(ql.epsilon, ql.gamma, ql.partitionWeights)
+	# print ""
+
+	# embed()
