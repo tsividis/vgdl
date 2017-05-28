a8a2a7ba7fb942e134e88801b681abdb9e039f66
joaoloula <joao.campos-loula@polytechnique.edu>
2017-05-28 15:54:51 -0400

    progress on running agent on explore/exploit curriculum

--- examples/gridphysics/theorytest.py
+++ examples/gridphysics/theorytest.py
@@ -1,54 +1,102 @@
 level="""
-44444444444444444444444444444444
-4                              4
-4                              4
-4                              4
-4  6                           4
-4 0                            4
-4                              4
-4       5                     74
-444                            4
-44444444444444444444444444444444
+22222222222222222222222222
+21   3      4   5    6   2
+2       7       3        2
+2    0      5    6   3   2
+2  0    3    4           2
+2     8         3   7    2
+2   6    7     8         2
+2         5    6      8  2
+2   4 8   4    5   7     2
+22222222222222222222222222
 """
 game = """
 BasicGame
 	SpriteSet
-		c3 > ResourcePack color=GREEN
-		c2 > Resource color=ORANGE
+		c9 > ResourcePack color=RED
+		c8 > Resource color=ORANGE
 		avatar > MovingAvatar color=WHITE
-		c4 > ResourcePack color=BLACK
+		c3 > Resource color=BLACK
+		c2 > Resource color=GREEN
+		c7 > ResourcePack color=PINK
+		c6 > ResourcePack color=YELLOW
+		c5 > ResourcePack color=LIGHTBLUE
+		c4 > Resource color=BLUE
 	InteractionSet
-		avatar c2 > killSprite
-		c4 EOS > stepBack
-		c2 c4 > killSprite
+		c2 avatar > killSprite
 		c4 c2 > killSprite
+		c2 c4 > killSprite
+		c5 EOS > stepBack
+		c6 avatar > killSprite
+		c8 EOS > stepBack
+		c5 c3 > killSprite
+		c3 c5 > killSprite
+		c4 c3 > killSprite
+		c3 c4 > killSprite
+		c5 c2 > killSprite
+		c2 c5 > killSprite
+		c9 c3 > killSprite
+		c3 c9 > killSprite
+		c4 EOS > stepBack
+		c7 avatar > killSprite
+		c8 c2 > killSprite
+		c2 c8 > killSprite
+		avatar c3 > stepBack
+		c6 c2 > killSprite
+		c2 c6 > killSprite
+		c8 c3 > killSprite
+		c3 c8 > killSprite
+		c7 c3 > killSprite
+		c3 c7 > killSprite
+		c9 c2 > killSprite
+		c2 c9 > killSprite
+		c2 c2 > killSprite
+		c7 EOS > stepBack
+		c3 c3 > killSprite
+		c3 EOS > stepBack
+		c7 c2 > killSprite
+		c2 c7 > killSprite
+		c8 avatar > killSprite
+		c6 c3 > killSprite
+		c3 c6 > killSprite
 		c2 c3 > killSprite
 		c3 c2 > killSprite
-		c2 c2 > killSprite
-		c3 c3 > killSprite
-		c3 avatar > killSprite
+		c6 EOS > stepBack
+		c5 avatar > killSprite
+		c4 avatar > killSprite
 		c2 EOS > stepBack
-		avatar c4 > stepBack
-		c3 EOS > stepBack
-		c4 c4 > killSprite
-		c3 c4 > killSprite
-		c4 c3 > killSprite
+		c9 avatar > killSprite
+		c9 EOS > stepBack
 	TerminationSet
 		NoveltyTermination s1=c2 s2=c2 win=True
 		NoveltyTermination s1=c2 s2=c3 win=True
-		NoveltyTermination s1=c2 s2=c4 win=True
 		NoveltyTermination s1=c3 s2=c3 win=True
-		NoveltyTermination s1=c3 s2=c4 win=True
-     	NoveltyTermination s1=c4 s2=c4 win=True
+		NoveltyTermination s1=c4 s2=avatar win=True
+		NoveltyTermination s1=c4 s2=c2 win=True
+		NoveltyTermination s1=c4 s2=c3 win=True
+		NoveltyTermination s1=c5 s2=avatar win=True
+		NoveltyTermination s1=c5 s2=c2 win=True
+		NoveltyTermination s1=c5 s2=c3 win=True
+		NoveltyTermination s1=c6 s2=avatar win=True
+		NoveltyTermination s1=c6 s2=c2 win=True
+		NoveltyTermination s1=c6 s2=c3 win=True
+		NoveltyTermination s1=c7 s2=avatar win=True
+		NoveltyTermination s1=c7 s2=c2 win=True
+		NoveltyTermination s1=c7 s2=c3 win=True
+		NoveltyTermination s1=c8 s2=avatar win=True
+		NoveltyTermination s1=c8 s2=c2 win=True
+		NoveltyTermination s1=c8 s2=c3 win=True
+		NoveltyTermination s1=c9 s2=avatar win=True
+		NoveltyTermination s1=c9 s2=c2 win=True
+		NoveltyTermination s1=c9 s2=c3 win=True
 		SpriteCounter stype=avatar limit=0 win=False
-		SpriteCounter stype=c3 limit=0 win=True
+		SpriteCounter stype=c2 limit=0 win=True
 	LevelMapping
+		2 > c3
+		1 > avatar
 		0 > c2
-		3 > c4
-		2 > avatar
-		1 > c3
 		G > goal
 """
 if __name__ == "__main__":
 	from vgdl.core import VGDLParser
-	V	GDLParser.playGame(game, level)
+	VGDLParser.playGame(game, level)
--- vgdl/main_agent.py
+++ vgdl/main_agent.py
@@ -331,8 +331,8 @@ if __name__ == "__main__":
 	# filename = "examples.gridphysics.pick_apples"
 	# filename = "examples.gridphysics.expt_antagonist"
 
-	# filename = "examples.gridphysics.expt_exploration_exploitation"
-	filename = "examples.gridphysics.expt_relational"
+	filename = "examples.gridphysics.expt_exploration_exploitation"
+	# filename = "examples.gridphysics.expt_relational"
 	agent = Agent('full', filename)
 
 	##then pass this down for multiple episodes
