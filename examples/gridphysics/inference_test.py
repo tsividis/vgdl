e7a0009b4446624f047af8cb197fa23c4cad64c5
Pedro <tsividis@mit.edu>
2017-11-20 21:20:32 -0500

    Wrote some framework code that instantiates multiple RLEs, each based on a a theory that's a slight modification of the default hypothesis. Also beginning to sketch out theory-expansion code in theory_template.py

---
+++ examples/gridphysics/inference_test.py
@@ -0,0 +1,106 @@
+
+# level0 = """
+# wwwwwwwwwwwwwwwwwwwwwwwwww
+# w               f    o d w
+# w   a      A    d        w
+# w      a         o  d    w
+# w            b           w
+# w     e       d  b  c    w
+# w   o    c     ec        w
+# w         f f  o      e  w
+# w   b e   b    f d c     w
+# wwwwwwwwwwwwwwwwwwwwwwwwww
+# """
+
+
+level0 = """
+wwwwwwwwwwwww
+w   o    c  w
+w a  d   Af w
+w   b e   b w
+wwwwwwwwwwwww
+"""
+
+
+# level0 = """
+# wwwwwwwww
+# w Ab b bw
+# wwwwwwwww
+# """
+
+game0 = """
+BasicGame frame_rate=30
+    SpriteSet
+        apple > Immovable color=GREEN
+        orange > Immovable color=ORANGE
+        blueberry > Immovable color=BLUE
+        dough > Immovable color=YELLOW
+        cranberry > Immovable color=LIGHTBLUE
+        eel > Immovable color=PINK
+        fruit > Immovable color=RED
+        avatar > MovingAvatar color=DARKBLUE
+        wall > Immovable color=BLACK
+    LevelMapping
+        w > wall
+        a > apple
+        b > blueberry
+        o > orange
+        c > cranberry
+        d > dough
+        e > eel
+        f > fruit
+    InteractionSet
+        avatar wall > stepBack
+        box avatar > bounceForward
+        blueberry avatar > killSprite
+        avatar cranberry > stepBack
+        dough avatar > killSprite
+        eel avatar > killSprite
+        fruit avatar > killSprite
+        apple avatar > killSprite
+        orange avatar > killSprite
+    TerminationSet
+        SpriteCounter stype=avatar  limit=0 win=False
+        SpriteCounter stype=blueberry limit=0 win=True
+
+"""
+
+level_game_pairs = [[game0, level0]]
+
+
+if __name__ == "__main__":
+    from vgdl.core import VGDLParser
+    import random, sys, time
+    import numpy as np
+    import csv
+    from IPython import embed
+    
+    levels = [l for l in locals().keys() if 'level' in l and len(l)<8]
+
+    if len(sys.argv)==2:
+        index = int(sys.argv[1])
+        VGDLParser.playGame(*level_game_pairs[index])
+    else:
+        # index = random.choice(range(len(level_game_pairs)))
+        for index, level in enumerate(level_game_pairs):
+            wins = 0
+            while wins<1:
+                VGDLParser.playGame(*level)
+                time.sleep(1)
+                data = np.load("temp_data.npy")
+                win = data[2]
+                if win:
+                    wins+=1
+                levels_won = index*2 + wins
+                # Save in format [subect, condition, gamename, levels won, steps taken, score, time elapsed]
+                row = ['human', 'no_score', 'expt_exploration_exploitation', levels_won, data[1], data[3], data[0]]
+
+                filename = "human_data.csv"
+                f = open(filename, 'a+') ##append, but also read.
+                g = open(filename, 'r')
+                writer = csv.writer(f)
+                if len(g.readlines())==0:
+                    writer.writerow(('subject', 'condition', 'gameName', 'levels_won', 'steps', 'score', 'time'))
+                writer.writerow(row)
+                f.close()
+                g.close()
--- examples/gridphysics/theorytest.py
+++ examples/gridphysics/theorytest.py
@@ -1,76 +1,36 @@
 level="""
-3333333333333333333333
-35                   3
-3    4    0          3
-3              2     3
-3                    3
-3      2             3
-3                 0  3
-3          4         3
-3                    3
-3333333333333333333333
+555555555
+5 32 2 25
+555555555
 """
 game = """
 BasicGame
 	SpriteSet
-		avatar > MovingAvatar color=WHITE
-		c3 > Chaser color=ORANGE fleeing=True cooldown=4
-		c2 > Chaser color=BLUE fleeing=True cooldown=4
-		c5 > Chaser color=YELLOW fleeing=True cooldown=4
-		c4 > Chaser color=BLACK fleeing=True cooldown=4
+		c3 > Chaser color=BLACK fleeing=True cooldown=5
+		c2 > Chaser color=BLUE fleeing=True cooldown=5
+		avatar > MovingAvatar color=DARKBLUE
 	InteractionSet
-		c2 avatar > killSprite
-		c2 c5 > nothing
-		c5 c2 > nothing
-		c4 c5 > nothing
-		c5 c4 > nothing
-		c2 c4 > nothing
-		c4 c2 > nothing
-		c2 c3 > nothing
+		c2 avatar > bounceForward
+		c2 c3 > bounceForward
 		c3 c2 > nothing
-		c2 c2 > nothing
-		c5 avatar > killSprite
-		c3 c3 > nothing
+		c2 c2 > bounceForward
+		c3 c3 > killSprite
 		avatar EOS > stepBack
-		c3 c4 > nothing
-		c4 c3 > nothing
-		c5 c5 > nothing
-		c4 avatar > killSprite
-		c3 c5 > nothing
-		c5 c3 > nothing
-		c4 c4 > nothing
-		c3 avatar > killSprite
-		c4 EOS > stepBack
-		c5 EOS > stepBack
-		c2 EOS > stepBack
-		c3 EOS > stepBack
+		c2 EOS > undoAll
+		c3 EOS > bounceForward
+		c3 avatar > nothing
 	TerminationSet
-		NoveltyTermination s1=c2 s2=avatar win=True
-		NoveltyTermination s1=c2 s2=c2 win=True
-		NoveltyTermination s1=c2 s2=c3 win=True
-		NoveltyTermination s1=c2 s2=c4 win=True
-		NoveltyTermination s1=c2 s2=c5 win=True
 		NoveltyTermination s1=c3 s2=avatar win=True
+		NoveltyTermination s1=c3 s2=c2 win=True
 		NoveltyTermination s1=c3 s2=c3 win=True
-		NoveltyTermination s1=c3 s2=c4 win=True
-		NoveltyTermination s1=c3 s2=c5 win=True
-		NoveltyTermination s1=c4 s2=avatar win=True
-		NoveltyTermination s1=c4 s2=c4 win=True
-		NoveltyTermination s1=c4 s2=c5 win=True
-		NoveltyTermination s1=c5 s2=avatar win=True
-		NoveltyTermination s1=c5 s2=c5 win=True
 		NoveltyTermination s1=c2 s2=EOS win=True
 		NoveltyTermination s1=c3 s2=EOS win=True
-		NoveltyTermination s1=c4 s2=EOS win=True
-		NoveltyTermination s1=c5 s2=EOS win=True
 		NoveltyTermination s1=avatar s2=EOS win=True
 		SpriteCounter stype=avatar limit=0 win=False
 	LevelMapping
-		0 > c2
-		2 > c5
-		3 > c4
-		4 > c3
-		5 > avatar
+		2 > c2
+		3 > avatar
+		5 > c3
 """
 if __name__ == "__main__":
 	from vgdl.core import VGDLParser
