1b3ee172ed85227a052713575ba8225d862c6c04
Tim Menke <tim_menke@g.harvard.edu>
2017-12-01 13:04:07 -0500

    added errorMap cleaning for unique intPairs

--- vgdl/agent.py
+++ vgdl/agent.py
@@ -36,6 +36,7 @@ class errorMapEntry:
 		self.intPairs = []
 		self.culpritClasses = []
 	def display(self):
+		print ""
 		print "diagnosis: {}".format(self.diagnosis)
 		print "targetToken: {}".format(self.targetToken)
 		print "targetClass: {}".format(self.targetClass)
@@ -455,7 +456,7 @@ class Agent:
 			errorMap.append(e)
 		# 2.3) Appearance
 		for sB in appeared_sprites_envB:
-			e = errorMapEntry
+			e = errorMapEntry()
 			e.diagnosis.append('newObjectAppeared')
 			e.targetToken = sB
 			# Find class of new object by comparing colors, or give 'unknown' if unsuccessful
@@ -467,7 +468,7 @@ class Agent:
 			if sMatch==[]:
 				e.targetClass = 'unknown'
 			else:
-				e.targetClass = sMatch.name
+				e.targetClass = sMatch[0].name
 			# Find neighbors of target sprite in current time step -> could have caused appearance
 			# Simultaneously find culprit classes - a neighboring sprite could have launched the sprite due to its class
 			neighbors_curr = self.neighborsPrev(envA, envB, sB) #use this function to find neighbors in current state and not previous ("Prev" label is unnecessary)
@@ -482,6 +483,21 @@ class Agent:
 		# and if that is consistent between envA and envB
 		#TODO
 
+		## Clean errorMap: delete redundant interaction pairs under same diagnosis (only works if there is just one diagnosis per errorMapEntry)
+		dia_list = [e.diagnosis[0] for e in errorMap]
+		dia_list = list(set(dia_list))
+		for dia in dia_list:
+			errors = [e for e in errorMap if e.diagnosis[0]==dia]
+			for n,e in enumerate(errors):
+				other_pairs = []
+				[other_pairs.extend(errorMap[i].intPairs) for i in range(n+1,len(errorMap)) ]
+				# Permute tuples of other pairs to compare pairs in current error
+				other_pairs = [(p[1],p[0]) for p in other_pairs]
+				# Find unique interaction pairs for current error
+				unique_pairs_e = []
+				[unique_pairs_e.append(p) for p in e.intPairs if (p not in other_pairs)]
+				# Set interaction pairs to unique pairs
+				e.intPairs = unique_pairs_e
 
 		## NOTE: We could extend by penalizing as a function of (most likely) vgdlType and color
 		## NOTE: Use intializeHypotheses function in this file to build my test theories
@@ -624,12 +640,12 @@ class Agent:
 		testClass = theory.spriteObjects['YELLOW'].className
 		testClass2 = theory.spriteObjects['ORANGE'].className
 		for interactionRule in theory.interactionSet: #<< find rule between avatar and e.g. c3
-			if interactionRule.slot1==testClass2 and interactionRule.slot2 == 'avatar': #modified
-				interactionRule.interaction = 'killSprite' #modified
-			if interactionRule.slot1=='avatar' and interactionRule.slot2 == testClass2: #modified
-				interactionRule.interaction = 'nothing' #modified
-			# if interactionRule.slot1==testClass and interactionRule.slot2 == 'avatar': #modified
+			# if interactionRule.slot1==testClass2 and interactionRule.slot2 == 'avatar': #modified
 			# 	interactionRule.interaction = 'bounceForward' #modified
+			# if interactionRule.slot1=='avatar' and interactionRule.slot2 == testClass2: #modified
+			# 	interactionRule.interaction = 'nothing' #modified
+			if interactionRule.slot1==testClass and interactionRule.slot2 == 'avatar': #modified
+				interactionRule.interaction = 'bounceForward' #modified
 			#if interactionRule.slot1=='c2' and interactionRule.slot2 == 'avatar':
 			#	interactionRule.interaction = 'stepBack'
 				#break
@@ -950,7 +966,7 @@ class Agent:
 
 			self.hypotheses = hypotheses
 
-		embed()
+		#embed()
 		return
 
 
@@ -1057,7 +1073,7 @@ class Agent:
 			from pygame.locals import K_SPACE, K_UP, K_DOWN, K_LEFT, K_RIGHT
 
 			
-			actions = [K_LEFT, K_DOWN]
+			actions = [K_RIGHT, K_RIGHT]
 			if len(actions)==1:
 				envRealPrev = copy.deepcopy(self.rle) #environment at step n-1; deepcopy is expensive
 				envTheoPrev = copy.deepcopy(theoryRLEs[1])
@@ -1077,7 +1093,6 @@ class Agent:
 
 				# if n==len(actions)-3:
 				# 	hypPrevprev = copy.deepcopy(self.hypotheses[1])
-
 
 			#TEST
 			#theoryRLEs[1].step(K_LEFT)
@@ -1464,7 +1479,7 @@ class Agent:
 		else:
 			print "Got no new theories"
 
-		embed()
+		#embed()
 
 		hypotheses = self.manageNewObjects(hypotheses)
 
@@ -1534,7 +1549,7 @@ if __name__ == "__main__":
 	# agent.playCurriculum(level_game_pairs=level_game_pairs)
 
 	##For local games, use this line
-	# agent.playCurriculum(level_game_pairs=None)
+	agent.playCurriculum(level_game_pairs=None)
 
 	## For testing, use this line
-	agent.testCurriculum(level_game_pairs=None)
+	# agent.testCurriculum(level_game_pairs=None)
