cd125671fee5c940d1352b83f19dca274085a1f9
jrburga <jrburga@mit.edu>
2018-03-09 17:30:50 -0500

    actually fixed merge conflicts

--- vgdl/core.py
+++ vgdl/core.py
@@ -1305,13 +1305,6 @@ class VGDLSprite(object):
             # print "MOVING"
         # if not( ((self.lastmove+1) % self.cooldown != 0) or abs(orientation[0])+abs(orientation[1])==0): ##used this until 9/14
             self.rect = self.rect.move((orientation[0]*speed, orientation[1]*speed))
-<<<<<<< HEAD
-
-        
-
-=======
-            # self.lastmove = 0
->>>>>>> with_tracker
 
     def _velocity(self):
         """ Current velocity vector. """
