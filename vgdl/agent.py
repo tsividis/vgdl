7bd0f75e666cea149041e04c4a40e973e9f22281
Nate Foss <npfoss@gmail.com>
2018-03-11 16:25:24 -0400

    fix bugs: killIfHasMore not in some predicate lists. 0 not proposed as limit. expandLine too loose with rule removal (for non intpair classes). no more residual conditionalKill in diagnosis (problems downstream). also VGDL is broken

--- vgdl/agent.py
+++ vgdl/agent.py
@@ -277,7 +277,7 @@ class Agent:
 
 	def testEpisode(self, gameObject, epoch=0):
 		
-		actions = [K_LEFT, K_LEFT, K_DOWN, K_DOWN, K_RIGHT]
+		actions = [K_LEFT, K_LEFT, K_DOWN, K_DOWN, K_RIGHT, K_RIGHT, K_RIGHT]
 		# actions = [K_UP, K_LEFT, K_LEFT]
 		self.initializeEnvironment()
 		# embed()
@@ -883,6 +883,9 @@ def errorSignal(envA, envB, theory, envPrev, p_dist=1, p_speed=1, p_miss=10, p_s
 
 	if envA._game.observation['score'] != envB._game.observation['score']:
 		total_penalty += p_score*abs(envA._game.observation['score']-envB._game.observation['score'])
+	if 0 < total_penalty and 'killIfHasMore' in [r.interaction for r in theory.interactionSet if r.slot2 == 'c2']:
+		print 'in errorSignal'
+		embed()
 	if penalty_only:
 		return total_penalty, []
 
@@ -1114,10 +1117,6 @@ def errorSignal(envA, envB, theory, envPrev, p_dist=1, p_speed=1, p_miss=10, p_s
 				pair = (p0, p1)
 				newIntPairs.append(pair)
 			e.intPairs = newIntPairs
-
-		# if 'conditionalKill' in e.diagnosis:
-		# 	print 'evidence of conditionalKill?'
-		# 	embed()
 
 	return total_penalty, errorMap
 
@@ -1528,7 +1527,7 @@ def filterTheories(scoreAndTheoryTuples, percentile, max_num, proportionOfSprite
 	return filtered
 
 def expandTheories(theories, errorList, envRealPrev, envRealCurrent, prevAction, rleHistory, actionHistory, symbolDict, best_params, bestSpriteTypeDict):
-	print "In expandTheories. errorList length: {}. Theories length {}".format(len(errorList), len(theories))
+	# print "In expandTheories. errorList length: {}. Theories length {}".format(len(errorList), len(theories))
 
 	# MEMOIZE!
 	# lookup table (dict) which maps (classPair, predicateTuple) to all combinations of all possible rules involving those classes and predicates
@@ -1669,8 +1668,7 @@ def expandTheoryForOneErrorMap(errorMap, envRealPrev, envRealCurrent, action, th
 		# if ('objectDestruction' in errorMap.diagnosis
 		# 			or any('objectDestruction' in e.diagnosis for e in theory.errorMapHistory) ) \
 		# 		and any([not rule.generic for rule in matchingRules]):
-		if not 'conditionalKill' in errorMap.diagnosis \
-				and any([not rule.generic for rule in matchingRules]):
+		if any([not rule.generic for rule in matchingRules]):
 			# embed()
 			if ('objectDestruction' in errorMap.diagnosis
 						or any(['objectDestruction' in e.diagnosis for e in theory.errorMapHistory if e.targetClass in targetClassPair]) ):
@@ -1691,6 +1689,12 @@ def expandTheoryForOneErrorMap(errorMap, envRealPrev, envRealCurrent, action, th
 		classPair, theories = expandLine(theory, errorMap, targetClassPair, predicates,
 			classPairPlusPredicateToRuleSets, n=n, 
 			observations=envRealCurrent._game.observation, generic=False)
+
+		if 'conditionalKill' in errorMap.diagnosis:
+			errorMap.diagnosis.remove('conditionalKill')
+			# print 'conditionalKill in expandTheoryForOneErrorMap'
+			# embed()
+
 		newTheories = list(set(theories))
 	if not newTheories:
 		newTheories = [theory]
--- vgdl/theory_template.py
+++ vgdl/theory_template.py
@@ -2340,7 +2340,7 @@ def proposeArgs(theory, predicate, errorMap, observations, generic=False):
 				for val in values:
 					argList.append({'limitspeed':val})
 			elif predicate == 'killIfTooFast':
-				values = [10,11,12]
+				values = [10,11]
 				for val in values:
 					argList.append({'speed':val})
 			elif predicate in ['killIfHasMore', 'killIfHasLess', 'killIfOtherHasMore', 'killIfOtherHasLess']:
@@ -2348,7 +2348,7 @@ def proposeArgs(theory, predicate, errorMap, observations, generic=False):
 				if len(resources) == 0:
 					print 'in proposeArgs: trying to propose conditional but no resources!'
 					embed()
-				limits = [1,2]
+				limits = [0,1]
 				for comb in list(itertools.product(resources, limits)):
 					argList.append({'resource':comb[0], 'limit':comb[1]})
 			else:
@@ -2409,7 +2409,7 @@ def proposePredicates(singlePairErrorSignal, observations):
 	## List of predicates that are unique to a physics type
 	physicsToPredicateMapping = {
 	'all' : 					['killSprite', 'cloneSprite', 'transformTo', 'transformToOnLanding',\
-								'killIfHasLess', 'killIfHasMore', 'killIfOtherHasLess', 'killIfOtherHasLess',\
+								'killIfHasLess', 'killIfHasMore', 'killIfOtherHasLess', 'killIfOtherHasMore',\
 								'killIfTooFast', 'killIfSlow',\
 								'undoAll', 'nothing',\
 								'turn', 'turnAround', 'reverseDirection', 'flipDirection', 'bounceForward',\
@@ -2426,7 +2426,7 @@ def proposePredicates(singlePairErrorSignal, observations):
 	'objectDestruction': 		['killSprite'],
 	'newObjectAppeared': 		['cloneSprite'],
 	'transformation': 			['transformTo', 'transformToOnLanding'],
-	'conditionalKill': 			['killIfHasLess', 'killIfHasMore', 'killIfOtherHasLess', 'killIfOtherHasLess',\
+	'conditionalKill': 			['killIfHasLess', 'killIfHasMore', 'killIfOtherHasLess', 'killIfOtherHasMore',\
 								 'killIfTooFast', 'killIfSlow', 'killIfFromAbove', 'killIfFromBelow'],
 
 	## Position difference
@@ -2565,7 +2565,7 @@ predicateToOrderingMapping = {
 	'killIfHasLess': 		(0,), 
 	'killIfHasMore': 		(0,),
 	'killIfOtherHasLess': 	(0,), 
-	'killIfOtherHasLess':	(0,),
+	'killIfOtherHasMore':	(0,),
 	'killIfTooFast':		(0,),
 	'killIfSlow':			(0,),
 	'killIfFromAbove':		(0,),
@@ -2643,7 +2643,7 @@ def expandLine(theory, errorMap, classPair, predicates, classPairPlusPredicateTo
 
 	## Conditionals can only replace kill rules. Remove the existing kill rules and replace them with conditionals.
 	if 'conditionalKill' in errorMap.diagnosis:
-		toRemove = [rule for rule in theory.interactionSet if rule.asTuple()[1]==errorMap.targetClass and rule.asTuple()[0]=='killSprite']
+		toRemove = [rule for rule in theory.interactionSet if classPair[0] in rule.asTuple() and classPair[1] in rule.asTuple() and rule.asTuple()[1] == errorMap.targetClass and rule.asTuple()[0]=='killSprite']
 		# if len(toRemove)>0:
 			# print "actually removing kill rules in expandLine"
 			# embed()
