6dd5b97617830cedf252ac3ec49a8ebfa27c01f2
jrburga <jrburga@mit.edu>
2018-03-13 19:50:42 -0400

    created LinkedDict type for use in matchEnvs (could have come in handy for event_handling, too)

--- vgdl/util.py
+++ vgdl/util.py
@@ -1,4 +1,5 @@
 from IPython import embed
+from line_profiler import LineProfiler
 import itertools
 import random
 import csv
@@ -8,6 +9,52 @@ from math import sqrt
 ALNUM = '0123456789bcdefhijklmnpqrstuvwxyzQWERTYUIOPSDFHJKLZXCVBNM,./;[]<>?:`-=~!@#$%^&*()_+'
 CHARS = 'bcdefhijklmnpqrstuvwxyzQWERTYUIOPSDFHJKLZXCVBNM'
 CAPCHARS = 'QWERTYUIOPSDFHJKLZXCVBNM'
+
+lp = LineProfiler()
+def profile(function):
+	''' Wrapper to print speed of function line-by-line '''
+	def profile(*args):
+		lp_wrapper = lp(function)
+		return_values = lp_wrapper(*args)
+		lp.print_stats()
+		return return_values
+	return profile
+
+
+class LinkedDict():
+    def __init__(self):
+        self._dict1 = {}
+        self._dict2 = {}
+
+    def __getitem__(self, key):
+        if key in self._dict1:
+            return self._dict1[key]
+        elif key in self._dict2:
+            return self._dict2[key]
+
+        return None
+
+    def __setitem__(self, key, value):
+        self._dict1[key] = value
+        self._dict2[value] = key
+
+    def __delitem__(self, key):
+        value = self._dict1[key]
+        del self._dict1[key]
+        del self._dict2[value]
+
+    def __len__(self):
+        """Returns the number of connections"""
+        return len(self._dict1)
+
+    def __repr__(self):
+        return '%r' % self.toList()
+
+    def iteritems(self):
+        return self._dict1.iteritems()
+
+    def toList(self):
+        return [(key, value) for key, value in self.iteritems()]
 
 def softmax(w, t = 1.0):
     e = np.exp(np.array(w) / t)
