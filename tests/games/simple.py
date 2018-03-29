85e0d657a1f0c8445eb60356bad247b56202ec51
jrburga <jrburga@mit.edu>
2018-03-29 16:51:02 -0400

    executing steps

--- tests/games/simple.py
+++ tests/games/simple.py
@@ -43,7 +43,6 @@ wwwwwwwwwwwwwwwwwwwwwwwwwwwwwwww
 """]
 
 
-
 game = """
 BasicGame
     SpriteSet
--- tests/test_agent.py
+++ tests/test_agent.py
@@ -14,6 +14,9 @@ class TestAgent(unittest.TestCase):
 	def setUp(self):
 		self.agent = Agent('full', FILENAME)
 
+	def tearDown(self):
+		pass
+
 	########################################
 	# Initialization
 	def initializeEnvironment(self, game_string=None, level_string=None):
@@ -21,20 +24,22 @@ class TestAgent(unittest.TestCase):
 		self.agent.levelString = level_string
 		self.agent.initializeEnvironment()
 
-	def initializeCurriculum(self, action_sequences):
-		num_episodes = len(action_sequences)
+	def initializeCurriculum(self, num_episodes):
 		self.agent.rleHistory = [[] for i in range(num_episodes)]
 		self.agent.actionHistory = [[] for i in range(num_episodes)]
 		self.agent.all_objects = [{} for i in range(num_episodes)]
-		self.action_sequences = [actions[:] for actions in action_sequences]
 
 	def initializeEpisode(self, episode_num):
-		self.action_num = 0
 		self.agent.all_objects[episode_num] = self.agent.rle._game.getObjects()
-		if episode_num == 0:
+		if not self.agent.hypotheses:
 			self.agent.initializeHypotheses(self.agent.all_objects[episode_num])
 		envReal = self.agent.fastcopy(self.agent.rle)
 		self.agent.rleHistory[episode_num].append(envReal)
+
+	def initialize(self, game_string, level_string, num_episodes=1):
+		self.initializeEnvironment(game_string, level_string)
+		self.initializeCurriculum(num_episodes)
+		self.initializeEpisode(0)
 
 	def buildTheory(self, game_string):
 		theory = Game(game_string).buildGenericTheory()
@@ -48,7 +53,7 @@ class TestAgent(unittest.TestCase):
 
 	def executeStep(self, episode_num, action, lastStep=False):
 		theoryRLEs = self.generateTheoryRLEs()
-		self.agent.hypotheses = self.executeStep(episode_num, self.agent.rleHistory, self.agent.actionHistory, 
+		self.agent.hypotheses = self.agent.executeStep(episode_num, self.agent.rleHistory, self.agent.actionHistory, 
 											     action, self.agent.hypotheses, theoryRLEs, lastStep)
 
 	def runEpisode(self, episode_num, actions):
@@ -57,6 +62,7 @@ class TestAgent(unittest.TestCase):
 			self.executeStep(episode_num, action)
 
 	def runCurriculum(self, action_sequences):
+		self.initializeCurriculum(len(action_sequences))
 		for episode_num, actions in enumerate(action_sequences):
 			self.runEpisode(episode_num, actions)
 
@@ -69,6 +75,8 @@ class TestAgent(unittest.TestCase):
 	# Test Suite
 	def testLevel0(self):
 		action_sequences = [[K_UP, K_UP], [K_DOWN, K_DOWN]]
+		self.initialize(simple.game, simple.levels[0], 2)
+		self.executeStep(0, K_UP)
 		self.assertTrue(True)
 
 	def testLevel1(self):
