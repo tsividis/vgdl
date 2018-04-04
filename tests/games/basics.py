45b215813bacb3798d8d827b06698fa553bce3f3
jrburga <jrburga@mit.edu>
2018-04-04 18:53:14 -0400

    Hopefully these test cases will be useful

--- tests/games/basics.py
+++ tests/games/basics.py
@@ -1,24 +1,26 @@
-levels = [
-"""
-wwwwwwwwwwwww
-w           w
-w           w
-w       b   w
-w       b   w
-w       A   w
-wwwwwwwwwwwww
-""",
-"""
-wwwwwwwwwwwww
-w           w
-w           w
-w       b   w
-w       b   w
-w       A   w
-wwwwwwwwwwwww
-""",
-]
+from tests.locals import *
+# These games will test mostly interactions
+# None of the other objects should be moving
 
+level1 ="""
+wwwwwwwwwwwww
+w           w
+w           w
+w       b   w
+w       b   w
+w       A   w
+wwwwwwwwwwwww
+"""
+
+level2 = """
+wwwwwwwwwwwww
+w           w
+w           w
+w       b   w
+w       b   w
+w       A   w
+wwwwwwwwwwwww
+"""
 '''
 stepBack
 killSprite
@@ -32,7 +34,7 @@ reverseDirection
 
 
 '''
-game = """
+game1 = """
 BasicGame
     SpriteSet
         box    > Immovable color=WHITE 
@@ -65,3 +67,42 @@ BasicGame
         SpriteCounter stype=box limit=0 win=True
         SpriteCounter stype=avatar limit=0 win=False
 """
+
+game1 = """
+BasicGame
+    SpriteSet
+        box    > Immovable color=WHITE 
+        avatar  > MovingAvatar color=DARKBLUE speed=1
+        wall > Immovable color=DARKGRAY
+    LevelMapping
+        A > avatar
+        b > box
+    InteractionSet
+        box avatar > killSprite
+
+        avatar wall > stepBack
+        wall avatar > stepBack
+        wall box > stepBack
+
+        box wall > stepBack
+        # avatar box > stepBack
+        box avatar > stepBack
+
+
+        avatar EOS > stepBack
+        wall EOS > stepBack
+        box EOS > stepBack
+
+        avatar avatar > stepBack
+        box box > stepBack
+        wall wall > stepBack
+
+    TerminationSet
+        SpriteCounter stype=box limit=0 win=True
+        SpriteCounter stype=avatar limit=0 win=False
+"""
+
+
+test1 = TestCase(game1, level1, [[K_UP, K_UP, K_RIGHT, K_RIGHT]])
+test2 = TestCase(game1, level1, [[K_UP, K_RIGHT, K_RIGHT]])
+test3 = TestCase(game1, level1, [[K_UP]])
--- tests/test_agent.py
+++ tests/test_agent.py
@@ -3,18 +3,18 @@ import unittest
 from IPython import embed
 
 from vgdl.agent import Agent, VrleInitPhase, sampleFromDistribution
-from pygame.locals import *
 from vgdl.ontology import *
 
 # imports all the game files created in games to the global namespace
 from tests.games import *
 from tests.theory_tools import *
+from tests.locals import *
 
 # I'm not sure if this actually matters...
 FILENAME = 'tests.game.simple'
 
 ###########################################
-# The abstract base class (DO NOT MODIFY)
+# The abstract base class. Only add Assertion Methods
 class _TestAgent(unittest.TestCase):
 
 	#######################################
@@ -47,14 +47,15 @@ class _TestAgent(unittest.TestCase):
 		envReal = self.agent.fastcopy(self.agent.rle)
 		self.agent.rleHistory[episode_num].append(envReal)
 
-	def initialize(self, game_string, level_string, num_episodes):
+	def initialize(self, game_string, level_string, action_sequences):
 		'''Sets up environment, curriculum, and episode'''
+		num_episodes = len(action_sequences)
 		self.initializeEnvironment(game_string, level_string)
 		self.initializeCurriculum(num_episodes)
 		self.initializeEpisode(0)
 
 	def initRunCreate(self, game, level, action_sequences):
-		self.initialize(game, level, len(action_sequences))
+		self.initialize(game, level, action_sequences)
 		self.runCurriculum(action_sequences)
 		return generateTheoryFromGameString(game)
 
@@ -129,7 +130,7 @@ class _TestAgent(unittest.TestCase):
 		# display += '\n%s' % theory2 
 		self.assertTrue(theoriesEqual(theory1, theory2), display)
 
-	def assertHasTheory(self, theory):
+	def assertAgentHasTheory(self, theory):
 		self.assertTrue(theoryInHypotheses(theory, self.agent.hypotheses), 'Theory not in hypotheses: \n%s' % theory)
 
 
@@ -138,6 +139,9 @@ class _TestAgent(unittest.TestCase):
 		for e, h in self.agent.scoresAndHypotheses:
 			if h == hypothesis:
 				self.assertTrue(e <= epsilon, 'Hypothesis does not have low error')
+
+	def assertAgentHasTheories(self):
+		self.assertTrue(len(self.agent.hypotheses) > 0, 'Agent has no theories')
 
 	def _testLevel0(self):
 		test_hypothesis = generateTheoryFromGameString(simple.test_hypothesis2)
@@ -157,12 +161,14 @@ def _testConstructor(game, level, action_sequences):
 	def testCase(self):
 		real_description = self.initRunCreate(game, level, action_sequences)
 
-		self.assertHasTheory(real_description)
+		self.assertAgentHasTheory(real_description)
 		self.assertTheoriesEqual(self.agent.hypotheses[0], real_description)
 		self.assertTheoryBelowEpsilonError(self.agent.hypotheses[0])
 
 	return testCase
 
+######################################################################################
+# The Test Class you actually want to modify!
 
 class TestAgent(_TestAgent):
 
@@ -170,17 +176,23 @@ class TestAgent(_TestAgent):
 	# Test Suite
 	#
 	# This is one way to create a test case. Defaults to the basics (defined above)
-	testBasicsKillSpritesAndWin = _testConstructor(basics.game, basics.levels[0], [[K_UP, K_UP]])
+	testBasicsKillSpritesAndWin = _testConstructor(*basics.test1)
 
-	# This is another way to create a test case. You do everything individually.
+	# This is another way to create a test case. You can do everything individually.
 	def testBasics(self):
-		action_sequences = [[K_LEFT, K_RIGHT]]
-		real_description = self.initRunCreate(basics.game, basics.levels[1], action_sequences)
+		real_description = self.initRunCreate(*basics.test2)
 		
-		self.assertHasTheory(real_description)
+		self.assertAgentHasTheory(real_description)
 		self.assertTheoriesEqual(self.agent.hypotheses[0], real_description)
 		self.assertTheoryBelowEpsilonError(self.agent.hypotheses[0])
 
+	def testFilter(self):
+		self.initialize(*basics.test1)
+
+		self.executeStep(0, K_UP)
+		self.assertAgentHasTheories()
+		self.executeStep(0, K_UP)
+		self.assertAgentHasTheories()
 
 
 
