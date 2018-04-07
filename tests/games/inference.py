15339ef1e73856097d442f3ad0789ba7fb819089
jrburga <jrburga@mit.edu>
2018-04-07 18:59:28 -0400

    Added description tools to locals for joining games together. Added inference.py tests using these tools.

--- tests/games/inference.py
+++ tests/games/inference.py
@@ -1,102 +1,165 @@
 from tests.locals import *
 
+tSetBase = """
+TerminationSet
+    SpriteCounter stype=avatar limit=0 win=False
+"""
+
+levelMapping = """
+LevelMapping
+    1 > box
+    2 > box2
+    3 > box3
+    w > wall
+    c > cannon
+    A > avatar
+"""
 
 
-# level = """
-# wwwwwwwwwwwwwwwwwwwwwwwwwwwwwwww
-# w                              w
-# w  C     C              1     Cw
-# w             C         1      w
-# w1111111111    C        1111111w
-# w         1   C2C  C        2  w
-# w         1  C C         C     w
-# w    C         A               w
-# wwwwwwwwwwwwwwwwwwwwwwwwwwwwwwww
+sSetBoxes = """
+SpriteSet
+    box	 > Immovable color=WHITE 
+    box2 > Immovable color=GREEN
+    box3 > Immovable color=YELLOW
+    wall > Immovable color=DARKGRAY
+"""
 
-# """
+sSetCannon = """
+SpriteSet
+	cannon > SpawnPoint color=RED stype=sam spawnCooldown=2
+	sam > Missile orientation=UP color=BLUE singleton=False cooldown=1
+"""
+
+iSetBoxes = """
+InteractionSet
+    avatar wall > stepBack
+    box avatar > transformTo stype=box2
+    box2 avatar > killSprite
+    box3 avatar > killSprite
+    # Need to include all step backs
+"""
+
+iSetCannons = """
+InteractionSet
+    cannon avatar > bounceForward
+    avatar sam > bounceForward
+    cannon sam > stepBack
+    sam cannon > stepBack	
+    # need to include additional stepbacks
+"""
+
+'''
+The original interaction set
+
+InteractionSet
+    avatar wall > stepBack
+    # box avatar > nothing
+    box avatar > transformTo stype=box2
+    # cannon sam > stepBack
+    # sam cannon > stepBack
+    box2 avatar > killSprite
+    # box2 avatar > bounceForward
+    box3 avatar > killSprite
+    cannon avatar > bounceForward
+    avatar sam > bounceForward
+    cannon sam > stepBack
+    sam cannon > stepBack
+'''
+
+
+gameBoxes = catDescriptions(sSetBoxes, iSetBoxes, tSetBase, levelMapping)
+gameCannons = catDescriptions(joinDescs(sSetBoxes, sSetCannon), 
+							  joinDescs(iSetBoxes, iSetCannons), tSetBase, levelMapping)
 
 # up, up, up, up, left
-# level = """
-# wwwwwwwwwwwwwwwwwwwwwwwwwwwwwwww
-# w                              w
-# w              1             1 w
-# w                            1 w
-# w              2               w
-# w                    2         w
-# w              A          3 3  w
-# w                              w
-# wwwwwwwwwwwwwwwwwwwwwwwwwwwwwwww
-# """
+level1 = """
+wwwwwwwwwwwwwwwwwwwwwwwwwwwwwwww
+w                              w
+w              1             1 w
+w                            1 w
+w              2               w
+w                    2         w
+w              A          3 3  w
+w                              w
+wwwwwwwwwwwwwwwwwwwwwwwwwwwwwwww
+"""
+test1 = TestCase(gameBoxes, level1, [[K_UP]*3+[K_LEFT]])
 
 #combine with avatar sam bounceFoward. works.
 #0,0,0,0,0,0
-# level = """
-# wwwwwwwwwwwwwwwwwwwwwwwwwwwwwwww
-# w                              w
-# w                            1 w
-# w                            1 w
-# w                              w
-# w              A               w
-# w                         3 3  w
-# w         c    c               w
-# wwwwwwwwwwwwwwwwwwwwwwwwwwwwwwww
-# """
+level2 = """
+wwwwwwwwwwwwwwwwwwwwwwwwwwwwwwww
+w                              w
+w                            1 w
+w                            1 w
+w                              w
+w              A               w
+w                         3 3  w
+w         c    c               w
+wwwwwwwwwwwwwwwwwwwwwwwwwwwwwwww
+"""
+test2 = TestCase(gameBoxes, level1, [[0]*6])
 
 # TODO: You need to be able to re-run testAndExpand() when no hypotheses pass your filter
 ## the problem here is that you need to build on expandSprite proposals with expandLine within one errorMap and
 ## you don't ordinarily do that.
 #0,0,0,0,0,0
-# level = """
-# wwwwwwwwwwwwwwwwwwwwwwwwwwwwwwww
-# w                              w
-# w                            1 w
-# w                            1 w
-# w                              w
-# w                              w
-# w              A          3 3  w
-# w         c    c               w
-# wwwwwwwwwwwwwwwwwwwwwwwwwwwwwwww
-# """
+level3 = """
+wwwwwwwwwwwwwwwwwwwwwwwwwwwwwwww
+w                              w
+w                            1 w
+w                            1 w
+w                              w
+w                              w
+w              A          3 3  w
+w         c    c               w
+wwwwwwwwwwwwwwwwwwwwwwwwwwwwwwww
+"""
+
+test3 = TestCase(gameBoxes, level3, [[0]*6])
 
 # works if you don't allow the eventHandler to apply effects to newly-created sprites
 #[0,0,0,K_LEFT, K_LEFT,0,0]
-# level = """
-# wwwwwwwwwwwwwwwwwwwwwwwwwwwwwwww
-# w                              w
-# w                            1 w
-# w                            1 w
-# w                              w
-# w                              w
-# w                         3 3  w
-# w         c    c A             w
-# wwwwwwwwwwwwwwwwwwwwwwwwwwwwwwww
-# """
+level4 = """
+wwwwwwwwwwwwwwwwwwwwwwwwwwwwwwww
+w                              w
+w                            1 w
+w                            1 w
+w                              w
+w                              w
+w                         3 3  w
+w         c    c A             w
+wwwwwwwwwwwwwwwwwwwwwwwwwwwwwwww
+"""
+test4 = TestCase(gameBoxes, level4, [[0]*3+[K_LEFT]*2+[0]*2])
 
 # #up, up, down
-# level = """
-# wwwwwwwwwwwwwwwwwwwwwwwwwwwwwwww
-# w                              w
-# w                            1 w
-# w                            1 w
-# w              1               w
-# w              1               w
-# w              A          3 3  w
-# w                              w
-# wwwwwwwwwwwwwwwwwwwwwwwwwwwwwwww
-# """
+level5 = """
+wwwwwwwwwwwwwwwwwwwwwwwwwwwwwwww
+w                              w
+w                            1 w
+w                            1 w
+w              1               w
+w              1               w
+w              A          3 3  w
+w                              w
+wwwwwwwwwwwwwwwwwwwwwwwwwwwwwwww
+"""
+test5 = TestCase(gameBoxes, level5, [[K_UP, K_UP, K_DOWN]])
 
 #0,0,0,0,0,0,0
-# level = """
-# wwwwwwwwwwwwwwwwwwwwwwwwwwwwwwww
-# w                              w
-# w                            1 w
-# w                            1 w
-# w              1               w
-# w              1               w
-# w              A          3 3  w
-# w   c   c                      w
-# wwwwwwwwwwwwwwwwwwwwwwwwwwwwwwww
-# """
+level6 = """
+wwwwwwwwwwwwwwwwwwwwwwwwwwwwwwww
+w                              w
+w                            1 w
+w                            1 w
+w              1               w
+w              1               w
+w              A          3 3  w
+w   c   c                      w
+wwwwwwwwwwwwwwwwwwwwwwwwwwwwwwww
+"""
+test6 = TestCase(gameBoxes, level6, [[0]*7])
 
 # level = """
 # wwwwwwwwwwwwwwwwwwwwwwwwwwwwwwww
--- tests/locals.py
+++ tests/locals.py
@@ -2,26 +2,74 @@ from collections import namedtuple
 from pygame.locals import K_UP, K_RIGHT, K_LEFT, K_DOWN, K_SPACE
 TestCase = namedtuple('TestCase', 'game, level, action_sequences')
 
-def catDescriptions(*description_sets):
+def args(**kwargs):
+	args = ""
+	for key, value in kwargs.iteritems():
+		assert isinstance(value, str), "argument values must be strings (this isn't that fancy)"
+		args += "%s=%s " % (key, value)
+	args.strip()
+	return args
+
+def classDef(class_name, vgdl_type, **kwargs):
+	return "%s > %s %s"  % (class_name, vgdl_type, args(**kwargs))
+
+def mapping(class_name, char):
+	assert len(char) == 1, "Char must be single character"
+	return "%s > %s" % (char, class_name)
+
+def ruleDef(class1, class2, rule_name, **kwargs):
+	return "%s %s > %s %s" % (class1, class2, rule_name, args(**kwargs))
+
+def catDescriptions(*desc_sets):
 	game_description = "BasicGame\n"
-	for desc_set in description_sets:
-		game_description += '\t'+'\t'.join(desc_set.splitlines(True))
-		game_description += '\n'
+	for desc_set in desc_sets:
+		game_description += '\t%s\n' % '\t'.join(desc_set.strip().splitlines(True))
 	return game_description
+
+def joinDescs(*desc_sets):
+	assert desc_sets, "joinDescs takes at least 1 argument"
+	# grabs the name of the set
+	new_set = desc_sets[0].strip().splitlines(True)[0]
+	for desc_set in desc_sets:
+		new_set += '%s\n' % ''.join(desc_set.strip().splitlines(True)[1:])
+	return new_set
+	
+def add2Set(desc_set, *descs):
+	new_desc_set = desc_set
+	for desc in descs:
+		new_desc_set += '\n\t%s\n' % desc
+	return new_desc_set
 
 
 if __name__ == '__main__':
-	sprite_set = """SpriteSet
+	sprite_set = """
+SpriteSet
     box    > Passive color=BROWN # orientation=RIGHT cooldown=1
-    box2 > Immovable color=PURPLE"""
+    box2 > Immovable color=PURPLE
+"""
+	sprite_set2 = """
+SpriteSet
+    box    > Passive color=BROWN # orientation=RIGHT cooldown=1
+    box2 > Immovable color=PURPLE
+"""
 
-	level_mapping = """LevelMapping
+	level_mapping = """
+LevelMapping
 	b > box
 	2 > box2"""
-	interaction_set = """InteractionSet
+	interaction_set = """
+InteractionSet
 	avatar box > killSprite"""
 
-	termination_set = """TerminationSet
+	termination_set = """
+TerminationSet
 	SpriteCounter stype=box limit=0 win=True"""
 
 	print catDescriptions(sprite_set, level_mapping, interaction_set, termination_set)
+
+	avatar = 'avatar > MovingAvatar'
+	print add2Set(sprite_set, avatar)
+	print classDef('avatar', 'Immovable', color='GREEN')
+	print mapping('avatar', 'A')
+	print args(color='GREEN')
+	print joinDescs(sprite_set, sprite_set2)
