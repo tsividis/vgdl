a37fdae6bdb8c21611e53f3528ce1c21cfc494d4
Pedro <tsividis@mit.edu>
2018-04-19 17:46:09 -0400

    Using regroundOrNot to decide whether to run induction at all -- induction and replay don't run when world looks as we predicted it would. Results in a huge speedup.

--- vgdl/agent.py
+++ vgdl/agent.py
@@ -129,7 +129,7 @@ class Agent:
 		self.distributions = {}
 		self.history = {}
 		self.lastObjectState = {}
-
+		self.bestScoresAndHypotheses = None
 		# Hyperopt output
 		self.total_game_steps = 0
 		self.total_planner_steps = 0
@@ -386,7 +386,8 @@ class Agent:
 			if not quitting:
 				for i, action in enumerate(solution):
 					print "executing step"
-					bestScoresAndHypotheses = self.executeStep(episode_num, self.rleHistory, self.actionHistory, action, self.hypotheses, theoryRLEs, lastStep=False)
+					bestScoresAndHypotheses = self.executeStep(episode_num, i, self.rleHistory, self.actionHistory, action, self.hypotheses, theoryRLEs, objectPositionsArray, lastStep=False)
+					self.bestScoresAndHypotheses = bestScoresAndHypotheses
 					# self.hypotheses = [bestScoresAndHypotheses[0][1]]
 					self.hypotheses = [item[1] for item in bestScoresAndHypotheses]
 					print "executed step"
@@ -404,7 +405,6 @@ class Agent:
 					if ended:
 						break
 
-					# if self.regrounding:
 					# if self.avoid_danger
 
 				if self.shortHorizon:
@@ -498,7 +498,7 @@ class Agent:
 				if num == len(actions)-1:
 					lastStep=True
 				t2 = time.time()
-				scoresAndHypotheses = self.executeStep(episode_num, self.rleHistory, self.actionHistory, action, self.hypotheses, theoryRLEs, lastStep)
+				scoresAndHypotheses = self.executeStep(episode_num, num, self.rleHistory, self.actionHistory, action, self.hypotheses, theoryRLEs, lastStep)
 				print ""
 				print "executed step in {} seconds".format(time.time()-t2)
 				print ""
@@ -576,7 +576,75 @@ class Agent:
 
 		return scoresAndHypotheses, scoreAndTheoryTuples
 
-	def executeStep(self, episode_num, rleHistories, actionHistories, action, hypotheses, theoryRLEs, lastStep=False):
+	def regroundOrNot(self, step_number, objectPositionsArray, hypothesis):
+		# regroundingFlag = False
+
+		matchedEnvs, la, lb = matchEnvs(self.rle, objectPositionsArray[step_number+1])
+		if la:
+			return True
+		if lb:
+			return True
+		for match in matchedEnvs:
+			if match[2]!=0:
+				return True
+		return False
+		# if (step_number+1)%self.regrounding==0:
+		# if True:
+		# 	try:
+		# 		rlePositions = sorted([(int(item.rect.x), int(item.rect.y), item) for sublist in self.rle._game.sprite_groups.values() for item in sublist])
+		# 		hypPositions = sorted([(int(item.rect.x), int(item.rect.y), item) for sublist in objectPositionsArray[step_number+1]._game.sprite_groups.values() for item in sublist])
+		# 		rlePositionsTuples, hypPositionsTuples = [(p[0], p[1]) for p in rlePositions], [(p[0], p[1]) for p in hypPositions]
+
+		# 		killer_types = [inter.slot2 for inter in hypothesis.interactionSet if inter.slot1=='avatar' and inter.interaction in ['killSprite']]
+		# 		# print "killer types", killer_types
+		# 		for objPos in hypPositions:
+		# 			if not regroundingFlag and (objPos[0], objPos[1]) not in rlePositionsTuples:
+		# 				# print "found object position difference", colored(objPos, 'white', 'on_magenta')
+		# 				# print 'regrounding because of', objPos[2].colorName, objPos[2], "position:", self.rle._rect2pos(objPos[2].rect)
+		# 				# try:
+		# 					# print "orientation:", objPos[2].orientation
+		# 				# except AttributeError:
+		# 					# pass
+		# 				nearest = findNearestSprite(objPos[2], [h[2] for h in rlePositions])
+		# 				# print "Nearest sprite:", nearest.colorName, nearest, "position:", self.rle._rect2pos(nearest.rect)
+		# 				# try:
+		# 					# print "orientation:", nearest.orientation
+		# 				# except AttributeError:
+		# 					# pass
+		# 				# print ""
+		# 				# embed()
+		# 				if self.selective_regrounding:
+		# 					if ((objPos[2].name=='avatar') or
+		# 						(objPos[2].name in killer_types and manhattanDist(self.rle._rect2pos(objPos[2].rect), self.rle._rect2pos(self.rle._game.getAvatars()[0].rect)) < self.safeDistance)):
+
+		# 						# if objPos[2].name=='avatar':
+		# 							# embed()
+		# 						regroundingFlag = True
+		# 						# embed()
+		# 						break
+		# 				else:
+		# 					regroundingFlag = True
+		# 					break
+
+		# 		if regroundingFlag:
+		# 			print "regrounding"
+		# 			return regroundingFlag
+		# 		# if tuple(rlePositions) != tuple(hypPositions):
+		# 		# # if any(np.where(list(gameString_array[i+1]))[0] !=
+		# 		# #        np.where(list(self.rle.show()))[0]):
+		# 		#     print 'regrounding'
+		# 		#     embed()
+		# 		#     # embed()
+		# 		#     break
+		# 	except:
+		# 		# Mismatch in gamestring lengths
+		# 		print ""
+		# 		print 'regrounding problem'
+		# 		embed()
+
+		return regroundingFlag
+
+	def executeStep(self, episode_num, step_num, rleHistories, actionHistories, action, hypotheses, theoryRLEs, objectPositionsArray, lastStep=False):
 
 		envRealPrev = self.fastcopy(self.rle)
 		actionHistories[episode_num].append(action)
@@ -617,15 +685,21 @@ class Agent:
 
 		print "evaluating {} old theories and proposing new ones".format(len(theoryRLEs))
 		updateTerminations(self.rle, hypotheses)
-		newTheories = []
-	
-		for num, env in enumerate(theoryRLEs):
-			theories = testAndExpand(env, self.hypotheses[num], action, self.rle, envRealPrev, self.rleHistory, \
-					self.actionHistory, episode_num)
-			newTheories.extend(theories)
+		
+		regroundingFlag = self.regroundOrNot(step_num, objectPositionsArray, hypotheses[0])
 
-		newTheories = list(set(newTheories))
+		if regroundingFlag:
+			newTheories = []
+			for num, env in enumerate(theoryRLEs):
+				theories = testAndExpand(env, self.hypotheses[num], action, self.rle, envRealPrev, self.rleHistory, \
+						self.actionHistory, episode_num)
+				newTheories.extend(theories)
 
+			newTheories = list(set(newTheories))
+		else:
+			newTheories = self.hypotheses
+			if self.bestScoresAndHypotheses:
+				return self.bestScoresAndHypotheses
 		self.allTheories.extend(newTheories)
 		print ""
 		print "Tested and expanded {} theories to produce {} child theories".format(len(theoryRLEs), len(newTheories))
