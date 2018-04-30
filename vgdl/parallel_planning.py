ec808153b53531c5e84ff2c41c053964176f4761
joaoloula <joao.campos-loula@polytechnique.edu>
2018-04-30 17:51:04 +0200

    created training_set_1 folder at root for gvgai games

--- vgdl/parallel_planning.py
+++ vgdl/parallel_planning.py
@@ -53,7 +53,7 @@ def play_trainset(hyperparameters):
         		yield color
 
 
-        gvgname = "./gvgai/training_set_1/{}".format(gameName)
+        gvgname = "./training_set_1/{}".format(gameName)
 
         gameString = read_gvgai_game('{}.txt'.format(gvgname))
 
