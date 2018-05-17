0f82c61152348eafbd9e2bb6a9a22aadc07bd61b
Nate Foss <npfoss@gmail.com>
2018-05-17 15:53:33 -0400

    programmatically determine legal actions for a given game

--- examples/gridphysics/theorytest.py
+++ examples/gridphysics/theorytest.py
@@ -1,90 +1,60 @@
 level="""
-333333333333333333
-3 4 0   3    5   3
-3   0   32   33333
-3 1 0         0  3
-333333333333333333
+1                             
+888                           
+888                           
+                              
+                              
+                              
+                              
+    888      888888     888   
+   88888    88888888   88888  
+   8   8    88    88   88888  
+                7             
 """
 game = """
 BasicGame
 	SpriteSet
-		avatar > MovingAvatar color=DARKBLUE
-		c3 > ResourcePack color=ORANGE
-		c2 > ResourcePack color=PINK
-		c6 > ResourcePack color=GOLD
-		c5 > ResourcePack color=BLACK
+		c3 > ResourcePack color=LIGHTGRAY
+		avatar > FlakAvatar color=DARKBLUE stype=sam
+		sam > Missile color=PINK singleton=True orientation=UP
 		c4 > ResourcePack color=WHITE
 	InteractionSet
-		c2 avatar > killSprite
-		c2 c4 > nothing
-		c4 c2 > nothing
-		c6 avatar > killSprite
-		c4 c5 > nothing
-		c5 c4 > nothing
-		c3 c5 > nothing
-		c5 c3 > nothing
+		sam c4 > nothing
+		c4 sam > nothing
+		sam sam > nothing
+		sam c3 > nothing
+		c3 sam > nothing
+		c3 c3 > nothing
+		avatar EOS > stepBack
 		c3 c4 > nothing
 		c4 c3 > nothing
-		c2 c5 > nothing
-		c5 c2 > nothing
-		avatar EOS > stepBack
-		c3 avatar > killSprite
-		c2 c6 > nothing
-		c6 c2 > nothing
-		c4 c6 > nothing
-		c6 c4 > nothing
-		c2 c2 > nothing
-		c5 c6 > nothing
-		c6 c5 > nothing
-		c3 c3 > nothing
-		c3 c6 > nothing
-		c6 c3 > nothing
-		c6 c6 > nothing
-		c2 c3 > nothing
-		c3 c2 > nothing
-		avatar c5 > stepBack
+		sam avatar > killSprite
 		c4 avatar > killSprite
-		c5 c5 > nothing
 		c4 c4 > nothing
-		c5 EOS > stepBack
+		c3 avatar > killSprite
+		sam EOS > stepBack
 		c4 EOS > stepBack
 		c3 EOS > stepBack
-		c6 EOS > stepBack
-		c2 EOS > stepBack
 	TerminationSet
-		NoveltyTermination s1=c2 s2=avatar win=True
-		NoveltyTermination s1=c2 s2=c2 win=True
-		NoveltyTermination s1=c2 s2=c3 win=True
-		NoveltyTermination s1=c2 s2=c4 win=True
-		NoveltyTermination s1=c2 s2=c5 win=True
-		NoveltyTermination s1=c2 s2=c6 win=True
+		NoveltyTermination s1=sam s2=avatar win=True
+		NoveltyTermination s1=sam s2=sam win=True
+		NoveltyTermination s1=sam s2=c3 win=True
+		NoveltyTermination s1=sam s2=c4 win=True
 		NoveltyTermination s1=c3 s2=avatar win=True
 		NoveltyTermination s1=c3 s2=c3 win=True
 		NoveltyTermination s1=c3 s2=c4 win=True
-		NoveltyTermination s1=c3 s2=c5 win=True
-		NoveltyTermination s1=c3 s2=c6 win=True
 		NoveltyTermination s1=c4 s2=avatar win=True
 		NoveltyTermination s1=c4 s2=c4 win=True
-		NoveltyTermination s1=c4 s2=c5 win=True
-		NoveltyTermination s1=c4 s2=c6 win=True
-		NoveltyTermination s1=c5 s2=c5 win=True
-		NoveltyTermination s1=c5 s2=c6 win=True
-		NoveltyTermination s1=c6 s2=avatar win=True
-		NoveltyTermination s1=c6 s2=c6 win=True
-		NoveltyTermination s1=c2 s2=EOS win=True
+		NoveltyTermination s1=sam s2=EOS win=True
 		NoveltyTermination s1=c3 s2=EOS win=True
 		NoveltyTermination s1=c4 s2=EOS win=True
-		NoveltyTermination s1=c5 s2=EOS win=True
-		NoveltyTermination s1=c6 s2=EOS win=True
 		NoveltyTermination s1=avatar s2=EOS win=True
 		SpriteCounter stype=avatar limit=0 win=False
 	LevelMapping
-		0 > c2
-		1 > c6
-		2 > avatar
-		3 > c5
-		4 > c3
-		5 > c4
+		0 > sam
+		1 > c3
+		7 > avatar
+		8 > c4
 """
 if __name__ == "__main__":
 	from vgdl.core import VGDLParser
