e5a9aaede445a52d2d8a86317b0b92e372fba02b
Pedro <tsividis@mit.edu>
2018-06-14 16:21:38 -0400

    debug

--- vgdl/main_agent.py
+++ vgdl/main_agent.py
@@ -286,10 +286,10 @@ class Agent:
             level_game_pairs = importlib.import_module(self.gameFilename).level_game_pairs
         episodes = []
         allEffectsEncountered = []
-        embed()
-        # if 'images' in os.listdir('.') and 'tmp' in os.listdir('images'):
-            # shutil.rmtree("images/tmp")
-        # os.makedirs("images/tmp")
+
+        if 'images' in os.listdir('.') and 'tmp' in os.listdir('images'):
+            shutil.rmtree("images/tmp")
+        os.makedirs("images/tmp")
         j=0
         flexible_goals = False
 
