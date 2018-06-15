da68e5fccec4abf77d86df2321403b14c8ac1e55
Pedro <tsividis@mit.edu>
2018-06-15 14:13:40 -0400

    Add proto-script for parallel game-running.

---
+++ params.py
@@ -0,0 +1,26 @@
+games_to_hyperparameters = {
+	'game1' : [hyperparameter_sets[0], hyperparameter_sets[1], hyperparameter_sets[2]]
+	'game2' : [hyperparameter_sets[1]]
+	'game3' : [hyperparameter_sets[0]]
+}
+
+hyperparameter_sets = [
+    {
+     'sprite_first_alpha': 10000,
+     'sprite_second_alpha': 100,
+     'sprite_negative_mult': .1,
+     'multisprite_first_alpha': 10000,
+     'multisprite_second_alpha': 100,
+     'novelty_first_alpha': 5000,
+     'novelty_second_alpha': 50,
+     },
+    {
+     'sprite_first_alpha': 10000,
+     'sprite_second_alpha': 100,
+     'sprite_negative_mult': 10.,
+     'multisprite_first_alpha': 10000,
+     'multisprite_second_alpha': 100,
+     'novelty_first_alpha': 5000,
+     'novelty_second_alpha': 50,
+     }
+]
---
+++ run_games.py
@@ -0,0 +1,16 @@
+import params, os, itertools, subprocess, shlex
+from joblib import Parallel, delayed
+
+num_cores = len(os.sched_getaffinity(0))
+
+lst = []
+for k,v in games_to_hyperparameters:
+	x = itertools.repeat(k)
+	lst.extend(zip(x,v))
+
+def run_models(game_name, hyperparams):
+	# call_string = "python -m vgdl.parallel_planning --game_name {} --hyperparams {}".format(game_name, hyperparams)
+	call_string = "echo \"{} {}\"".format(game_name, hyperparams)
+	subprocess.call(shlex.split(call_string))
+
+Parallel(n_jobs=num_cores)(delayed(run_models)(game_name, hyperparams) for game_name, hyperparams in lst)
