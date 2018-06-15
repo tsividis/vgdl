91d1e85ad97e5e1fdace32dcb1fbf76ae82bdba8
Ubuntu <ubuntu@ip-172-31-1-38.us-east-2.compute.internal>
2018-06-15 19:32:54 +0000

    stuff

--- run_games.py
+++ run_games.py
@@ -1,15 +1,16 @@
-import params, os, itertools, subprocess, shlex, psutil
+import params, os, itertools, subprocess, shlex, psutil, time
 from joblib import Parallel, delayed
 
 num_cores = psutil.cpu_count()
-
+print num_cores
 lst = []
-for k,v in params.games_to_hyperparameters:
+for k,v in params.games_to_hyperparameters.items():
 	x = itertools.repeat(k)
 	lst.extend(zip(x,v))
 
 def run_models(game_name, hyperparams):
 	# call_string = "python -m vgdl.parallel_planning --game_name {} --hyperparams {}".format(game_name, hyperparams)
+	time.sleep(10)
 	call_string = "echo \"{} {}\"".format(game_name, hyperparams)
 	subprocess.call(shlex.split(call_string))
 
