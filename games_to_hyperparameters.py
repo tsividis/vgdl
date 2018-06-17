dd5856bfa00cabfa7e12b7dcc17a454145879c19
Pedro <tsividis@mit.edu>
2018-06-17 14:27:27 -0400

    set which games to run

--- games_to_hyperparameters.py
+++ games_to_hyperparameters.py
@@ -5,10 +5,11 @@ game_names = ['angelsdemons', 'avoidgeorge', 'bait', 'boulderchase', \
              'chopper', 'clusters', 'cookmepasta', 'infection',\
              'jaws', 'lemmings', 'modality', 'myAliens',\
              'overload', 'plants', 'plaqueattack', 'shipwreck',\
-             'superman', 'tercio', 'thesnowman', 'tiny_game1', 'tiny_game2',\
+             'superman', 'tercio', 'thesnowman', \
              'watergame', 'waves', 'wildgunman']
 
 mapping = dict()
 
-for k in ['angelsdemons', 'avoidgeorge']:
+for k in game_names:
+    if k not in ['lemmings', 'plants', 'shipwreck', 'waves']: # ignore these for now
     mapping[k] = [0,2]
