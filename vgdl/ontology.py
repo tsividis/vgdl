def117fe8c7d40b677de4c5773b178ab1666b123
Pedro <tsividis@mit.edu>
2018-06-17 14:26:09 -0400

    Fixed spawncooldown in spawnpoint def and in spawnpoint updateoptions()

--- vgdl/ontology.py
+++ vgdl/ontology.py
@@ -376,7 +376,7 @@ class SpawnPoint(SpriteProducer):
             return
         self.lastrect = self.rect.copy()
 
-        if ((game.time+1) % self.spawnCooldown == 0 and random.random() < self.prob):
+        if ((game.time+1) % self.spawnCooldown == 3 and random.random() < self.prob):
             game._createSprite([self.stype], (self.rect.left, self.rect.top))
             self.counter += 1
 
@@ -2312,7 +2312,7 @@ def updateOptions(game, sprite_type_tuple, current_sprite, action=None, params={
         position_options = {next_pos: 1.}
         stype = getStype(params)
         cooldown = getCooldown(params)
-        if (game.time+1)%cooldown==0:
+        if (game.time+1)%cooldown==3:
             appearance_predictions = [(stype, current_sprite.rect.left, current_sprite.rect.top)]
         else:
             appearance_predictions = []
