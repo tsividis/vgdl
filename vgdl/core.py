064eb63124fd0f4130fcdfe65ada11d1d824ca4e
Eric Wu <eric_wu@g.harvard.edu>
2018-06-19 14:52:45 -0400

    Add __eq__ comparison to VGDLSprite

--- vgdl/core.py
+++ vgdl/core.py
@@ -24,6 +24,7 @@ from IPython import embed
 import time
 import os
 import uuid
+from util import *
 
 # ---------------------------------------------------------------------
 #     Constants
@@ -1287,6 +1288,11 @@ class VGDLSprite(object):
 
     def __repr__(self):
         return str(self.name)+" at (%s,%s)"%(self.rect.left, self.rect.top)
+
+    def __eq__ (self, other):
+        if other == None:
+            return False
+        return self.ID == other.ID
 
 class EOS(VGDLSprite):
     color = ENDOFSCREEN
