c8a4e77cd5a44c67508cfee8560980b60d7d34ca
Pedro <tsividis@mit.edu>
2018-06-20 12:14:58 -0400

    added all_games

--- vgdl/load_games.py
+++ vgdl/load_games.py
@@ -15,8 +15,7 @@ game_number = args.game_number
 game_name = args.game_name
 hyperparameter_index = args.hyperparameter_index
 
-# gameFileString = 'training_set_1'
-gameFileString = 'gvgai/games'
+gameFileString = 'all_games'
 
 hyperparameter_sets = [
     {'idx'           : 0,
