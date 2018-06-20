668fe84575ee6934b7ff0b5873ab71b61b15f5ba
Pedro <tsividis@mit.edu>
2018-06-20 15:14:29 -0400

    changed gvgai_by_name

--- model_batch.sh
+++ model_batch.sh
@@ -2,12 +2,17 @@
 
 
 #SBATCH --job-name=run_vgdl_model
-#SBATCH --array=0-2
+#SBATCH --array=0-13%30
 #SBATCH --output=slurm_logs/array_%A_%a.out
 #SBATCH --time=120
 #SBATCH --qos=tenenbaum
 #SBTACH --cpus-per-task=2
 #SBATCH --mem=4G
+
+# --array=0-2%30 tells it to run array instances 0-2 and to never run more than 30 jobs at a time.
+# if i'm using qos=tenenbaum i shouldn't exceed 30.
+# if i use qos normal i can run more. but then jobs might get taken down and will resume later.
+# given that I don't have safe states it's hard to do that well.
 
 # This is the root path of the repo
 ROOT="/om2/user/tsividis/vgdl"
@@ -20,7 +25,7 @@ SRC=""
 DST=""
 
 # Figure out which game and hyperparameter
-N_GAMES=10
+N_GAMES=14
 N_PARAMS=3
 GAME_NUMBER=$(($SLURM_ARRAY_TASK_ID % $N_GAMES))
 HYPER_IDX=$(($SLURM_ARRAY_TASK_ID % $N_PARAMS))
@@ -40,4 +45,5 @@ if [ ! -d "slurm_logs" ]; then
 fi
 
 # finally, run the model
-singularity exec  -B "/$BASE:/$BASE" $CONT python -m vgdl.load_games --game_number $GAME_NUMBER --hyperparameter_index $HYPER_IDX
+# singularity exec  -B "/$BASE:/$BASE" $CONT python -m vgdl.load_games --game_number $GAME_NUMBER --hyperparameter_index $HYPER_IDX
+singularity exec  -B "/$BASE:/$BASE" $CONT python -m vgdl.load_games --game_number $GAME_NUMBER --hyperparameter_index 2
