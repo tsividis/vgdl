20df5d4f10d737fa1f7796a4a8a1f90cb01b51f2
Pedro <tsividis@mit.edu>
2018-06-20 12:22:09 -0400

    Got load_games and games_to_hyperparameters from hyperopt_optimized_planner.

--- vgdl/load_games.py
+++ vgdl/load_games.py
@@ -1,4 +1,5 @@
 from main_agent import Agent
+from games_to_hyperparameters import *
 import time
 import dill
 import os
@@ -15,6 +16,11 @@ game_number = args.game_number
 game_name = args.game_name
 hyperparameter_index = args.hyperparameter_index
 
+if game_name==str(0):
+    game_name = game_names[game_number]
+
+# gameFileString = 'training_set_1'
+# gameFileString = 'gvgai/games'
 gameFileString = 'all_games'
 
 hyperparameter_sets = [
