3d2f108cb32f4d9bb944bc57417fd2c88ce3d474
Pedro Tsividis <tsividis@openmind7.mit.edu>
2018-06-21 14:34:02 -0400

    game change

--- examples/gridphysics/theorytest.py
+++ examples/gridphysics/theorytest.py
@@ -1,39 +1,40 @@
 level="""
-33333333333333333333333333333333
-3                              3
-3                              3
-3                              3
-3                              3
-3                              3
-3                              3
-3       8     4                3
-333                           03
-33333333333333333333333333333333
+44444444444444444444444444444444
+4                              4
+4        7         7           4
+4              7               4
+4  5                           4
+4   6                     7    4
+4   3               7          4
+4     6  7                     4
+444                 5          4
+44444444444444444444444444444444
 """
 game = """
 BasicGame
 	SpriteSet
 		avatar > MovingAvatar color=DARKBLUE
-		c3 > ResourcePack color=DARKGRAY
-		c2 > ResourcePack color=PINK
-		c5 > Chaser color=LIGHTGREEN fleeing=False cooldown=1 stype=c4
-		c4 > ResourcePack color=YELLOW
+		c3 > ResourcePack color=WHITE
+		c2 > ResourcePack color=ORANGE
+		c5 > Chaser color=GREEN fleeing=False cooldown=8 stype=c3
+		c4 > ResourcePack color=BLACK
 	InteractionSet
-		c2 avatar > killSprite
-		c2 c5 > nothing
+		avatar c2 > nothing
 		c5 c2 > nothing
+		c2 c5 > nothing
 		c2 c4 > nothing
 		c4 c2 > nothing
-		c4 c5 > killSprite
-		c2 c3 > nothing
-		c3 c2 > nothing
+		c5 c4 > nothing
+		c4 c5 > nothing
+		c3 c2 > killSprite
 		c2 c2 > nothing
-		avatar c5 > nothing
-		c3 c3 > nothing
+		c5 avatar > killSprite
+		avatar c4 > stepBack
 		avatar EOS > stepBack
-		avatar c3 > stepBack
-		c5 c5 > nothing
-		c4 avatar > bounceForward
+		c3 avatar > bounceForward
+		c3 c3 > bounceForward
+		c5 c3 > nothing
+		c3 c5 > nothing
 		c4 c4 > nothing
 		c3 c4 > nothing
 		c4 c3 > nothing
@@ -41,31 +42,29 @@ BasicGame
 		c5 EOS > stepBack
 		c2 EOS > stepBack
 		c3 EOS > stepBack
-		c5 c3 > stepBack
 	TerminationSet
-		NoveltyTermination s1=c2 s2=avatar win=True
 		NoveltyTermination s1=c2 s2=c2 win=True
-		NoveltyTermination s1=c2 s2=c3 win=True
 		NoveltyTermination s1=c2 s2=c4 win=True
-		NoveltyTermination s1=c2 s2=c5 win=True
-		NoveltyTermination s1=c3 s2=c3 win=True
 		NoveltyTermination s1=c3 s2=c4 win=True
 		NoveltyTermination s1=c4 s2=c4 win=True
-		NoveltyTermination s1=c5 s2=c5 win=True
 		NoveltyTermination s1=c2 s2=EOS win=True
 		NoveltyTermination s1=c3 s2=EOS win=True
 		NoveltyTermination s1=c4 s2=EOS win=True
-		NoveltyTermination s1=c5 s2=EOS win=True
 		NoveltyTermination s1=avatar s2=EOS win=True
+		NoveltyTermination s1=c5 s2=avatar win=True
+		NoveltyTermination s1=c5 s2=EOS win=True
+		NoveltyTermination s1=c5 s2=c2 win=True
+		NoveltyTermination s1=c5 s2=c3 win=True
+		NoveltyTermination s1=c5 s2=c4 win=True
 		SpriteCounter stype=avatar limit=0 win=False
-		SpriteCounter stype=c4 limit=0 win=False
+		SpriteCounter stype=c3 limit=0 win=True
 	LevelMapping
-		0 > c2
-		2 > avatar
-		3 > c3
+		8 > avatar c2
+		3 > avatar
 		4 > c4
-		5 > c5
-		8 > avatar c5
+		5 > c2
+		6 > c5
+		7 > c3
 """
 if __name__ == "__main__":
 	from vgdl.core import VGDLParser
