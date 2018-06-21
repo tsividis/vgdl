a63d3b8452f77f0ede112e1ef77b14f3057059cf
Pedro <tsividis@mit.edu>
2018-06-21 10:01:37 -0400

    changed max_steps

--- vgdl/main_agent.py
+++ vgdl/main_agent.py
@@ -27,7 +27,7 @@ AvatarTypes = [MovingAvatar, HorizontalAvatar, VerticalAvatar, FlakAvatar, Aimed
 RotatingAvatar, RotatingFlippingAvatar, NoisyRotatingFlippingAvatar, ShootAvatar, AimedAvatar,
 AimedFlakAvatar, InertialAvatar, MarioAvatar]
 
-MAX_STEPS = 600
+MAX_STEPS = 1000
 
 # orientationPairs = {(0, 1):(0, -1), DOWN:UP, LEFT:RIGHT, RIGHT:LEFT}
 
