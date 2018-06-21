175e7e7131fb5925f22f5f5ba7c4486b25f3c3ba
Pedro <tsividis@mit.edu>
2018-06-20 20:49:52 -0400

    changed qos and time

--- model_batch.sh
+++ model_batch.sh
@@ -4,8 +4,8 @@
 #SBATCH --job-name=run_vgdl_model
 #SBATCH --array=0-13%30
 #SBATCH --output=slurm_logs/array_%A_%a.out
-#SBATCH --time=480
-#SBATCH --qos=normal
+#SBATCH --time=120
+#SBATCH --qos=tenenbaum
 #SBTACH --cpus-per-task=2
 #SBATCH --mem=8G
 
