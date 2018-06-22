510f682009793431b62e631d64dad5d9d3538137
Pedro <tsividis@mit.edu>
2018-06-22 13:13:06 -0400

    undid IW lesion

--- vgdl/WBP.py
+++ vgdl/WBP.py
@@ -274,7 +274,9 @@ class WBP():
 
 	def rewardSelection(self, QReward, QNovelty):
 		acceptableNodes = QReward
-		acceptableNodes = filter(lambda n: (not n.terminal or n.win), acceptableNodes)
+		acceptableNodes = filter(lambda n:n.novelty<3, QReward)
+		## Use this for IW lesion
+		#acceptableNodes = filter(lambda n: (not n.terminal or n.win), acceptableNodes)
 		bestNodes = sorted(acceptableNodes, key=lambda n: (-n.intrinsic_reward, n.novelty))
 		try:
 			current = bestNodes.pop(0)
