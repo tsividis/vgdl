96483344235cc4f2250c9f37ec4c36222487f02e
Pedro <tsividis@mit.edu>
2018-06-24 17:26:40 -0400

    fix

--- model_batch.sh
+++ model_batch.sh
@@ -2,7 +2,7 @@
 
 
 #SBATCH --job-name=run_vgdl_model
-#SBATCH --array=0-26%30
+#SBATCH --array=0-24%30
 #SBATCH --output=slurm_logs/hyperopt_boulderdash/array_%A_%a.out
 #SBATCH --time=720
 #SBATCH --qos=tenenbaum
