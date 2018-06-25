687a8f1204893a726748bccf1c20055721d5e2db
Pedro <tsividis@mit.edu>
2018-06-25 10:16:24 -0400

    changed helper and lemmings, runnning those w/ modal params

--- games_to_hyperparameters.py
+++ games_to_hyperparameters.py
@@ -14,12 +14,13 @@
 
 # game_names = ['aliens', 'butterflies', 'expt_helper', 'expt_push_boulders', 'frogs', 'portals', 'survivezombies', 'zelda']
 
-game_names = [ 'expt_helper', 'expt_push_boulders', 'zelda', 'angelsdemons', 'avoidgeorge', 
-			'bait', 'camelRace', 'chopper', 'clusters',
-			'jaws', 'lemmings', 'myAliens', 'overload', 'plants', 
-			'plaqueattack', 'shipwreck','superman','watergame', 'waves',
-			 'wildgunman', 'aliens', 'boulderchase', 'modality', 'tercio']
+# game_names = [ 'expt_helper', 'expt_push_boulders', 'zelda', 'angelsdemons', 'avoidgeorge', 
+# 			'bait', 'camelRace', 'chopper', 'clusters',
+# 			'jaws', 'lemmings', 'myAliens', 'overload', 'plants', 
+# 			'plaqueattack', 'shipwreck','superman','watergame', 'waves',
+# 			 'wildgunman', 'aliens', 'boulderchase', 'modality', 'tercio']
 
+game_names = ['lemmings', 'expt_helper']
 #skipped: , cookmepasta, thesnowman, shipwreck
 # run w/ different params: boulderchase, modality, tercio
 
--- model_batch.sh
+++ model_batch.sh
@@ -2,9 +2,9 @@
 
 
 #SBATCH --job-name=run_vgdl_model
-#SBATCH --array=0-24%30
+#SBATCH --array=0-1%30
 #SBATCH --output=slurm_logs/hyperopt_boulderdash/array_%A_%a.out
-#SBATCH --time=720
+#SBATCH --time=600
 #SBATCH --qos=tenenbaum
 #SBTACH --cpus-per-task=2
 #SBATCH --mem=32G
