ed9a3213fccb9b2fe24f05380b21438c5a7f90ab
Pedro <tsividis@mit.edu>
2018-06-26 14:04:20 -0400

    fixed other display call

--- vgdl/WBP.py
+++ vgdl/WBP.py
@@ -1311,7 +1311,7 @@ class Node():
 				# if k not in self.WBP.seen_limits:
 					# print("Current resource={}, limit={}".format(self.rle._game.getAvatars()[0].resources[k], self.WBP.theory.resource_limits[k]))
 			if any([self.rle._game.getAvatars()[0].resources[k]==self.WBP.theory.resource_limits[k] for k in self.rle._game.getAvatars()[0].resources.keys() if k not in self.WBP.seen_limits]):
-				if self.display:
+				if self.WBP.display:
 					print "resource limit win"
 				self.win=True
 		except IndexError:
