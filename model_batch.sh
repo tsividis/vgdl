9b7473caa6f0a2f7d31bdf6145ef867c0d23da85
Pedro <tsividis@mit.edu>
2018-06-26 14:28:46 -0400

    preparing run

--- model_batch.sh
+++ model_batch.sh
@@ -2,7 +2,7 @@
 
 
 #SBATCH --job-name=run_vgdl_model
-#SBATCH --array=0-11
+#SBATCH --array=0-3
 #SBATCH --output=slurm_logs/hyperopt_boulderdash_optimized2/array_%A_%a.out
 #SBATCH --time=600
 #SBATCH --qos=tenenbaum
