f64e7b386655c15f40998e7ae8997bc66ac85698
Pedro <tsividis@mit.edu>
2018-06-26 14:55:23 -0400

    running more games

--- games_to_hyperparameters.py
+++ games_to_hyperparameters.py
@@ -25,8 +25,8 @@
 # game_names = ['bait', 'clusters']
 
 ## running with hyperparams3
-game_names = ['myAliens'] ##running for longer
-# game_names = ['chopper', 'jaws', 'plaqueattack', 'wildgunman']
+# game_names = ['myAliens'] ##running for longer
+game_names = ['chopper', 'jaws', 'plaqueattack', 'wildgunman']
 # game_names = ['avoidgeorge', 'bait', 'chopper', 
 # 				'infection', 'jaws', 'myAliens', 'overload', 'plants', 'plaqueattack', 
 # 				'superman', 'waves', 'wildgunman']
