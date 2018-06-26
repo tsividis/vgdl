190b8b16694d54bf516f9797a05625ede7034976
Pedro <tsividis@mit.edu>
2018-06-26 14:55:23 -0400

    running more games

--- model_batch.sh
+++ model_batch.sh
@@ -2,9 +2,9 @@
 
 
 #SBATCH --job-name=run_vgdl_model
-#SBATCH --array=0
+#SBATCH --array=0-3
 #SBATCH --output=slurm_logs/hyperopt_boulderdash_optimized2/array_%A_%a.out
-#SBATCH --time=800
+#SBATCH --time=720
 #SBATCH --qos=tenenbaum
 #SBTACH --cpus-per-task=2
 #SBATCH --mem=32G
