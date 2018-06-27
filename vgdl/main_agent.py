33b47474eac3af06d237debbc3c518f4feaecf62
Pedro <tsividis@mit.edu>
2018-06-27 13:37:33 -0400

    using embed() statements in WBP and main_agent

--- vgdl/main_agent.py
+++ vgdl/main_agent.py
@@ -453,7 +453,7 @@ class Agent:
 
         ## Initialize external environment
         self.initializeEnvironment()
-        # embed()
+        embed()
         print "initializing RLE"
         steps = 0
         self.quits = 0
