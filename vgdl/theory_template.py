16404a29b88b7599ea2f5e830cad8adf70593b7f
Pedro <tsividis@mit.edu>
2018-06-28 10:40:20 -0400

    same

--- vgdl/theory_template.py
+++ vgdl/theory_template.py
@@ -13,7 +13,7 @@ import operator
 import time, math
 from util import factorize, objectsToSymbol, ccopy
 from rlenvironmentnonstatic import createMindEnv
-from line_profiler import LineProfiler
+# from line_profiler import LineProfiler
 
 
 ALNUM = '0123456789bcdefhijklmnpqrstuvwxyzQWERTYUIOPSDFHJKLZXCVBNM,./;[]<>?:`-=~!@#$%^&*()_+'
