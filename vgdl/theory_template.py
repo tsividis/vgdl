903038ce727254c4a339968a4cd37835e5d7503a
Pedro <tsividis@mit.edu>
2018-07-01 17:28:04 -0400

    theory.interpret() adds classes to theory if necessary

--- vgdl/theory_template.py
+++ vgdl/theory_template.py
@@ -787,6 +787,15 @@ class Theory(object):
 
 		return newTheories
 
+
+	def getNewClassName(self, color):
+        existing_classes = [key for key in theory.classes if key[0] == 'c']
+        max_num = max([int(c[1:]) for c in existing_classes])
+        class_num = max_num+1
+        newClassName = 'c'+str(class_num)
+        return newClassName
+        #     theory.addSpriteToTheory(newClassName, color, vgdlType=Resource, args={'limit':errorMap.targetToken.inventory[k][1]})
+
 	"""Helper functions"""
 	def interpret(self, event):
 		"""
@@ -800,11 +809,23 @@ class Theory(object):
 		that corresponds to (bounceForward, c1, c2)
 		"""
 
+		if event[1] in self.spriteObjects:
+			obj1 = self.spriteObjects[event[1]]
+		else:
+			self.addSpriteToTheory(event[1])
+			# obj1 = self.spriteObjects[event[1]]
+
+		if event[2] in self.spriteObjects:
+			obj2 = self.spriteObjects[event[2]]
+		else:
+			self.addSpriteToTheory(event[2])
+			# obj2 = self.spriteObjects[event[2]]
+
 		try:
 			obj1 = self.spriteObjects[event[1]]
 			obj2 = self.spriteObjects[event[2]]
 		except:
-			print "couldn't find event[2] in interpret()"
+			print "couldn't find spriteObjects[event[k]] in interpret()"
 			embed()
 
 		c1, c2 = self.getClass(obj1), self.getClass(obj2)
@@ -1835,6 +1856,25 @@ class Game(object):
 
 		T.updateTerminations()
 		return T
+
+	def addSpriteToTheory(self, vgdlType='default', args=None):
+		
+		newSpriteName = self.getNewClassName(color)
+		if vgdlType=='default':
+			vgdlType = ResourcePack
+		sprite = Sprite(vgdlType, color, className=newSpriteName, args=args)
+		self.classes[newSpriteName] = [sprite]
+		self.spriteSet.append(sprite)
+		self.spriteObjects[color] = sprite
+		for (o1,o2) in itertools.product([newSpriteName], self.classes.keys()):
+			if o2=='avatar':
+				rule1 = InteractionRule('killSprite', o1, o2, {}, set(), generic=True)
+			else:
+				rule1 = InteractionRule('nothing', o1, o2, {}, set(), generic=True)
+			rule2 = InteractionRule('nothing', o2, o1, {}, set(), generic=True)
+			self.interactionSet.append(rule1)
+			self.interactionSet.append(rule2)
+		return
 
 	def addNewObjectsToTheory(self, theory, spriteSample):
 		# Get the important objects in the theory names
