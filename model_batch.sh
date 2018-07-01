cebfcb93a9eb485f3ae1be68125fff4783b736e2
Pedro <tsividis@mit.edu>
2018-06-30 20:16:04 -0400

    changed shorthorizon params. Grabbed boulderdash from other branch

--- model_batch.sh
+++ model_batch.sh
@@ -2,7 +2,7 @@
 
 
 #SBATCH --job-name=run_vgdl_model
-#SBATCH --array=0-3
+#SBATCH --array=0-11
 #SBATCH --output=slurm_logs/hyperopt_boulderdash_optimized/array_%A_%a.out
 #SBATCH --time=720
 #SBATCH --qos=tenenbaum
