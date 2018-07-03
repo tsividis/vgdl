c635fa4c27df0d158a296add37cb630a604c3c4b
Pedro <tsividis@mit.edu>
2018-07-02 21:02:25 -0400

    ready to run games

--- model_batch.sh
+++ model_batch.sh
@@ -2,7 +2,7 @@
 
 
 #SBATCH --job-name=run_vgdl_model
-#SBATCH --array=0-1
+#SBATCH --array=0-2
 #SBATCH --output=slurm_logs/short_horizon/array_%A_%a.out
 #SBATCH --time=720
 #SBATCH --qos=tenenbaum
