dc17eda0b54c57b3b13638f1066c86e06fca3ae4
Pedro <tsividis@mit.edu>
2018-07-02 20:57:10 -0400

    ready to run games

--- vgdl/main_agent.py
+++ vgdl/main_agent.py
@@ -49,7 +49,7 @@ class Agent:
         self.shortHorizon = hyperparameters['short_horizon']#False
         self.firstOrderHorizon = hyperparameters['first_order_horizon'] #True ## Makes you commit to a plan once first-order distances change (e.g., spritecounter values)        if self.shortHorizon == True:
         if self.shortHorizon == True:
-            self.starting_max_nodes = 100
+            self.starting_max_nodes = 500
             self.max_nodes_annealing = 1.05
         else:
             self.starting_max_nodes = 10000
