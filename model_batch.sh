5d3707f07f89a6cecea316eef5bc25d18ad1d9dc
Pedro <tsividis@mit.edu>
2018-07-04 00:03:13 -0400

    ready to run

--- model_batch.sh
+++ model_batch.sh
@@ -4,10 +4,10 @@
 #SBATCH --job-name=run_vgdl_model
 #SBATCH --array=0-4
 #SBATCH --output=slurm_logs/planner_integration/array_%A_%a.out
-#SBATCH --time=600
+#SBATCH --time=840
 #SBATCH --qos=tenenbaum
 #SBTACH --cpus-per-task=2
-#SBATCH --mem=32G
+#SBATCH --mem=64G
 
 # --array=0-2%30 tells it to run array instances 0-2 and to never run more than 30 jobs at a time.
 # if i'm using qos=tenenbaum i shouldn't exceed 30.
