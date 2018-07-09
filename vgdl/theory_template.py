ee3ae9eef0d50744fbd73209b8d429e6b12169c2
Pedro <tsividis@mit.edu>
2018-07-09 17:35:31 -0400

    updateTerminations() screens in the same way for precondition rules as for normal rules when making NoveltyTerminations. Removed an embed in buildArgsString

--- vgdl/theory_template.py
+++ vgdl/theory_template.py
@@ -577,14 +577,18 @@ class Theory(object):
 
 				if addNoveltyRules and rule.generic and rule.preconditions:
 					# if (rule.slot1, rule.slot2) not in imaginedEffectTuples:
-					terminationRule = NoveltyRule(rule.slot1, rule.slot2, True, copy.deepcopy(rule.preconditions))
-					# print "got novelty termination with preconditions"
-					# embed()
-					if (all([not ((t.termination.s2==rule.slot1) and (t.termination.s1==rule.slot2))
-							for t in self.terminationSet if t.ruleType=='NoveltyRule']) and
-						all([not terminationRule.__eq__(t) for t in self.terminationSet]) and
-						all([not terminationRule.__eq__(t) for t in self.falsified])):
-						self.terminationSet.add(terminationRule)
+					if (    (rule.asTuple()[0]!='nothing' and not (rule.slot1==thingWeShoot and rule.slot2=='avatar')) or 
+							( rule.interaction == 'nothing' and not (rule.slot1==thingWeShoot and rule.slot2==thingWeShoot) and not ('Random' in str(self.classes[rule.slot1][0].vgdlType)) and rule.slot2 not in ['avatar', thingWeShoot]) or
+							( rule.interaction == 'nothing' and not (rule.slot2==thingWeShoot and rule.slot1==thingWeShoot) and not ('Random' in str(self.classes[rule.slot2][0].vgdlType)) and rule.slot1 not in ['avatar', thingWeShoot])
+							):
+						terminationRule = NoveltyRule(rule.slot1, rule.slot2, True, copy.deepcopy(rule.preconditions))
+						# print "got novelty termination with preconditions"
+						# embed()
+						if (all([not ((t.termination.s2==rule.slot1) and (t.termination.s1==rule.slot2))
+								for t in self.terminationSet if t.ruleType=='NoveltyRule']) and
+							all([not terminationRule.__eq__(t) for t in self.terminationSet]) and
+							all([not terminationRule.__eq__(t) for t in self.falsified])):
+							self.terminationSet.add(terminationRule)
 				elif addNoveltyRules and rule.generic and not rule.preconditions:
 					if (rule.slot1, rule.slot2) not in imaginedEffectTuples:
 						## Omit noveltytermination for randoms bumping into objects in the game; makes us disrupt plans even though we shouldnt't.
@@ -1554,8 +1558,9 @@ def buildArgsString(interactionRule, theory, rle):
 			argsString = " resource=%s limit=%s"%(precondition.item, str(limit))
 
 		else:
-			print "buildArgsString got called but no precondition"
-			embed()
+			argsString = ""
+			# print "buildArgsString got called but no precondition"
+			# embed()
 
 	return argsString, newInteractionName
 
