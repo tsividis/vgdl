7e24894593ef9b722eb7259eb2464cd009f14aea
Pedro <tsividis@mit.edu>
2018-07-11 17:54:08 -0400

    Re-creating NoveltyTerminations for generic rules with some probability even when we think we've seen those interactions

--- vgdl/theory_template.py
+++ vgdl/theory_template.py
@@ -597,7 +597,7 @@ class Theory(object):
 							all([not terminationRule.__eq__(t) for t in self.falsified])):
 							self.terminationSet.add(terminationRule)
 				elif addNoveltyRules and rule.generic and not rule.preconditions:
-					if (rule.slot1, rule.slot2) not in imaginedEffectTuples:
+					if (rule.slot1, rule.slot2) not in imaginedEffectTuples or (rule.generic and random.random()<.2):
 						## Omit noveltytermination for randoms bumping into objects in the game; makes us disrupt plans even though we shouldnt't.
 					# if ((rule.slot1, rule.slot2) not in imaginedEffectTuples) or (rule.generic and random.random()<.2):
 						# if (    (rule.asTuple()[0]!='nothing' and not (rule.slot1==thingWeShoot and rule.slot2=='avatar')) or 
