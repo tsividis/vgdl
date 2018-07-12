d53f929ef696b745f4c14eb6b957ec2d9efc20c9
Pedro <tsividis@mit.edu>
2018-07-12 17:54:18 -0400

    ready to run

--- vgdl/ontology.py
+++ vgdl/ontology.py
@@ -2070,8 +2070,8 @@ def initializeDistributionArgs(sprite_type, objectColors):
     """
 
     def initializeSpeed():
-        speedValues = [0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.]
-        # speedValues = [0.1, 0.2, 0.4, 1.]
+        # speedValues = [0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.]
+        speedValues = [0.1, 0.2, 0.4, 1.]
         return [('speed', v) for v in speedValues]
         # initializeProperty(args, 'speed', speedValues)
 
