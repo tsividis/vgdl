b79229420426ccff039431f28ba8a31a2ac7ffe3
Pedro <tsividis@mit.edu>
2018-07-13 15:08:53 -0400

    ready to run

--- vgdl/WBP.py
+++ vgdl/WBP.py
@@ -1351,7 +1351,7 @@ class Node():
 
 		# heuristicVal += sum(self.rolloutArray)
 
-		# heuristicVal += self.rle._game.score*abs(heuristicVal)
+		heuristicVal += self.rle._game.score*abs(heuristicVal)
 		
 		return heuristicVal
 
