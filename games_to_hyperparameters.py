672e7f90f8defac744ab97d1df50ef5a175fc760
Pedro <tsividis@mit.edu>
2018-07-14 09:38:11 -0400

    ready to run

--- games_to_hyperparameters.py
+++ games_to_hyperparameters.py
@@ -18,10 +18,10 @@
 
 ## idx 2, more memory:
 # game_names = ['expt_push_boulders', 'sokoban', 'bait', 'watergame']
-game_names = ['boulderdash', 'boulderchase']
+# game_names = ['boulderdash', 'boulderchase']
 
 # ## idx 3
-# game_names = ['aliens', 'butterflies', 'chase', 'expt_helper', 'missilecommand', 'survivezombies', 'zelda', 'camelRace', 'jaws', 'lemmings', 'myAliens', 'overload', 'plaqueattack', 'waves']
+game_names = ['aliens', 'butterflies', 'chase', 'expt_helper', 'missilecommand', 'survivezombies', 'zelda', 'camelRace', 'jaws', 'lemmings', 'myAliens', 'overload', 'plaqueattack', 'waves', 'chopper']
 # game_names = ['chase', 'expt_antagonist', 'missilecommand', 'zelda', 'camelRace', 'jaws', 'lemmings']
 
 # ## idx 3, more memory
