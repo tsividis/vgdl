3bdfdcb2df987779dfab70851be442fe8a720a02
Pedro <tsividis@mit.edu>
2018-07-15 22:29:25 -0400

    ready to run

--- vgdl/WBP.py
+++ vgdl/WBP.py
@@ -77,7 +77,7 @@ class WBP():
 		self.extra_atom = extra_atom
 		self.gameString_array = []
 		self.rolloutHyperparameters = dict([(k,v) if 'second' not in k else (k,0) for k,v in self.hyperparameters.items()])
-		self.display = True
+		self.display = False
 
 
 		if theory == None:
