c4baad4ea9298248aaed07b185b19a9117db46f3
Pedro <tsividis@mit.edu>
2018-07-15 22:27:02 -0400

    ready to run

--- games_to_hyperparameters.py
+++ games_to_hyperparameters.py
@@ -18,13 +18,13 @@
 
 ## idx 2, more memory:
 # game_names = ['expt_push_boulders', 'sokoban', 'bait', 'watergame']
-# game_names = ['boulderdash', 'boulderchase']
+# game_names = ['boulderdash', 'boulderchase', 'frogs', 'sokoban', 'watergame']
 
 # ## idx 3
-game_names = ['aliens', 'avoidgeorge', 'butterflies', 'chase', 'expt_helper', 'missilecommand', 'survivezombies', 'zelda', 'camelRace', 'jaws', 'lemmings', 'myAliens', 'overload', 'plaqueattack', 'waves', 'chopper']
+# game_names = ['aliens', 'avoidgeorge', 'butterflies', 'chase', 'expt_helper', 'missilecommand', 'survivezombies', 'zelda', 'camelRace', 'jaws', 'lemmings', 'myAliens', 'overload', 'plaqueattack', 'waves', 'chopper']
 # game_names = ['aliens', 'survivezombies', 'jaws', 'plaqueattack', 'waves']
-# game_names = ['myAliens']
-# game_names = ['jaws', 'waves']
+game_names = ['chopper', 'plaqueattack']
+
 # ## idx 3, more memory
 # game_names = ['chopper']
 
--- model_batch.sh
+++ model_batch.sh
@@ -7,7 +7,7 @@
 #SBATCH --time=960
 #SBATCH --qos=tenenbaum
 #SBTACH --cpus-per-task=2
-#SBATCH --mem=32G
+#SBATCH --mem=64G
 
 # --array=0-2%30 tells it to run array instances 0-2 and to never run more than 30 jobs at a time.
 # if i'm using qos=tenenbaum i shouldn't exceed 30.
