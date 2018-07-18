fc278fe4ecfd7a65d95d1eebd6c2430ffee5f1da
Pedro <tsividis@mit.edu>
2018-07-17 23:30:44 -0400

    ready to run

--- model_batch.sh
+++ model_batch.sh
@@ -2,8 +2,8 @@
 
 
 #SBATCH --job-name=run_vgdl_model
-#SBATCH --array=0
-#SBATCH --output=slurm_logs/short_horizon2/array_%A_%a.out
+#SBATCH --array=0-15
+#SBATCH --output=slurm_logs/short_horizon/array_%A_%a.out
 #SBATCH --time=960
 #SBATCH --qos=tenenbaum
 #SBTACH --cpus-per-task=2
@@ -39,11 +39,11 @@ if [ "$OS" = "CentOS Linux" ]; then
 fi
 
 # make log path if not already present
-if [ ! -d "${ROOT}/slurm_logs/short_horizon2" ]; then
-    mkdir "${ROOT}/slurm_logs/short_horizon2"
+if [ ! -d "${ROOT}/slurm_logs/short_horizon" ]; then
+    mkdir "${ROOT}/slurm_logs/short_horizon"
 fi
 
 # finally, run the model
 # singularity exec  -B "/$BASE:/$BASE" $CONT python -m vgdl.load_games --game_number $GAME_NUMBER --hyperparameter_index $HYPER_IDX
-singularity exec  -B "/$BASE:/$BASE" $CONT python -m vgdl.load_games --game_number $GAME_NUMBER --hyperparameter_index 2
+singularity exec  -B "/$BASE:/$BASE" $CONT python -m vgdl.load_games --game_number $GAME_NUMBER --hyperparameter_index 3
 #echo "-m vgdl.load_games --game_name ${GAME_NAME} --hyperparameter_index ${HYPER_IDX}"
--- vgdl/util.py
+++ vgdl/util.py
@@ -86,7 +86,7 @@ def make_random_name(chars):
 	return name
 
 def write_to_csv(foldername, filename, game):
-	dirname = 'model_results_short_horizon2/'
+	dirname = 'model_results_short_horizon/'
 	
 	if filename not in os.listdir(dirname+foldername+'/'):
 		f = open(dirname+foldername+'/'+filename, 'w+') #newfile and write
