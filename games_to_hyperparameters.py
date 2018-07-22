7a6bfc69fbb342e181b5a66ed6b7af5b75ea54e0
Pedro <tsividis@mit.edu>
2018-07-22 10:58:45 -0400

    Added expt_exploration_exploitation, aka, expt_ee

--- games_to_hyperparameters.py
+++ games_to_hyperparameters.py
@@ -13,8 +13,11 @@
 # game_names = ['expt_push_boulders', 'portals']
 # game_names = ['expt_push_boulders']
 # game_names = ['portals']
-game_names = ['expt_preconditions']
+# game_names = ['expt_preconditions']
+game_names = ['expt_ee']
+
 #idx_4
+
 # game_names = ['zelda', 'expt_helper']
 # game_names = ['expt_antagonist']
 # game_names = ['boulderchase', 'jaws', 'myAliens', 'overload', 'avoidgeorge']
