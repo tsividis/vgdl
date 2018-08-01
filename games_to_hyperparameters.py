3031c62c3ff93799163dce0b1aa36506410f468e
Pedro <tsividis@mit.edu>
2018-08-01 16:49:17 -0400

    ready to run

--- games_to_hyperparameters.py
+++ games_to_hyperparameters.py
@@ -23,7 +23,7 @@
 # game_names = ['boulderdash', 'boulderchase']
 # game_names = ['expt_preconditions']
 # ## idx 3
-game_names = ['aliens']
+game_names = ['jaws']
 # game_names = ['aliens', 'avoidgeorge', 'butterflies', 'chase', 'expt_helper', 'missilecommand', 'survivezombies', 'zelda', 'camelRace', 'jaws', 'lemmings', 'myAliens', 'overload', 'plaqueattack', 'waves']
 # game_names = ['aliens', 'survivezombies', 'jaws', 'plaqueattack', 'waves']
 # game_names = ['chopper', 'plaqueattack']
