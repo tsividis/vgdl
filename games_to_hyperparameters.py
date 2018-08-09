249257f2b10359f56b8a13f19edfd6c7b4f65ca6
Pedro <tsividis@mit.edu>
2018-08-09 17:07:18 -0400

    ready to run

--- games_to_hyperparameters.py
+++ games_to_hyperparameters.py
@@ -11,7 +11,7 @@
 
 
 
-game_names = ['expt_antagonist']
+game_names = ['portals']
 
 ## idx 2:
 # game_names = ['expt_push_boulders', 'portals', 'frogs', 'overload', 'expt_antagonist', 'expt_helper'] #boulderdash, boulderchase
