b914134bb4ca5a808aa8e87989bc2da892f21f66
Pedro <tsividis@mit.edu>
2018-08-09 11:29:02 -0400

    ready to run

--- games_to_hyperparameters.py
+++ games_to_hyperparameters.py
@@ -23,9 +23,9 @@
 # game_names = ['expt_antagonist', 'expt_helper', 'portals', 'med_boulderdash']
 
 # ## idx 3
-game_names = ['frogs']
+# game_names = ['frogs']
 # game_names = ['aliens', 'avoidgeorge', 'butterflies', 'chase', 'missilecommand', 'survivezombies', 'zelda', 'camelRace', 'jaws', 'lemmings', 'myAliens', 'overload', 'plaqueattack', 'waves', 'frogs']
-# game_names = ['plaqueattack', 'waves']
+game_names = ['survivezombies', 'jaws']
 # game_names = ['chopper', 'plaqueattack']
 # game_names = ['zelda', 'jaws', 'waves', 'survivezombies']
 
