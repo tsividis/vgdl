4d537a430bb301aa28c73ef1126b3287ca18be24
Pedro <tsividis@mit.edu>
2018-08-13 12:09:47 -0400

    ready to run

--- games_to_hyperparameters.py
+++ games_to_hyperparameters.py
@@ -11,7 +11,7 @@
 
 
 
-game_names = ['expt_preconditions']
+game_names = ['frogs']
 
 ## idx 1:
 # game_names = ['expt_push_boulders', 'portals', 'frogs', 'overload', 'expt_antagonist', 'expt_helper'] 
