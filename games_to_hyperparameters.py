76cfffa7b704633728950529741c827950b96db6
Pedro <tsividis@mit.edu>
2018-08-23 13:01:11 -0400

    ready to run

--- games_to_hyperparameters.py
+++ games_to_hyperparameters.py
@@ -11,11 +11,11 @@
 
 
 ## low memory
-game_names = ['aliens', 'avoidgeorge',  'butterflies', 'chase', 'missilecommand', 'survivezombies', 'zelda', 'camelRace', 'lemmings', 'myAliens', 
-			'plaqueattack', 'waves', 'frogs', 'portals', 'expt_ee','expt_push_boulders', 'sokoban', 'bait']
+# game_names = ['aliens', 'avoidgeorge',  'butterflies', 'chase', 'missilecommand', 'survivezombies', 'zelda', 'camelRace', 'lemmings', 'myAliens', 
+			# 'plaqueattack', 'waves', 'frogs', 'portals', 'expt_ee','expt_push_boulders', 'sokoban', 'bait']
 
 ## more memory
-# game_names = ['expt_antagonist', 'expt_helper', 'expt_preconditions', 'expt_relational', 'jaws', 'med_boulderdash', 'watergame']
+game_names = ['expt_antagonist', 'expt_helper', 'expt_preconditions', 'expt_relational', 'jaws', 'med_boulderdash', 'watergame']
 # game_names = ['med_boulderdash', 'boulderdash']
 
 ##low-memory games where position_score seems to affect results
