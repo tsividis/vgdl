b51359d1da136b4610c1ef254dc08d963e0f7d56
Pedro <tsividis@mit.edu>
2018-08-22 21:17:45 -0400

    writing to short_horizon2

--- model_batch.sh
+++ model_batch.sh
@@ -3,7 +3,7 @@
 
 #SBATCH --job-name=run_vgdl_model
 #SBATCH --array=0
-#SBATCH --output=slurm_logs/short_horizon/array_%A_%a.out
+#SBATCH --output=slurm_logs/short_horizon2/array_%A_%a.out
 #SBATCH --time=1480
 #SBATCH --qos=tenenbaum
 #SBTACH --cpus-per-task=2
@@ -45,5 +45,5 @@ fi
 
 # finally, run the model
 # singularity exec  -B "/$BASE:/$BASE" $CONT python -m vgdl.load_games --game_number $GAME_NUMBER --hyperparameter_index $HYPER_IDX
-singularity exec  -B "/$BASE:/$BASE" $CONT python -m vgdl.load_games --game_number $GAME_NUMBER --hyperparameter_index 3
+singularity exec  -B "/$BASE:/$BASE" $CONT python -m vgdl.load_games --game_number $GAME_NUMBER --hyperparameter_index 1
 #echo "-m vgdl.load_games --game_name ${GAME_NAME} --hyperparameter_index ${HYPER_IDX}"
