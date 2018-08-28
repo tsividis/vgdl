0fd1817b7aa63969c0d77d7da3c81ed9c7ba5af6
Pedro <tsividis@mit.edu>
2018-08-28 17:53:48 -0400

    ready to run

--- model_batch.sh
+++ model_batch.sh
@@ -4,7 +4,7 @@
 #SBATCH --job-name=run_vgdl_model
 #SBATCH --array=0-16
 #SBATCH --output=slurm_logs/short_horizon/array_%A_%a.out
-#SBATCH --time=480
+#SBATCH --time=1480
 #SBATCH --qos=tenenbaum
 #SBTACH --cpus-per-task=2
 #SBATCH --mem=16G
