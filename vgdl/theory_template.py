f11030a1f2c755bf1841178ad6a7dff63e6a6641
Pedro <tsividis@mit.edu>
2018-08-28 17:51:34 -0400

    fixed expt_ee descriptions

--- vgdl/theory_template.py
+++ vgdl/theory_template.py
@@ -411,7 +411,7 @@ class Theory(object):
 					# interpretation = self.interpret(event)
 					theories.extend(self.addRules(event))
 				else:
-					embed()
+					# embed()
 					theories.extend(self.addPreconditions(event, timestep, timesteps))
 			# Add new rule
 			elif failCase == 4:
