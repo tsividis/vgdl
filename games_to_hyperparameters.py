3176fc2e1629f1d3091803471f7fd1e623cd31a9
Pedro <tsividis@mit.edu>
2018-08-28 01:59:10 -0400

    ready to run

--- games_to_hyperparameters.py
+++ games_to_hyperparameters.py
@@ -15,8 +15,8 @@
 			 # 'frogs', 'portals', 'expt_ee','expt_push_boulders', 'sokoban', 'bait', 'd2']
 
 ## more memory
-# game_names = ['avoidgeorge', 'expt_antagonist', 'expt_helper', 'expt_preconditions', 'expt_relational', 'd3', 'jaws', 'med_boulderdash', 'watergame']
-game_names = ['d3']
+game_names = ['avoidgeorge', 'expt_antagonist', 'expt_helper', 'expt_preconditions', 'expt_relational', 'd3', 'jaws', 'med_boulderdash', 'watergame']
+# game_names = ['d3']
 
 
 ##low-memory games where position_score seems to affect results
