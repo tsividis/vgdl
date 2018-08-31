dbeb8ee888339310e3935a8bdf90c45a335dd2c6
Pedro <tsividis@mit.edu>
2018-08-31 01:33:28 -0400

    normal hyperparameter_index, running on short_horizon

--- vgdl/WBP.py
+++ vgdl/WBP.py
@@ -82,7 +82,7 @@ class WBP():
 		self.hypotenuse_squared = self.rle.outdim[0]**2 + self.rle.outdim[1]**2
 		
 		if self.hyperparameter_index == 1:
-			self.position_score_multiplier = 0#-10
+			self.position_score_multiplier = -10
 		elif self.hyperparameter_index == 3:
 			self.position_score_multiplier = -10
 			# self.position_score_multiplier = random.choice([-100, -1000])
