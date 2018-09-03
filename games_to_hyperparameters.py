87f8a9d372f76d78789504e804af389e3adf5642
Pedro <tsividis@mit.edu>
2018-09-03 18:23:37 -0400

    Added some variant games

--- games_to_hyperparameters.py
+++ games_to_hyperparameters.py
@@ -14,9 +14,17 @@
 # game_names = ['aliens', 'butterflies', 'chase', 'missilecommand', 'survivezombies', 'zelda', 'camelRace', 'lemmings', 'myAliens', 'plaqueattack',
 			 # 'frogs', 'expt_ee','expt_push_boulders', 'sokoban', 'd2']
 
-## more memory
-game_names = ['avoidgeorge', 'expt_antagonist', 'expt_helper', 'expt_preconditions', 'expt_relational', 'jaws', 'boulderdash', 'portals', 'watergame', 'bait']
+## variants -- low memory
+game_names = ['variant_aliens_1', 'variant_aliens_2', 'variant_aliens_3', 'variant_aliens_4', 
+			  'variant_butterflies_1', 'variant_butterflies_2', 'variant_chase_1', 'variant_myAliens_1'
+			  'variant_frogs_1']
 
+## more memory
+# game_names = ['avoidgeorge', 'expt_antagonist', 'expt_helper', 'expt_preconditions', 'expt_relational', 'jaws', 'boulderdash', 'portals', 'watergame', 'bait']
+
+## variants -- more memory
+game_names = ['variant_avoidgeorge_1', 'variant_avoidgeorge_2', 'variant_avoidgeorge_3', 'variant_avoidgeorge_4', 
+			'variant_boulderdash_1', 'variant_jaws_1', 'variant_watergame_1', 'variant_watergame_2']
 # game_names = ['watergame', 'bait', 'sokoban']
 ##low-memory games where position_score seems to affect results
 # game_names = ['avoidgeorge', 'med_boulderdash', 'survivezombies', 'zelda', 'frogs']
