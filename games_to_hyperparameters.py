0a762d77e68499014bf6a00c2b48fa08b8ff8b99
Pedro <tsividis@mit.edu>
2018-09-06 19:04:21 -0400

    more variants

--- games_to_hyperparameters.py
+++ games_to_hyperparameters.py
@@ -20,9 +20,10 @@
 			  # 'variant_missilecommand_1', 'variant_missilecommand_2', 'variant_myAliens_1', 'variant_zelda_1', 
 			  #'variant_zelda_2', 'variant_zelda_3', 'variant_plaqueattack_1']
 
-game_names = ['variant_survivezombies_1', 'variant_survivezombies_2', 'variant_chase_2', 'variant_lemmings_2',
-			'variant_lemmings_3', 'd2']
+# game_names = ['variant_survivezombies_1', 'variant_survivezombies_2', 'variant_chase_2', 'variant_lemmings_2',
+			# 'variant_lemmings_3', 'd2']
 
+game_names = ['variant_expt_ee_1']
 ## more memory
 # game_names = ['avoidgeorge', 'expt_antagonist', 'expt_helper', 'expt_preconditions', 'expt_relational', 'jaws', 'boulderdash', 'portals', 'watergame', 'bait']
 
