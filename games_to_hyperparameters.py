ffdcf7e09c76a200bb952590142b14358ca012de
Pedro <tsividis@mit.edu>
2018-09-07 12:16:12 -0400

    ready to run

--- games_to_hyperparameters.py
+++ games_to_hyperparameters.py
@@ -33,8 +33,10 @@
 			#'variant_frogs_1', 'variant_frogs_2', 'variant_portals_1', 'variant_sokoban_1']
 # game_names = ['variant_jaws_2']
 
-game_names = ['variant_lemmings_3', 'variant_chase_2', 'variant_plaqueattack_1', 'variant_expt_antagonist_1',
-			  'variant_expt_helper_1', 'variant_expt_preconditions_1', 'variant_expt_push_boulders_1', 'variant_expt_relational_1']
+# game_names = ['variant_lemmings_3', 'variant_chase_2', 'variant_plaqueattack_1', 'variant_expt_antagonist_1',
+			  # 'variant_expt_helper_1', 'variant_expt_preconditions_1', 'variant_expt_push_boulders_1', 'variant_expt_relational_1']
+
+game_names = ['variant_lemmings_3']
 
 # game_names = ['watergame', 'bait', 'sokoban']
 ##low-memory games where position_score seems to affect results
