4ec7e1c256598da7297d6e6476da10e4a760ad7a
Pedro <tsividis@mit.edu>
2018-09-13 15:08:47 -0400

    ready to run

--- games_to_hyperparameters.py
+++ games_to_hyperparameters.py
@@ -38,7 +38,7 @@ game_names = ['variant_missilecommand_3', 'variant_missilecommand_4', 'variant_p
 			#'variant_expt_antagonist_1', # 'variant_expt_helper_1', 'variant_expt_preconditions_1', 'variant_expt_push_boulders_1', 
 			#'variant_expt_relational_1', 'variant_lemmings_3']
 
-game_names = ['variant_frogs_4', 'variant_expt_push_boulders_2', 'variant_expt_relational_2']
+# game_names = ['variant_frogs_4', 'variant_expt_push_boulders_2', 'variant_expt_relational_2']
 
 ##128GB!
 # game_names = ['variant_sokoban_1', 'variant_expt_push_boulders_1', 'variant_expt_helper_1']
