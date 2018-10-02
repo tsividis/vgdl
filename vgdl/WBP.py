e08d23732c48140391f26fb02614753906fc4100
Pedro <tsividis@mit.edu>
2018-10-02 16:57:35 -0400

    sorting collision_eff by effect and then by color, to enforce consistency between in-the-head and real game. Prior to this the order was arbitrarily determined.

--- vgdl/WBP.py
+++ vgdl/WBP.py
@@ -108,7 +108,7 @@ class WBP():
 		
 		#################
 		#################
-		self.display = True
+		self.display = False
 
 		if self.theory.classes['avatar'][0].args and 'stype' in self.theory.classes['avatar'][0].args:
 			self.thingWeShoot = self.theory.classes['avatar'][0].args['stype']
@@ -1328,7 +1328,7 @@ class Node():
 		for stype in term.termination.stypes:
 			val += self.spritecounter_val(theory, term, stype, rle,
 				first_alpha=first_alpha, second_alpha=second_alpha, surrogate_multisprite_counter=True)
-		val /= len(term.termination.stypes)**2
+		val /= len(term.termination.stypes)#**2
 			# print stype, val
 		# val /= 10**len(term.termination.stypes)**2
 		return val
