fd0eb7903b3f2c9cc035c66d78489280ba6aa678
Pedro <tsividis@mit.edu>
2018-10-05 16:46:22 -0400

    games_to_hyperparameters cleanup

--- games_to_hyperparameters.py
+++ games_to_hyperparameters.py
@@ -1,111 +1,36 @@
-# 
-# game_names = ['expt_antagonist', 'expt_helper', 'expt_push_boulders',	'expt_preconditions',					  #0-3
-#                 'aliens', 'boulderdash', 'butterflies', 'chase', 'frogs',                   #5-8
-#                 'missilecommand', 'portals', 'sokoban', 'survivezombies',                   #9-12
-#                 'zelda',                                                                    #13
-                    
-#                 'avoidgeorge', 'bait', 'boulderchase',                   #14-17
-#                 'camelRace', 'chopper', 'jaws', 'lemmings', 'myAliens',		                     	 #18-21
-#                  'overload', 'plaqueattack','watergame',	    		 #22-25
-#                  'waves']                               	 #26-27
 
+## low memory -- 16gb
+game_names = ['aliens', 'variant_aliens_1', 'variant_aliens_2', 'variant_aliens_3', 'variant_aliens_4', 
+'bees_and_birds','variant_bees_and_birds_1',  
+'butterflies', 'variant_butterflies_1', 'variant_butterflies_2', 
+'chase', 'variant_chase_1', 'variant_chase_2',
+'closing_gates', 'variant_closing_gates_1',
+'corridor','variant_corridor_1', 
+'expt_ee', 'variant_expt_ee_1', 'variant_expt_ee_2', 'variant_expt_ee_3', 
+'frogs', 
+'lemmings', 'variant_lemmings_1',  'variant_lemmings_2', 'variant_lemmings_3',
+'missilecommand', 'variant_missilecommand_1', 'variant_missilecommand_2', 'variant_missilecommand_3', 'variant_missilecommand_4',
+ 'myAliens',  'variant_myAliens_1', 'variant_myAliens_2',
+'survivezombies', 'variant_survivezombies_1', 'variant_survivezombies_2'
+ 'zelda','variant_zelda_1', 'variant_zelda_2', 'variant_zelda_3']
 
-## low memory
-# game_names = ['aliens', 'butterflies', 'chase', 'missilecommand', 'survivezombies', 'zelda', 'camelRace', 'lemmings', 'myAliens', 'plaqueattack',
-			 # 'frogs', 'expt_ee','expt_push_boulders', 'sokoban', 'bees_and_birds']
+## more memory -- 64gb
 
-## variants -- low memory
-# game_names = ['variant_aliens_1', 'variant_aliens_2', 'variant_aliens_3', 'variant_aliens_4', 
-			  # 'variant_butterflies_1', 'variant_butterflies_2', 'variant_chase_1', 'variant_lemmings_1', 
-			  # 'variant_missilecommand_1', 'variant_missilecommand_2', 'variant_myAliens_1', 'variant_zelda_1', 
-			  #'variant_zelda_2', 'variant_zelda_3', 'variant_plaqueattack_1', 'variant_expt_ee_1']
+game_names = ['avoidgeorge', 'variant_avoidgeorge_1', 'variant_avoidgeorge_2', 'variant_avoidgeorge_3', 'variant_avoidgeorge_4',
+'bait', 'variant_bait_1', 'variant_bait_2',
+'boulderdash','variant_boulderdash_1',
+'variant_chase_3', 
+'expt_antagonist', 'variant_expt_antagonist_1','variant_expt_antagonist_2', 
+'expt_helper', 'variant_expt_helper_1' 'variant_expt_helper_2', 
+'expt_preconditions', 'variant_expt_preconditions_1', 'variant_expt_preconditions_2', 
+'expt_push_boulders', 'variant_expt_push_boulders_1', 'variant_expt_push_boulders_2', 
+'expt_relational', 'variant_expt_relational_1', 'variant_expt_relational_2',
+'variant_frogs_1', 'variant_frogs_2', 'variant_frogs_3',
+'jaws', 'variant_jaws_1', 'variant_jaws_2',
+'portals','variant_portals_1', 'variant_portals_2',
+'plaqueattack', 'variant_plaqueattack_1', 'variant_plaqueattack_2', 'variant_plaqueattack_3',
+'sokoban', 'variant_sokoban_1', 'variant_sokoban_2',
+'watergame', 'variant_watergame_1', 'variant_watergame_2']
 
-# game_names = ['variant_survivezombies_1', 'variant_survivezombies_2', 'variant_chase_2', 'variant_lemmings_2',
-			# 'variant_lemmings_3', 'bees_and_birds']
-
-# game_names = ['variant_missilecommand_3', 'variant_missilecommand_4', 'variant_plaqueattack_2', 'variant_plaqueattack_3', 
-			# 'bees_and_birds', 'variant_bees_and_birds_1', 'variant_closing_gates_1','variant_corridor_1',
-			  # 'lightthefuse', 'variant_lightthefuse_1', 'variant_lightthefuse_2']
-# game_names = ['variant_watergame_1', 'variant_watergame_2']
-
-# game_names = ['variant_expt_helper_2', 'lightthefuse', 'variant_lightthefuse_1', 'variant_lightthefuse_2', 'variant_portals_2']
-game_names = ['scoretest']
-
-## more memory
-# game_names = ['avoidgeorge', 'expt_antagonist', 'expt_helper', 'expt_preconditions', 'expt_relational', 'jaws', 'boulderdash', 'portals', 'watergame', 'bait']
-# game_names = ['lightthefuse', 'variant_lightthefuse_1', 'variant_lightthefuse_2', 'variant_expt_helper_2', 'variant_plaqueattack_1', 'variant_portals_2']
-# game_names = ['lightthefuse', 'variant_lightthefuse_1', 'variant_lightthefuse_2']
-
-# game_names = ['plaqueattack', 'variant_plaqueattack_1', 'variant_plaqueattack_2', 'variant_plaqueattack_3']
-
-## variants -- more memory
-# game_names = ['variant_avoidgeorge_1', 'variant_avoidgeorge_2', 'variant_avoidgeorge_3', 'variant_avoidgeorge_4', 
-			# 'variant_boulderdash_1', 'variant_jaws_1', 'variant_watergame_1', 'variant_watergame_2',
-			#'variant_frogs_1', 'variant_frogs_2', 'variant_portals_1', 'variant_sokoban_1',
-			#'variant_jaws_2', 'variant_lemmings_3', 'variant_chase_2', 'variant_plaqueattack_1',
-			#'variant_expt_antagonist_1', # 'variant_expt_helper_1', 'variant_expt_preconditions_1', 'variant_expt_push_boulders_1', 
-			#'variant_expt_relational_1', 'variant_lemmings_3']
-
-# game_names = ['variant_frogs_4', 'variant_expt_push_boulders_2', 'variant_expt_relational_2']
-
-# game_names = ['variant_expt_helper_2', 'variant_plaqueattack_1']
-
-##128GB!
-# game_names = ['variant_sokoban_1', 'variant_expt_push_boulders_1', 'variant_expt_helper_1']
-
-# game_names = ['watergame', 'bait', 'sokoban']
-##low-memory games where position_score seems to affect results
-# game_names = ['avoidgeorge', 'med_boulderdash', 'survivezombies', 'zelda', 'frogs']
-##high-memory games where position_score seems to affect results
-# game_names = ['expt_relational', 'jaws', 'watergame', 'bait', 'portals']
-
-# game_names = ['zelda', 'bait']
-
-# game_names = ['expt_helper']
-
-
-
-## idx 2, normal
-# game_names = ['expt_antagonist', 'expt_helper', 'portals', 'med_boulderdash']
-
-# ## idx 3
-# game_names = ['frogs']
-# game_names = ['aliens', 'avoidgeorge', 'butterflies', 'chase', 'missilecommand', 'survivezombies', 'zelda', 'camelRace', 'jaws', 'lemmings', 'myAliens', 'overload', 'plaqueattack', 'waves', 'frogs', 'portals']
-# game_names = ['survivezombies', 'jaws']
-# game_names = ['chopper', 'plaqueattack']
-# game_names = ['zelda', 'jaws', 'waves', 'survivezombies']
-
-# ## idx 3, more memory
-# game_names = ['chopper']
-
-# game_names = ['expt_push_boulders', 'aliens', 'butterflies', 'expt_helper', 'frogs', 'portals', 'survivezombies', 'zelda']
-# game_names = ['boulderchase', 'jaws', 'myAliens', 'overload', 'avoidgeorge']
-
-##running with hyperparams2
-# game_names = ['avoidgeorge', 'bait', 'boulderchase', 'chopper', 'clusters',
-# 				jaws', 'lemmings',
-# 				'myAliens', 'overload', 'plants', 'plaqueattack', 
-# 				'superman', 'watergame', 'waves', 'wildgunman']
-
-## running with hyperparams2
-# game_names = ['bait', 'camelRace', 'lemmings', 'watergame','boulderchase', 'boulderdash']
-
-## running with hyperparams3
-# game_names = ['myAliens'] ##running for longer
-# game_names = ['avoidgeorge', 'jaws','boulderchase', 'boulderdash', 'chopper', 'myAliens', 'plaqueattack', 'waves', 'overload', 'missilecommand', 'lemmings', 'superman']
-# game_names = ['avoidgeorge', 'bait', 'chopper', 
-				# 'infection', 'jaws', 'myAliens', 'overload', 'plants', 'plaqueattack', 
-				# 'superman', 'waves', 'wildgunman']
-
-# game_names = ['angelsdemons', 'waves', 'myAliens', 'jaws']
-
-## to run after local debugging:
-# ['boulderchase']
-# mapping = dict()
-
-# for k in game_names:
-#     if k not in ['infection', 'lemmings', 'modality', 'plants', 'tercio', 'waves']: #[need much looking into]
-#         mapping[k] = [0,2,3]
-
-# for k in game_names:
-    # mapping[k] = [3]
+## lots of memory -- 128gb??
+# game_names = ['bait','sokoban', 'variant_sokoban_1', 'variant_expt_push_boulders_1', 'variant_expt_helper_1', 'watergame']
