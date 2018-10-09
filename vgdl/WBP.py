a7f510245a7c5186f1d9034e214ee207098f9aba
Pedro <tsividis@mit.edu>
2018-10-09 10:19:47 -0400

    ready to run (wbp.display=True)

--- vgdl/WBP.py
+++ vgdl/WBP.py
@@ -107,7 +107,7 @@ class WBP():
 
 		#################
 		#################
-		self.display = False
+		self.display = True
 
 		if self.display:
 			print "In planner; MovingTypesInGame: {}. Planning with idx {} and position_multiplier {}".format(movingTypesInGame, self.hyperparameter_index, self.position_score_multiplier)
