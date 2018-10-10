2054de45a51c2ff5548a7314b7a2b418951d7fee
Pedro <tsividis@mit.edu>
2018-10-10 09:22:49 -0400

    ready to run

--- games_to_hyperparameters.py
+++ games_to_hyperparameters.py
@@ -32,19 +32,22 @@
 # 'sokoban', 'variant_sokoban_1', 'variant_sokoban_2',
 # 'watergame', 'variant_watergame_1', 'variant_watergame_2'] ##45
 
-game_names = ['boulderdash', 'variant_boulderdash_1']
 
-# game_names = ['aliens', 'variant_aliens_1', 'variant_aliens_2', 'variant_aliens_3', 'variant_aliens_4',
-# 			  'avoidgeorge',	
-# 			  'butterflies', 'variant_butterflies_1', 'variant_butterflies_2', 
-# 			  'boulderdash', 'variant_boulderdash_1',
-# 			  'chase', 'variant_chase_1', 'variant_chase_2',
-# 			  'jaws', 'variant_jaws_1', 'variant_jaws_2',
-# 			  'missilecommand', 'variant_missilecommand_1', 'variant_missilecommand_2', 'variant_missilecommand_3', 'variant_missilecommand_4',
-#  			  'myAliens', 'variant_myAliens_1', 'variant_myAliens_2',
-# 			  'plaqueattack', 'variant_plaqueattack_1', 'variant_plaqueattack_2', 'variant_plaqueattack_3',
-# 			  'survivezombies',
-# 			  'zelda']
+game_names = ['variant_avoidgeorge_1', 'variant_avoidgeorge_2', 'variant_avoidgeorge_3', 'variant_avoidgeorge_4',
+'bait', 'variant_bait_1', 'variant_bait_2',
+'variant_chase_3',
+'expt_antagonist', 'variant_expt_antagonist_1','variant_expt_antagonist_2', 
+'expt_helper', 'variant_expt_helper_1', 'variant_expt_helper_2', 
+ 'expt_preconditions', 'variant_expt_preconditions_1', 'variant_expt_preconditions_2',
+ 'expt_push_boulders', 'variant_expt_push_boulders_1', 'variant_expt_push_boulders_2', 
+'expt_relational', 'variant_expt_relational_1', 'variant_expt_relational_2',
+'variant_frogs_1', 'variant_frogs_2', 'variant_frogs_3',
+'portals', 'variant_portals_1', 'variant_portals_2',
+'sokoban', 'variant_sokoban_1', 'variant_sokoban_2',
+'variant_survivezombies_1', 'variant_survivezombies_2',
+'variant_watergame_1', 'variant_watergame_2'
+'variant_zelda_1', 'variant_zelda_2', 'variant_zelda_3']
+
 # game_names = ['lemmings', 'variant_lemmings_2', 'variant_lemmings_3']
 # game_names = ['watergame', 'variant_closing_gates_1']
 # game_names = ['boulderdash', 'variant_boulderdash_1']
