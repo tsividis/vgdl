685e5dcaf9ec9c0dd8ce87fb05fb0199a787f06d
Pedro <tsividis@mit.edu>
2018-10-09 22:09:07 -0400

    ready to run

--- model_batch.sh
+++ model_batch.sh
@@ -2,7 +2,7 @@
 
 
 #SBATCH --job-name=run_vgdl_model
-#SBATCH --array=0-1%31
+#SBATCH --array=0-83%30
 #SBATCH --output=slurm_logs/main/array_%A_%a.out
 #SBATCH --time=1480
 #SBATCH --qos=tenenbaum
