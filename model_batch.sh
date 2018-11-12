5db6dcdd1a04f00bddc398b1bc47fc7d75c9225b
Pedro <tsividis@mit.edu>
2018-11-12 11:45:04 -0500

    ready to run without snapshots

--- model_batch.sh
+++ model_batch.sh
@@ -25,7 +25,7 @@ SRC=""
 DST=""
 
 # Figure out which game and hyperparameter
-N_GAMES=90
+N_GAMES=3
 # N_PARAMS=3
 GAME_NUMBER=$(($SLURM_ARRAY_TASK_ID % $N_GAMES))
 # IW=$(($SLURM_ARRAY_TASK_ID / 90 + 1))
