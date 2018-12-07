7e76036545dfb7f12882ffd75c3a169fd67d8a3a
Pedro <tsividis@mit.edu>
2018-12-07 12:14:54 -0500

    modified default IW

--- games_to_hyperparameters.py
+++ games_to_hyperparameters.py
@@ -1,5 +1,5 @@
 
-low memory -- 16gb
+# low memory -- 16gb
 game_names = ['aliens', 'variant_aliens_1', 'variant_aliens_2', 'variant_aliens_3', 'variant_aliens_4', 
 'avoidgeorge', 'variant_avoidgeorge_1', 'variant_avoidgeorge_2', 'variant_avoidgeorge_3', 'variant_avoidgeorge_4',
 'bait', 'variant_bait_1', 'variant_bait_2',
