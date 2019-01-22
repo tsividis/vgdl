2d704c4bed1682027dd85d6b77e6efee8338193b
Pedro <tsividis@mit.edu>
2019-01-22 16:58:20 -0500

    ready to run

--- analysis.py
+++ analysis.py
@@ -77,7 +77,6 @@ def process_model_run(data, modelrun_ID):
 	# else:
 	# 	agent_type = 'normal'
 	# print "you've modified agent_type to test a single thing, but you need to remove this modification"
-	# embed()
 	condition = data['condition'] if 'condition' in data.keys() else 'full'
 	game_name = data['gameInfo']['gameName']
 	cumulative_timestep, cumulative_max_score, sparse_score, cumulative_wins, cumulative_planner_nodes = 0,0,0,0,0
