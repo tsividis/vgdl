4e7f2f1fab965b9c39be92eeb2166e50536319b6
Pedro <tsividis@mit.edu>
2019-01-28 11:41:13 -0500

    r stuff

--- vgdl_analyses_oct2018.R
+++ vgdl_analyses_oct2018.R
@@ -36,7 +36,7 @@ original_games = c('aliens', 'antagonist', 'avoidgeorge', 'bait', 'bees_and_bird
 ## nov28: AGH+IW planner lesion
 dates = c('nov8', 'nov12', 'nov15', 'nov16', 'dec4')
 ## warning: don't plot frogs from anything before nov13b
-dates = list('jan27')
+dates = list('jan28')
 saveddata = data
 data = list()
 for (date in dates){
@@ -93,8 +93,10 @@ for (colname in names(data)){
   }
 }
 
-lesion_games = levels(data$game_name)
-data = rbind(data, subset(MEPdata3, game_name%in%lesion_games))
+representation_lesion_games = c('aliens', 'butterflies', 'chase', 'corridor', 'helper', 'frogs', 'portals', 'sokoban', 'survivezombies', 'zelda')
+data = subset(data, grepl('PL=', agent_type)&game_name%in%representation_lesion_games)
+
+data = rbind(data, subset(MEPdata3, game_name%in%representation_lesion_games))
 
 for (colname in names(humandata)){
   if (!(colname %in% names(data))){
@@ -108,7 +110,7 @@ for (colname in names(data)){
     print(colname)
   }
 }
-data = rbind(data, subset(humandata, game_name%in%lesion_games))
+data = rbind(data, subset(humandata, game_name%in%representation_lesion_games))
 
 p=ggplot(subset(data, game_name%in%lesion_games), aes(x=cumulative_steps, y=cumulative_wins,color=agent_type))
 p=p+geom_point()+geom_jitter()+geom_smooth(span=.5, se=FALSE, size=1, alpha=0.5)+theme(legend.position="none")+facet_wrap(~game_name)
@@ -1120,6 +1122,9 @@ for (i in 1:length(levels(dataframe$game_name))){
   if (game_name %in% c('expt_preconditions', 'expt_preconditions_2')){
     num_levels=5
   }
+  if (game_name == 'entropy'){
+    num_levels=2
+  }
   new = data.frame(game_name=game_name, num_levels=num_levels)
   games_to_levels = rbind(games_to_levels, new)
 }
@@ -1130,11 +1135,11 @@ plantimedata = data.frame(game_name=as.character(), agent_type=as.character(), m
                           max_steps=as.numeric(), planning_time=as.numeric(), max_levels_won=as.numeric(),
                           level_num=as.numeric(), level_efficiency=as.numeric())
 
-for (j in 1:length(levels(dataframe$agent_type))){
-  agent = levels(dataframe$agent_type)[j]
+for (j in 1:length(unique(dataframe$agent_type))){
+  agent = unique(dataframe$agent_type)[j]
   print(agent)
-  for (i in 1:length(levels(dataframe$game_name))){
-    g = subset(dataframe, game_name==levels(dataframe$game_name)[i])
+  for (i in 1:length(unique(dataframe$game_name))){
+    g = subset(dataframe, game_name==unique(dataframe$game_name)[i])
     print(levels(dataframe$game_name)[i])
     s = subset(g, agent_type==agent)
     if (length(s$level_max_score)>0){
@@ -1189,11 +1194,11 @@ plantimedata = mutate(plantimedata, plan_nodes_per_step=planning_time/max_steps)
 plantimedata$human_normed_composite_ratio=NA
 plantimedata$EMPA_normed_composite_ratio=NA
 ##norm by human level_efficiency
-for (i in 1:length(levels(plantimedata$game_name))){
-  game = levels(plantimedata$game_name)[i]
+for (i in 1:length(unique(plantimedata$game_name))){
+  game = unique(plantimedata$game_name)[i]
   human_composite_ratio = subset(plantimedata, agent_type=='human' & game_name==game)$composite_ratio
   EMPA_composite_ratio = subset(plantimedata, agent_type=="IW=1_ea=True_sh=500_lh=1000_sha=1.05_lha=2.0_shr=[200, 500, 1000]_nF=True_abmax=50000_lR=True_eG=False_sTE=1000_hyb=False"  & game_name==game)$composite_ratio
-  for (agent in levels(plantimedata$agent_type)){
+  for (agent in unique(plantimedata$agent_type)){
     row = plantimedata[which(plantimedata$agent_type==agent & plantimedata$game_name==game),]
     plantimedata[which(plantimedata$agent_type==agent & plantimedata$game_name==game),]$human_normed_composite_ratio = row$composite_ratio/human_composite_ratio
     plantimedata[which(plantimedata$agent_type==agent & plantimedata$game_name==game),]$EMPA_normed_composite_ratio = row$composite_ratio/EMPA_composite_ratio
@@ -1210,9 +1215,11 @@ for (i in 1:length(plantimedata$human_normed_composite_ratio)){
   }
 }
 
-representation_lesion_names = c('No Chaser', 'No Missile', 'Missing Speeds', 'No Push', 'No Clone', 'No Destroy', 'No Pull', 'No Teleport', 'No Transform', 'EMPA')
+representation_lesion_names = c('No Chaser', 'No Missile','No Push','No Pull',  'No Destroy','No Clone','No Transform','No Teleport', 'Missing Speeds', 'EMPA')
 
-p=ggplot(subset(plantimedata, !(agent_type%in%c('human', levels(plantimedata$agent_type)[10], levels(plantimedata$agent_type)[12]))), aes(agent_type, game_name, fill=log(EMPA_normed_composite_ratio)))+geom_tile()+
+p=ggplot(subset(plantimedata, game_name!='sokoban' & !(agent_type%in%c('human', 
+                            as.character(unique(plantimedata$agent_type)[9]), as.character(unique(plantimedata$agent_type)[11]))))
+                , aes(agent_type, game_name, fill=log(EMPA_normed_composite_ratio)))+geom_tile()+
   # +  theme(axis.text=element_blank(),axis.ticks= element_blank(),axis.title = element_blank(),panel.background = element_blank())
   theme(axis.text.x = element_text(angle = 90, hjust = 1),axis.title = element_blank())+scale_fill_gradient2(low='red', midpoint=0, mid='white', high='green')+
   scale_x_discrete(labels=representation_lesion_names)
