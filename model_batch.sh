aa719671149f710b91787d25fd220f4484a5c60a
Pedro <tsividis@mit.edu>
2019-03-25 11:30:38 -0400

    changed qos to tennebaum temporarily

--- model_batch.sh
+++ model_batch.sh
@@ -4,7 +4,7 @@
 #SBATCH --array=0-1
 #SBATCH --output=slurm_logs/main/array_%A_%a.out
 #SBATCH --time=1680
-#SBATCH --qos=use-everything
+#SBATCH --qos=tenenbaum
 #SBTACH --cpus-per-task=2
 #SBATCH --mem=16G
 #SBATCH --requeue
