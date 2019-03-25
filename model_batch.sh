1c4f3e52a873ffa0fd262444bb93025a826b79b2
Pedro <tsividis@mit.edu>
2019-03-25 11:31:11 -0400

    changed qos back

--- model_batch.sh
+++ model_batch.sh
@@ -4,7 +4,7 @@
 #SBATCH --array=0-1
 #SBATCH --output=slurm_logs/main/array_%A_%a.out
 #SBATCH --time=1680
-#SBATCH --qos=tenenbaum
+#SBATCH --qos=use-everything
 #SBTACH --cpus-per-task=2
 #SBATCH --mem=16G
 #SBATCH --requeue
