ee793383d4b705d6dff6d13e353c54343106c9b4
Pedro <tsividis@mit.edu>
2019-06-05 10:51:34 -0400

    Now only running the remainder of games that need 10x runs

--- games_to_hyperparameters.py
+++ games_to_hyperparameters.py
@@ -35,16 +35,15 @@ game_names = ['variant_bees_and_birds_1', 'variant_closing_gates_1', 'butterflie
 'variant_expt_antagonist_1', 'variant_expt_antagonist_2', 'variant_expt_ee_2', 'variant_expt_preconditions_1', 'variant_expt_preconditions_2',
 'variant_expt_push_boulders_1', 'variant_expt_push_boulders_2', 'variant_expt_relational_1', 'variant_expt_relational_2',
 'variant_missilecommand_1','variant_missilecommand_2', 'variant_missilecommand_3', 'variant_missilecommand_4', 'variant_survivezombies_1', 'variant_survivezombies_2',
-
-### 5x
-'aliens','avoidgeorge','bait','bees_and_birds','boulderdash','closing_gates','corridor','expt_antagonist','expt_ee','expt_helper',
- 'expt_preconditions','expt_push_boulders','expt_relational','frogs','jaws','lemmings','myAliens','plaqueattack','portals','sokoban','surprise','survivezombies',
- 'variant_aliens_1','variant_aliens_2','variant_aliens_3','variant_aliens_4','variant_avoidgeorge_1','variant_avoidgeorge_2','variant_avoidgeorge_3','variant_avoidgeorge_4',
- 'variant_bait_1','variant_bait_2','variant_boulderdash_1','variant_boulderdash_2','variant_butterflies_1','variant_chase_2','variant_chase_3','variant_corridor_1',
- 'variant_expt_ee_1','variant_expt_ee_3','variant_expt_helper_1','variant_expt_helper_2','variant_frogs_1','variant_frogs_2','variant_frogs_3','variant_jaws_1',
- 'variant_jaws_2','variant_lemmings_1','variant_lemmings_2','variant_lemmings_3','variant_myAliens_1','variant_myAliens_2','variant_plaqueattack_1',
- 'variant_plaqueattack_2','variant_plaqueattack_3','variant_portals_1','variant_portals_2','variant_sokoban_1','variant_sokoban_2','variant_surprise_1',
- 'variant_surprise_2','variant_watergame_1','variant_watergame_2','variant_zelda_1','variant_zelda_2','variant_zelda_3','watergame','zelda'
+# ### 5x
+# 'aliens','avoidgeorge','bait','bees_and_birds','boulderdash','closing_gates','corridor','expt_antagonist','expt_ee','expt_helper',
+#  'expt_preconditions','expt_push_boulders','expt_relational','frogs','jaws','lemmings','myAliens','plaqueattack','portals','sokoban','surprise','survivezombies',
+#  'variant_aliens_1','variant_aliens_2','variant_aliens_3','variant_aliens_4','variant_avoidgeorge_1','variant_avoidgeorge_2','variant_avoidgeorge_3','variant_avoidgeorge_4',
+#  'variant_bait_1','variant_bait_2','variant_boulderdash_1','variant_boulderdash_2','variant_butterflies_1','variant_chase_2','variant_chase_3','variant_corridor_1',
+#  'variant_expt_ee_1','variant_expt_ee_3','variant_expt_helper_1','variant_expt_helper_2','variant_frogs_1','variant_frogs_2','variant_frogs_3','variant_jaws_1',
+#  'variant_jaws_2','variant_lemmings_1','variant_lemmings_2','variant_lemmings_3','variant_myAliens_1','variant_myAliens_2','variant_plaqueattack_1',
+#  'variant_plaqueattack_2','variant_plaqueattack_3','variant_portals_1','variant_portals_2','variant_sokoban_1','variant_sokoban_2','variant_surprise_1',
+#  'variant_surprise_2','variant_watergame_1','variant_watergame_2','variant_zelda_1','variant_zelda_2','variant_zelda_3','watergame','zelda'
 ]
 
 
