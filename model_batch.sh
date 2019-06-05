4a26d41c96b6c9555e187d90851f29ac66e8f6d1
Pedro <tsividis@mit.edu>
2019-06-05 10:52:51 -0400

    Now only running the remainder of games that need 10x runs

--- model_batch.sh
+++ model_batch.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #SBATCH --job-name=run_vgdl_model
-#SBATCH --array=0-90
+#SBATCH --array=0-22
 #SBATCH --output=slurm_logs/main/array_%A_%a.out
 #SBATCH --time=1440
 #SBATCH --qos=use-everything
@@ -27,7 +27,7 @@ SRC=""
 DST=""
 
 # Figure out which game and hyperparameter
-N_GAMES=90
+N_GAMES=22
 # N_PARAMS=3
 GAME_NUMBER=$(($SLURM_ARRAY_TASK_ID % $N_GAMES))
 # META_IDX=$(($SLURM_ARRAY_TASK_ID / $N_GAMES))
