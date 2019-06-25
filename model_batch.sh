0c1eb462f8c98821121cf979e12d10fc4bd3ed3e
Pedro <tsividis@mit.edu>
2019-06-25 16:04:27 -0400

    ready to run e-greedy 2k DS on 3 games

--- model_batch.sh
+++ model_batch.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #SBATCH --job-name=run_vgdl_model
-#SBATCH --array=0-90
+#SBATCH --array=0-3
 #SBATCH --output=slurm_logs/main/array_%A_%a.out
 #SBATCH --time=1440
 #SBATCH --qos=use-everything
@@ -32,7 +32,7 @@ DST=""
 #
 
 # Figure out which game and hyperparameter
-N_GAMES=90
+N_GAMES=3
 # N_PARAMS=3
 GAME_NUMBER=$(($SLURM_ARRAY_TASK_ID % $N_GAMES))
 META_IDX=$(($SLURM_ARRAY_TASK_ID / $N_GAMES))
@@ -55,6 +55,6 @@ fi
 ## to run conditioned on some exploration
 # singularity exec  -B "/$BASE:/$BASE" $CONT python -m vgdl.load_games --game_number $GAME_NUMBER --hyperparameter_index 3 --metacontroller_index $META_IDX --IW 2 --extra_atom_allowed True --task_ID $SLURM_ARRAY_TASK_ID --make_movie False --pickled_theory_path 1
 ## to run epsilon-greedy
-singularity exec  -B "/$BASE:/$BASE" $CONT python -m vgdl.load_games --game_number $GAME_NUMBER --hyperparameter_index 3 --metacontroller_index 1 --IW 1 --extra_atom_allowed True --task_ID $SLURM_ARRAY_TASK_ID --make_movie False --epsilon_greedy True --random_policy False
+singularity exec  -B "/$BASE:/$BASE" $CONT python -m vgdl.load_games --game_number $GAME_NUMBER --hyperparameter_index 3 --metacontroller_index 2 --IW 1 --extra_atom_allowed True --task_ID $SLURM_ARRAY_TASK_ID --make_movie False --epsilon_greedy True --random_policy False
 
 #echo "-m vgdl.load_games --game_name ${GAME_NAME} --hyperparameter_index ${HYPER_IDX}"
