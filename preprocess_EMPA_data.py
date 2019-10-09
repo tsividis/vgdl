c0515236a9b07e4f3b10d78b02dcc1ad82892ab3
PEDRO TSIVIDIS <pedrotsividis@PEDROs-MacBook-Pro.local>
2019-10-08 17:59:53 -0700

    Imported heatmap-making code from other branch

--- preprocess_EMPA_data.py
+++ preprocess_EMPA_data.py
@@ -20,11 +20,14 @@ python -m process_EMPA_data --date vgdl ## for local stuff
 parser = argparse.ArgumentParser(description='.')
 parser.add_argument('--date', type=str, default='apr4', help='date') ##date whose data you want to process
 parser.add_argument('--game', type=str, default='', help='game') ##game whose data you want to process
+parser.add_argument('--heatmap',type=str2bool, default=False)
 
 args = parser.parse_args()
 
 date = args.date
 game = args.game if args.game!='' else None
+heatmap = args.heatmap
+
 relative_path = '../data_files/EMPA_data_files'
 
 path = '{}/{}/results'.format(relative_path, date)
@@ -36,50 +39,95 @@ def process_model_run(data, modelrun_ID):
 	## takes a cPickle file of a full model run
 	## writes a csv
 
+	## you need to count max_score differently, as you want to show the max points someone has gotten, but you don't want to give people
+	## points for continually almost winning a level. 
+	## you want to end up with one csv per game. if you want to look at things across games, you just have to merge those csvs, but this is the cleanest way to do it
+	## and to avoid loading huge csv files.
+	## also don't process a particular run multiple times. you need a way of storing the processed model_IDs so that you don't keep appending to a long csv.
 	modelrun_ID = modelrun_ID[modelrun_ID.find('201'):modelrun_ID.find('201')+11]
+	subject_ID = generate_subject_ID()
+
 	data_path = '{}/{}/{}'.format(relative_path, date, 'csv_data')
- 	if 'csv_data' not in os.listdir('{}/{}'.format(relative_path, date)):
+	if 'csv_data' not in os.listdir('{}/{}'.format(relative_path, date)):
+	# data_path = '{}/{}'.format(date, 'csv_data')
+	# if 'csv_data' not in os.listdir('{}'.format(date)):
 		os.makedirs(data_path)
 
 	if 'merged_data' not in os.listdir(data_path):
 		g = open('{}/merged_data'.format(data_path), 'w+')
 		mergedfilewriter = csv.writer(g)
-		mergedfilewriter.writerow(('agent_type', 'subject_ID', 'modelrun_ID', 'condition', 'exploration_burn_ins', 'game_name', 'level_number', 
-								'timestep', 'cumulative_timestep', 'time_elapsed', 'entropy', 'score', 'level_max_score', 'cumulative_max_score', 
-							'sparse_score', 'level_accumulated_score', 'episode_end', 'win', 'cumulative_wins', 'sparse_levels_won', 'planner_settings', 'planner_nodes', 'cumulative_planner_nodes'))
+		mergedfilewriter.writerow(('agent_type', 'subject_ID', 'modelrun_ID', 'condition', 'exploration_burn_ins', 'game_name', 'level_number', 'timestep', 'cumulative_timestep', 
+							'entropy', 'score', 'level_max_score', 'cumulative_max_score', 
+							'sparse_score', 'level_accumulated_score', 'episode_end', 'win', 'cumulative_wins', 'planner_settings', 'planner_nodes', 'cumulative_planner_nodes'))
 	else:
 		g = open('{}/merged_data'.format(data_path), 'a+')	
 		mergedfilewriter = csv.writer(g)
+
+	if 'interaction_data' not in os.listdir(data_path):
+		h = open('{}/interaction_data'.format(data_path), 'w+')
+		interactionfilewriter = csv.writer(h)
+		interactionfilewriter.writerow(('agent_type', 'subject_ID', 'modelrun_ID', 'game_name', 'level_number', 'episode_number', 'event_type', 'count'))
+	else:
+		h = open('{}/interaction_data'.format(data_path), 'a+')	
+		interactionfilewriter = csv.writer(h)
 
 	game_name = data['gameInfo']['gameName']
 
 	if game_name not in os.listdir(data_path):
 		f = open('{}/{}'.format(data_path, game_name), 'w+') #newfile and write
 		gamefilewriter = csv.writer(f)
-		gamefilewriter.writerow(('agent_type', 'subject_ID', 'modelrun_ID', 'condition', 'exploration_burn_ins', 'game_name', 'level_number', 
-								'timestep', 'cumulative_timestep', 'time_elapsed', 'entropy', 'score', 'level_max_score', 'cumulative_max_score', 
-								'sparse_score','level_accumulated_score', 'episode_end', 'win', 'cumulative_wins', 'sparse_levels_won', 'planner_settings', 'planner_nodes', 'cumulative_planner_nodes'))
+		gamefilewriter.writerow(('agent_type', 'subject_ID', 'modelrun_ID', 'condition', 'exploration_burn_ins', 'game_name', 'level_number', 'timestep', 'cumulative_timestep', 
+							'entropy', 'score', 'level_max_score', 'cumulative_max_score', 
+							'sparse_score','level_accumulated_score', 'episode_end', 'win', 'cumulative_wins', 'planner_settings', 'planner_nodes', 'cumulative_planner_nodes'))
 	else:
 		f = open('{}/{}'.format(data_path, game_name), 'a+') #append and read
 		gamefilewriter = csv.writer(f)
 	
 	agent_type = data['modelParams']
-	subject_ID = generate_subject_ID()
 	exploration_burn_ins = data['exploration_burn_ins'] if 'exploration_burn_ins' in data.keys() else 'NA'
+	# if 'exploration_burn_ins' in data.keys():
+	# 	agent_type = 'lesion'
+	# else:
+	# 	agent_type = 'normal'
+	# print "you've modified agent_type to test a single thing, but you need to remove this modification"
+	# embed()
 	condition = data['condition'] if 'condition' in data.keys() else 'full'
 	game_name = data['gameInfo']['gameName']
+
+	all_event_types = set()
 	cumulative_timestep, cumulative_max_score, sparse_score, cumulative_wins, cumulative_planner_nodes = 0,0,0,0,0
 	prev_level_number = 0
 	accumulated_score = 0 ## at end of each level, you keep whatever score you've picked up.
+	episode_number = 0
+
 	for level_number,level in enumerate(data['episodes']):
+		unpacked_states = [item for sublist in level for item in sublist]
 		level_max_score = 0
-		for episode in level:
+		for episode_num, episode in enumerate(level):
+			episode_events = defaultdict(lambda:0)
 			for t, state in enumerate(episode):
-				timestep, entropy, score, planner_nodes, episode_end, win, planner_settings = state['timestep'], state['entropy'], state['score'], state['planner_nodes'], state['ended'], state['win'], state['planner_settings']
-				time_elapsed = float("{0:6.3f}".format(state['time_elapsed'])) if 'time_elapsed' in state else 'NA'
+				timestep, entropy, score, planner_nodes, episode_end, win, planner_settings, events = state['timestep'], state['entropy'], state['score'], state['planner_nodes'], state['ended'], state['win'], state['planner_settings'], state['events']
+				# if events:
+					# embed()
+				timestep_events = set()
+
+				for e in events:
+					## because event handling is so weird in Frogs, we need to filter out these events.
+					## Avatar-water and avatar-log collisions will still be reported from the (killSprite avatar water) interaction and (pullWithIt avatar log) interaction
+					## which is what a player perceives when they play
+					if e in  [('changeResource', 'avatar', 'water'),('changeResource', 'avatar', 'log')]:
+						pass
+					else:
+						timestep_events.add(tuple(sorted((e[1], e[2]))))
+						all_event_types.add(tuple(sorted((e[1], e[2]))))
+
+				for e in timestep_events:
+					episode_events[e] += 1
+				# for e in events:
+					# episode_events[tuple(sorted((e[1], e[2])))] += .5
+					# all_event_types.add(tuple(sorted((e[1], e[2]))))
 				entropy = round(entropy,4) if entropy is not None else 'NA'
-				score=round(score,2)
-				
+				score = round(score,2)
 				## All this weird stuff needs to be done because we don't have a single-stream game.
 				if level_number > prev_level_number:
 					level_max_score = 0
@@ -91,6 +139,7 @@ def process_model_run(data, modelrun_ID):
 						level_accumulated_score = accumulated_score + score
 					else:
 						level_accumulated_score = 'NA'
+					# level_accumulated_score = accumulated_score + score
 					if score > level_max_score:
 						score_delta = score - level_max_score
 						level_max_score = max(score, level_max_score)
@@ -116,28 +165,35 @@ def process_model_run(data, modelrun_ID):
 				if win:
 					accumulated_score = accumulated_score + score
 					level_accumulated_score = accumulated_score
-					sparse_levels_won = cumulative_wins
-				else:
-					## first level, first round: sparse_levels_won has to be 0
-					if level_number==0 and t==0:
-						sparse_levels_won = 0
-					else:
-						sparse_levels_won = 'NA'
 				cumulative_timestep += 1
 
 				mean_burn_in = np.mean(exploration_burn_ins) if type(exploration_burn_ins)==list else 'NA'
-
-				row = (agent_type, subject_ID, modelrun_ID, condition, mean_burn_in, game_name, level_number, t, cumulative_timestep, time_elapsed, entropy, score, level_max_score, cumulative_max_score,
-						sparse_score, level_accumulated_score, episode_end, win, cumulative_wins, sparse_levels_won, planner_settings, planner_nodes, cumulative_planner_nodes)
-				gamefilewriter.writerow(row)
+				row = (agent_type, subject_ID, modelrun_ID, condition, mean_burn_in, game_name, level_number, t, cumulative_timestep, entropy, score, level_max_score, cumulative_max_score,
+						sparse_score, level_accumulated_score, episode_end, win, cumulative_wins, planner_settings, planner_nodes, cumulative_planner_nodes)
+				# gamefilewriter.writerow(row)
 				mergedfilewriter.writerow(row)
 
 				prev_level_number = level_number
+			for event_name in all_event_types:
+				if event_name not in episode_events:
+					episode_events[event_name] = 0
+			for event_name, count in episode_events.items():
+				interactionfilewriter.writerow((agent_type, subject_ID, modelrun_ID, game_name, level_number, episode_number, event_name, count))
+
+			episode_number += 1
+
+		# if 'ee_3' in game_name:
+		# 	try:
+		# 		makeHeatmap(unpacked_states, agent_type, 'heatmap_{}_level_{}_{}.pdf'.format(game_name, level_number, agent_type))
+		# 	except:
+		# 		print "error with heatmaps"
+		# 		embed()
 
 	f.close()
 	g.close()
+	h.close()
 
-def make_csvs(path, game=None):
+def make_csvs(path, heatmap, game_names = [], game=None):
 	for folder in open_folder(path):
 		for gamefolder in open_folder("{}/{}".format(path,folder)):
 			if game==None or game==gamefolder:
@@ -148,12 +204,17 @@ def make_csvs(path, game=None):
 					with open(modelrun_path, 'r') as o:
 						try:
 							data = cPickle.load(o)
-							process_model_run(data, modelrun_ID)
 						except:
-							print "error with {}, {}, {}...".format(path, folder, modelrun_ID)
-							# embed()
+							print "problem loading pickle for {}".format(modelrun_path)
+						if heatmap:
+							make_heatmaps(data, modelrun_ID, game_names)
+						else:
+							try:
+								process_model_run(data, modelrun_ID)
+							except:
+								print "error..."
+								embed()
 						o.close()
-					# embed()
 
 def generate_subject_ID(length=7):
 	## Generate random subject IDs so that you can refer to them in R analyses
@@ -181,5 +242,88 @@ def merge_results(date):
 						if 'DS_Store' not in r:
 							copy2('{}/{}/results/{}/'.format(relative_path, date, mod)+d+'/'+r,target)
 
-merge_results(date)
-make_csvs(path)
+def make_heatmaps(data, modelrun_ID, game_names):
+	## takes a cPickle file of a full model run
+	## makes heatmaps for specified games
+	modelrun_ID = modelrun_ID[modelrun_ID.find('201'):modelrun_ID.find('201')+11]
+	subject_ID = generate_subject_ID()
+	
+	agent_type = data['modelParams']
+	exploration_burn_ins = data['exploration_burn_ins'] if 'exploration_burn_ins' in data.keys() else 'NA'
+
+	condition = data['condition'] if 'condition' in data.keys() else 'full'
+	game_name = data['gameInfo']['gameName']
+
+	if game_name in game_names or game_names=='all':
+		for level_number,level in enumerate(data['episodes']):
+			unpacked_states = [item for sublist in level for item in sublist]
+			try:
+				makeHeatmap(unpacked_states, agent_type, '{}_level_{}_{}.pdf'.format(game_name, level_number, agent_type))
+			except:
+				print "error with heatmaps"
+				embed()
+
+def makeHeatmap(statesEncountered, agent_type, filename):
+	from vgdl.plotting import featurePlot
+	import matplotlib.pyplot as plt
+	from matplotlib.ticker import NullLocator
+	import numpy as np
+
+	if agent_type not in os.listdir('heatmaps'):
+		os.makedirs('heatmaps/{}'.format(agent_type))
+
+	avatar_color = 'DARKBLUE'
+	if 'relational' in filename:
+		avatar_color = 'WHITE'
+	states = []
+	for s in statesEncountered:
+		avatar_list = [(o[1][0], o[1][1], s['timestep']) for o in s['objects'] if o[0]==avatar_color]
+		if avatar_list:
+			states.append(avatar_list[0])
+
+	positions = [o[1] for o in statesEncountered[0]['objects']]
+	width, height = max([p[0] for p in positions]), max([p[1] for p in positions])
+	## check all avatar positions; maybe it moves somewhere outside the bounds of the original positions of objects on the screen
+	## this happens in games like aliens
+	width, height = max(width, max([s[0] for s in states])), max(height,max([s[1] for s in states]))
+
+	m = np.zeros((width+1, height+1))
+	Xs, Ys = [],[]
+	block_size = 30
+	prev_state = (None, None)
+	set_first_frame = False
+
+	for s in states:
+		frame = s[2]
+		x = int(round(s[0]))
+		y = int(round(s[1]))
+		if (x,y) != prev_state and (frame!=0 or not set_first_frame):
+			m[x, y] += 1
+
+		prev_state = (x,y)
+		if frame == 0:
+			set_first_frame = True
+
+		# Xs.append(x*block_size+block_size/2.)
+		# Ys.append(y*block_size+block_size/2.)
+	# plt.scatter(x=Xs, y=Ys, alpha=.5, edgecolor='')
+	plt.imshow(m.T, cmap='viridis')
+	plt.gca().set_axis_off()
+	plt.subplots_adjust(top = 1, bottom = 0, right = 1, left = 0,
+		hspace = 0, wspace = 0)
+	plt.margins(0, 0)
+	plt.gca().xaxis.set_major_locator(NullLocator())
+	plt.gca().yaxis.set_major_locator(NullLocator())
+	tmpfilename = filename[:filename.find('.pdf')]+'_max='+str(int(np.max(m)))
+	filename = tmpfilename + '.pdf'
+	# filename = filename+'_max='+str(int(np.max(m)))
+	print filename
+	plt.savefig('heatmaps/{}/{}'.format(agent_type, filename), bbox_inches='tight', pad_inches=0)
+	plt.close()
+
+
+# merge_results(date)
+# make_csvs(path)
+## make all heatmaps
+# game_names = 'all'
+# make_csvs(path, heatmap, game_names)
