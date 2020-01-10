6cb97d66b97765d87631f3073df38e4e84c0820a
PEDRO TSIVIDIS <tsividis@mit.edu>
2020-01-10 12:27:46 -0800

    solution gets saved in agent

--- vgdl/main_agent.py
+++ vgdl/main_agent.py
@@ -1415,7 +1415,7 @@ class Agent:
         self.memory.episodeSteps +=1
         if theory_change_flag:
             self.hypotheses = hypotheses
-            # self.hypotheses[0].display()
+            self.hypotheses[0].display()
 
         print actionDict[action]
         self.rle.step(action)
