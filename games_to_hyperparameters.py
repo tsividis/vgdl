e9bc59d5f7d4822bd890d4b05c2609b15e0ef456
PEDRO TSIVIDIS <tsividis@mit.edu>
2020-01-22 14:51:33 -0500

    running tests

--- games_to_hyperparameters.py
+++ games_to_hyperparameters.py
@@ -1,5 +1,4 @@
-game_names = ['bait', 'expt_preconditions']
-# game_names = ['aliens', 'avoidgeorge', 'bait', 'boulderdash', 'chase', 'expt_antagonist', 'expt_preconditions', 'expt_push_boulders', 'expt_relational', 'frogs', 'lemmings', 'portals', 'plaqueattack', 'sokoban', 'survivezombies', 'zelda']
+game_names = ['aliens', 'avoidgeorge', 'bait', 'boulderdash', 'chase', 'expt_antagonist', 'expt_preconditions', 'expt_push_boulders', 'expt_relational', 'frogs', 'lemmings', 'portals', 'plaqueattack', 'sokoban', 'survivezombies', 'zelda']
 
 # # low memory -- 16gb
 # game_names = ['aliens', 'variant_aliens_1', 'variant_aliens_2', 'variant_aliens_3', 'variant_aliens_4', 
