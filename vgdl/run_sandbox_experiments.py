f02a82bb7abfddf11e832fca7e120d1f698aa663
PEDRO TSIVIDIS <tsividis@mit.edu>
2020-02-14 15:56:59 -0500

    running and plotting grid of parameters

--- vgdl/run_sandbox_experiments.py
+++ vgdl/run_sandbox_experiments.py
@@ -1,4 +1,10 @@
 from generative_model import *
+# from ggplot import *
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+
 
 ## function that takes rules, num_timesteps, error_rates, thresholds, and returns rule overlap
 ## function that calls that function N times
@@ -37,8 +43,67 @@ parameters = {
 	'effect_false_positive_rates': 0.
 }
 
-scores = []
-for i in range(10,1000, 50):
-	scores.append(run_experiments(rules, i, parameters, 5))
 
-embed()
+condition_false_negative_ranges = np.array(range(0,3,1))/10.
+condition_false_positive_ranges = np.array(range(0,3,1))/10.
+effect_false_negative_ranges = np.array(range(0,3,1))/10.
+effect_false_positive_ranges = np.array(range(0,3,1))/10.
+
+data = []
+
+for params in itertools.product(*[condition_false_negative_ranges, condition_false_positive_ranges]):
+	# , effect_false_negative_ranges, effect_false_positive_ranges]):
+
+
+	parameters['condition_false_negative_rates'] = params[0]
+	parameters['condition_false_positive_rates'] = params[1]
+	# parameters['effect_false_negative_rates'] = params[2]
+	# parameters['effect_false_positive_rates'] = params[3]
+
+	param_name = str([(k[0:3], v) for k,v in parameters.items()])
+
+	print param_name
+
+	for i in range(50,1000,50):
+		score = run_experiments(rules, i, parameters, 5)
+		data.append((param_name, i, score))
+
+
+df = pd.DataFrame(data, 
+               columns =['Params', 'Timesteps', 'Score']) 
+
+
+dirname = '../plots/sanbdox_plots/'
+if not os.path.isdir(dirname):
+	os.makedirs(dirname)
+
+
+sns.set(style="whitegrid")
+
+f, ax = plt.subplots(figsize=(8, 6))
+sns.despine(f, left=True, bottom=True)
+
+sns.lineplot(x="Timesteps", y="Score",
+                hue="Params", #size="depth",
+                palette="ch:r=-.2,d=.3_r",
+                # hue_order=clarity_ranking,
+                #sizes=(1, 8), linewidth=0,
+                data=df, ax=ax)
+lgd = plt.legend(loc='upper center',bbox_to_anchor=(.5, -.2), borderaxespad=0.)
+
+plt.savefig(dirname+'foo.png',bbox_extra_artists=(lgd,),bbox_inches='tight', dpi=500)
+
+
+# Draw a scatter plot while assigning point colors and sizes to different
+# # variables in the dataset
+# f, ax = plt.subplots(figsize=(6.5, 6.5))
+# sns.despine(f, left=True, bottom=True)
+
+# sns.scatterplot(x="Timesteps", y="Score",
+#                 hue="Params", #size="depth",
+#                 palette="ch:r=-.2,d=.3_r",
+#                 # hue_order=clarity_ranking,
+#                 sizes=(1, 8), linewidth=0,
+#                 data=df, ax=ax)
+# plt.show()
+
