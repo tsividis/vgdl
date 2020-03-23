d211b6cec7a0b212f888e7aaafd59d004643d215
Momchil Tomov <tomov90@gmail.com>
2020-03-23 18:57:49 -0400

    sh from ncf

--- fmri_empaReplay.sh
+++ fmri_empaReplay.sh
@@ -7,7 +7,7 @@ mkdir output
 subjects=( 1 )  #  e.g. subjects=( 1 2 5 6 7 10 )
 subj_arg="${subjects[@]}" # stringify it
 
-games=('vgfmri3_sokoban' 'vgfmri3_chase' 'vgfmri3_helper' 'vgfmri3_bait' 'vgfmri3_lemmings' 'vgfmri3_plaqueAttack' 'vgfmri3_zelda')
+games=( 'vgfmri3_chase' 'vgfmri3_helper' 'vgfmri3_bait' 'vgfmri3_lemmings' 'vgfmri3_plaqueAttack' 'vgfmri3_zelda')
 #games=('vgfmri3_chase')
 
 echo ---------------- >> jobs.txt
--- run_fmri_empaReplay.sh
+++ run_fmri_empaReplay.sh
@@ -17,7 +17,27 @@ do
     do
         for instance in {0..2}
         do
-            python fmri_empaReplay.py ${1} ${run} ${block} ${instance} ${2}
+            echo ==== run_fmri_empaReplay: subj ${1}, run $run, block $block, instance $instance, game ${2}
+
+            # get # of plays with given run, block, instance
+            out=`mongo heroku_7lzprs54 --eval "db.plays.count({'subj_id': '${1}', 'run_id': ${run}, 'block_id': ${block}, 'instance_id': ${instance}, 'game_name': '${2}'})"`
+
+            echo mongo out -- $out
+
+            # https://stackoverflow.com/questions/24628076/bash-convert-n-delimited-strings-into-array/45565601
+            SAVEIFS=$IFS   # Save current IFS
+            IFS=$'\n'      # Change IFS to new line
+            split=($out) # split into array based on newline
+            IFS=$SAVEIFS   # Restore IFS
+
+            nplays=${split[3]}
+            echo nplays = $nplays
+
+            for (( play=0; play<$nplays; play++ ))
+            do
+                echo ==== run_fmri_empaReplay: subj ${1}, run $run, block $block, instance $instance, play $play, game ${2}
+                python fmri_empaReplay.py ${1} ${run} ${block} ${instance} ${play} ${2}
+            done
         done
     done
 done
