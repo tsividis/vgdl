c1132ec122a13bc22ceef175b15eb861984c8862
Momchil Tomov <tomov90@gmail.com>
2020-04-29 15:02:52 -0400

    theory_template

--- vgdl/theory_template.py
+++ vgdl/theory_template.py
@@ -245,6 +245,21 @@ class ruleCluster(object):
 def printInteractionSet(interactionSet):
 		print [i.display() for i in interactionSet]
 
+# calculate posterior of set of hypotheses
+#
+def getPosterior(hypotheses, timesteps):
+    posts = [0] * len(hypotheses)
+
+    logliks = [sum([h.likelihood(ts) for ts in timesteps]) for h in hypotheses]
+    liks = np.exp(logliks)
+
+    priors = [h.prior() for h in hypotheses]
+
+    posts = np.multiply(liks, priors)
+    posts = np.divide(posts, sum(posts))
+
+    return posts.tolist()
+
 class Theory(object):
 	"""
 	A VGDL description of a game. Corresponds to a 'model' in the TBRL write-up.
@@ -1119,11 +1134,24 @@ class Theory(object):
 		for tc in self.terminationSet:
 			tc.display()
 
-	def display(self):
-		print "_______"
-		self.displayRules()
-		self.displayClasses()
-		self.displayTerminationSet()
+	def display(self, as_string=False):
+
+                if as_string:
+                    # from https://stackoverflow.com/questions/1218933/can-i-redirect-the-stdout-in-python-into-some-sort-of-string-buffer
+                    from cStringIO import StringIO # Python3 use: from io import StringIO
+                    import sys
+                    old_stdout = sys.stdout
+                    sys.stdout = mystdout = StringIO()
+                else:
+                    print "_______"
+
+ 		self.displayRules()
+ 		self.displayClasses()
+ 		self.displayTerminationSet()
+
+                if as_string:
+                    sys.stdout = old_stdout
+                    return mystdout.getvalue()
 		return
 
 	def __eq__(self, other):
