e87b4ae0423a0aab585384b82a959ac5ef6b0569
PEDRO TSIVIDIS <tsividis@mit.edu>
2020-08-03 14:10:31 -0400

    running 1 game each on planner lesion agh2 (no subgoals)

--- model_batch.sh
+++ model_batch.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 #SBATCH --job-name=run_vgdl_model
-#SBATCH --array=0
+#SBATCH --array=0-89
 #SBATCH --output=slurm_logs/main/array_%A_%a.out
-#SBATCH --time=2880
+#SBATCH --time=1440
 #SBATCH --partition=normal
 #SBTACH --cpus-per-task=2
 #SBATCH --mem=64G
@@ -27,7 +27,7 @@ SRC=""
 DST=""
 
 # Figure out which game and hyperparameter
-N_GAMES=1
+N_GAMES=90
 # N_PARAMS=3
 GAME_NUMBER=$(($SLURM_ARRAY_TASK_ID % $N_GAMES))
 # META_IDX=$(($SLURM_ARRAY_TASK_ID / $N_GAMES))
@@ -49,5 +49,5 @@ if [ ! -d "${ROOT}/slurm_logs/main" ]; then
 fi
 
 # finally, run the model
-singularity exec  -B "/$BASE:/$BASE" $CONT python -m vgdl.load_games --game_number $GAME_NUMBER --task_ID $SLURM_ARRAY_TASK_ID --make_movie False
+singularity exec  -B "/$BASE:/$BASE" $CONT python -m vgdl.load_games --game_number $GAME_NUMBER --task_ID $SLURM_ARRAY_TASK_ID --make_movie False --metacontroller_index 3
 # echo "-m vgdl.load_games --game_number ${GAME_NUMBER} --metacontroller_index ${META_IDX}"
