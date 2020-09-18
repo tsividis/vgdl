a5fc312c6aaa56a5a6cdcb45a7dc752661e1eea3
PEDRO TSIVIDIS <tsividis@mit.edu>
2020-09-18 16:18:20 -0400

    raised MAX_STEPS to 100k

--- vgdl/environment.py
+++ vgdl/environment.py
@@ -14,7 +14,7 @@ import random
 Environment class for running VGDL experiments
 """
 
-MAX_STEPS = 10000
+MAX_STEPS = 100000
 actionDict = {K_SPACE: 'space', K_UP: 'up', K_DOWN: 'down', K_LEFT: 'left', K_RIGHT: 'right', 0:'none', None: 'none'}
 
 class Environment:
