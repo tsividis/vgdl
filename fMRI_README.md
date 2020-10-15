df26f952fb42996e260eadd3e38ca94981476a64
Momchil Tomov <tomov90@gmail.com>
2020-10-15 17:32:24 -0400

    fmri_empaTheoryReplay

--- fMRI_README.md
+++ fMRI_README.md
@@ -110,6 +110,12 @@ Btw seems like the `ncf` and `ncf_interact` nodes are on a different network fro
 
 Then `mongodump` from a `ncf_interact` node and then `mongorestore` on the VDI node.
 
+
+On Cannon cluster, create [VDI node](https://vdi.rc.fas.harvard.edu/pun/sys/dashboard/batch_connect/sessions) (FAS-RC remote desktop, 28 day, 20 GB) and start with:
+
+    mongod --config /n/gershman_lab/users/mtomov13/mongo/mongod.conf
+
+
 fMRI Human Play
 -----
 
---
+++ fmri_empaTheoryReplay.sh
@@ -0,0 +1,45 @@
+# run fmri_empaTheoryReplay for a bunch of subjects
+# copied from ccnl_fmri_preproc.sh
+#
+
+mkdir output
+
+subjects=( 1 )  #  e.g. subjects=( 1 2 5 6 7 10 )
+subj_arg="${subjects[@]}" # stringify it
+
+#games=( 'vgfmri3_chase' 'vgfmri3_helper' 'vgfmri3_bait' 'vgfmri3_lemmings' 'vgfmri3_plaqueAttack' 'vgfmri3_zelda')
+games=( 'vgfmri3_lemmings'  )
+#games=( 'vgfmri3_chase' 'vgfmri3_zelda')
+#games=( 'none' )
+
+echo ---------------- >> jobs.txt
+echo --- $(date): Running fmri_empaTheoryReplay for subjects ${subj_arg} in parallel >> jobs.txt
+echo ---------------- >> jobs.txt
+head -n 1 gitlog.txt >> jobs.txt
+
+for subj in ${subjects[*]}; do
+    for game in ${games[*]}; do
+        outfileprefix="output/fmri_empaTheoryReplay_${subj}_${game}"
+        echo ---------------------------------------------------------------------------------
+        echo Subject ${subj}, game ${game}, file prefix = $outfileprefix
+
+        # send the job to NCF
+        #
+        sbatch_output=`sbatch -p ncf --mem 20001 -t 1-15:20 -o ${outfileprefix}_%j.out -e ${outfileprefix}_%j.err --wrap="./run_fmri_empaTheoryReplay.sh ${subj} ${game}"`
+        # for local testing
+        #sbatch_output=`echo Submitted batch job 88725418`
+        echo $sbatch_output
+
+        # Append job id to jobs.txt
+        #
+        sbatch_output_split=($sbatch_output)
+        job_id=${sbatch_output_split[3]}
+        echo fmri_empaTheoryReplay.sh for subject ${subj}, game ${game}: ${outfileprefix}_${job_id}.out -- $sbatch_output >> jobs.txt
+
+        echo watch job status with: sacct -j ${job_id}
+        echo watch output with: tail -f ${outfileprefix}_${job_id}.out
+        echo watch error with: tail -f ${outfileprefix}_${job_id}.err
+
+        sleep 1
+    done
+done
