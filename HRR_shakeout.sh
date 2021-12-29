f49daa4041b777989296a7a4ae9f533e032b8f3d
Momchil Tomov <mtomov13@fasselogin01.rc.fas.harvard.edu>
2021-12-29 12:26:43 -0500

    HRR_shakeout.sh

---
+++ HRR_shakeout.sh
@@ -0,0 +1,7 @@
+python HRR.py --subj-id=1 --K=50 --N=100 --E=0.05 --nsamples=1 --batch-size=1 --normalize=2 --type=kernel 
+
+# UNTESTED
+#python HRR.py --subj-id=1 --K=50 --N=100 --E=0.05 --nsamples=1  --normalize=2 --type=unique 
+
+# UNTESTED
+#python HRR.py --subj-id=1 --K=50 --N=100 --E=0.05 --nsamples=1 --batch-size=1 --normalize=2 --dist=correlation --glmodel=24 --agg=avg  --type=RDM
