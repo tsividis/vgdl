a093d6f362cf3c8b24406ac0f43b523bded1d3b5
Cédric Foucault <cedric.foucault@gmail.com>
2025-02-27 17:23:53 +0000

    Add code to perform photodiode testing of trigger timing

--- vgdl/core.py
+++ vgdl/core.py
@@ -42,7 +42,8 @@ import meg_trigger
 #     Constants
 # ---------------------------------------------------------------------
 
-fMRI_screensize = (800,580)  # dimension of the screen shown to participants
+# fMRI_screensize = (800,580)  # dimension of the screen shown to participants
+fMRI_screensize = (1920,1080)  # dimension of the screen shown to participants
 render_screensize = (80,60)  # dimensions of the renders used for training the DQN, PCA, etc.
 BLOCK_SIZE = 35 #35
 
@@ -54,6 +55,9 @@ keyPresses = {K_UP: 'up', K_DOWN: 'down', K_LEFT: 'left', K_RIGHT: 'right', K_SP
 direction_to_key = {"left": K_LEFT, "right": K_RIGHT, "up": K_UP, "down": K_DOWN}
 # emptyKeyState = tuple([0]*323) #keyState when no keys are pressed
 emptyKeyState = util.getEmptyKeyState()
+
+DO_PHOTODIODE_TEST = False
+# DO_PHOTODIODE_TEST = True # If photodiode test, flash a white circle on black background above the game world
 
 # fMRI helpers
 # TODO momchil put somewhere else
@@ -370,6 +374,8 @@ class VGDLParser(object):
             levels = game['levels']
             alphabet = game['alphabet']
             bg_color = game['bg_color']
+            if DO_PHOTODIODE_TEST:
+                bg_color = black
             assert games[block['game_id']] == game
             
             run['blocks'][b]['start_time'] = time.time()
@@ -1998,6 +2004,17 @@ class BasicGame(object):
                         trigger.send(play_clock=trigger_play_clock)
                 else:
                     trigger_play_clock = None # To record that no trigger was sent on this frame
+                if DO_PHOTODIODE_TEST:
+                    # print("do photodiode")
+                    x, y = (fMRI_screensize[0]//2, 40)
+                    radius = 24
+                    rect = pygame.Rect(x-radius, y-radius, radius*2, radius*2)
+                    if (self.time % self.frame_rate) == 0:
+                        pygame.draw.circle(self.screen, white, (x, y), radius)
+                        pygame.display.update(rect)
+                    elif (self.time % self.frame_rate) == 1: # 50 ms flashing circle at 20 FPS
+                        pygame.draw.circle(self.screen, black, (x, y), radius)
+                        pygame.display.update(rect)
 
             if fMRI_timeout is not None: # TODO momchil have actual is_fMRI flag
                 self._fMRI_clearAll()
